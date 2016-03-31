2f7b9e5c95dee1232db3991b330677f6dea09610
Eduardo Perdices <eperdices@gsyc.es>
2016-03-31 16:38:50 +0200

    Obstacles avoidance: Targest updated

--- src/obstacle_avoidance/MyAlgorithm.py
+++ src/obstacle_avoidance/MyAlgorithm.py
@@ -33,11 +33,30 @@ class MyAlgorithm():
 
     def initTargets(self):
         self.targets = []
-        self.targets.append(Target('target1',jderobot.Pose3DData(1,-30,0,0,0,0,0,0),False,False))
-        self.targets.append(Target('target1',jderobot.Pose3DData(-2,-40,0,0,0,0,0,0),False,False))
-        self.targets.append(Target('target2',jderobot.Pose3DData(-11,-37,0,0,0,0,0,0),False,False))
-        self.targets.append(Target('target2',jderobot.Pose3DData(-13,-33,0,0,0,0,0,0),False,False))
-        self.targets.append(Target('target2',jderobot.Pose3DData(-13,-18,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target01',jderobot.Pose3DData(1,-30,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target02',jderobot.Pose3DData(-5,-41,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target03',jderobot.Pose3DData(-12,-33,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target04',jderobot.Pose3DData(-15,-14,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target05',jderobot.Pose3DData(-54,-13,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target06',jderobot.Pose3DData(-67,-29,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target07',jderobot.Pose3DData(-71,3,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target08',jderobot.Pose3DData(-49,6,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target09',jderobot.Pose3DData(-49,20,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target10',jderobot.Pose3DData(-118,20,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target11',jderobot.Pose3DData(-116,8,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target12',jderobot.Pose3DData(-106,-2,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target13',jderobot.Pose3DData(-150,-4,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target14',jderobot.Pose3DData(-150,40,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target15',jderobot.Pose3DData(-106,41,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target16',jderobot.Pose3DData(-96,29,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target17',jderobot.Pose3DData(-84,43,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target18',jderobot.Pose3DData(-46,49,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target19',jderobot.Pose3DData(-40,62,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target20',jderobot.Pose3DData(-31,45,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target21',jderobot.Pose3DData(-20,45,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target22',jderobot.Pose3DData(-17,57,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target23',jderobot.Pose3DData(-1,57,0,0,0,0,0,0),False,False))
+        self.targets.append(Target('target24',jderobot.Pose3DData(0,0,0,0,0,0,0,0),False,False))
 
     def getNextTarget(self):
         for target in self.targets:
