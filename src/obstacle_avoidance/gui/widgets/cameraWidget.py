3ff12d99a351310e6fea852ebf21c5ef8519d593
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2016-10-20 10:42:06 +0200

    updated widgets to PyQt5

--- src/obstacle_avoidance/gui/widgets/cameraWidget.py
+++ src/obstacle_avoidance/gui/widgets/cameraWidget.py
@@ -1,25 +1,7 @@
-#
-#  Copyright (C) 1997-2016 JDE Developers Team
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see http://www.gnu.org/licenses/.
-#  Authors :
-#       Alberto Martin Florido <almartinflorido@gmail.com>
-#       Aitor Martinez Fernandez <aitor.martinez.fernandez@gmail.com>
-#
 
-from PyQt4 import QtGui,QtCore
+from PyQt5 import QtGui,QtCore
 import cv2
+
 
 class CameraWidget:
     IMG_WIDTH=320
@@ -36,7 +18,7 @@ class CameraWidget:
     def updateImage(self):
 
         imgLeft = self.winParent.getCameraL().getImage()
-        if imgLeft is not None:
+        if imgLeft is not  None:
             resized = cv2.resize(imgLeft,(self.IMG_WIDTH,self.IMG_HEIGHT))
             image = QtGui.QImage(resized.data, resized.shape[1], resized.shape[0], resized.shape[1]*resized.shape[2], QtGui.QImage.Format_RGB888);
             size=QtCore.QSize(imgLeft.shape[1],imgLeft.shape[0])
@@ -55,7 +37,7 @@ class CameraWidget:
         #print the filtered images
 
         imgLeftFiltered = self.winParent.getAlgorithm().getLeftImageFiltered()
-        if imgLeftFiltered != None:
+        if imgLeftFiltered is not None:
             resized = cv2.resize(imgLeftFiltered,(self.IMG_WIDTH,self.IMG_HEIGHT))
             image = QtGui.QImage(resized.data, resized.shape[1], resized.shape[0], resized.shape[1]*resized.shape[2], QtGui.QImage.Format_RGB888);
             size=QtCore.QSize(imgLeftFiltered.shape[1],imgLeftFiltered.shape[0])
@@ -63,7 +45,7 @@ class CameraWidget:
             self.labelImageLeftFiltered.setPixmap(QtGui.QPixmap.fromImage(image))
 
         imgRightFiltered = self.winParent.getAlgorithm().getRightImageFiltered()
-        if imgRightFiltered != None:
+        if imgRightFiltered is not None:
             resized = cv2.resize(imgRightFiltered,(self.IMG_WIDTH,self.IMG_HEIGHT))
             image = QtGui.QImage(resized.data, resized.shape[1], resized.shape[0], resized.shape[1]*resized.shape[2], QtGui.QImage.Format_RGB888);
             size=QtCore.QSize(imgRightFiltered.shape[1],imgRightFiltered.shape[0])
