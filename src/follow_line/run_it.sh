1decf7b97d9652d408b86a4760ffdaa2171631cf
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2016-11-08 14:27:05 +0100

    [issue #105] deleted all worlds and updated run_it.sh

--- src/follow_line/run_it.sh
+++ src/follow_line/run_it.sh
@@ -4,15 +4,15 @@
 # Author: Victor Arribas <v.arribas.urjc@gmail.com>
 # License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
 
-world=gazebo/simpleCircuit.world
-#world=gazebo/circuit.world
+world=simpleCircuit.world
+#world=circuit.world
 
 gzserver --verbose --minimal_comms $world &
 sleep 5 # up to 20 for circuit.world
 
 [ "$1" = "GUI" ] && gzclient &
 
-python main.py --Ice.Config=followLineF1.cfg
+python3 follow_line.py --Ice.Config=followLineF1.cfg
 
 killall gzserver
 [ "$1" = "GUI" ] && killall gzclient
