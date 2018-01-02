fbd7d51a03b773701381743b86e0bc0aecb83fa0
cawadall <supesandiastyle@hotmail.es>
2018-01-02 12:00:12 +0100

    improved GUI

--- src/3d_reconstruction/MyAlgorithm.py
+++ src/3d_reconstruction/MyAlgorithm.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from docutils.nodes import image
 from sensors import sensor
 import numpy as np
 import threading
@@ -9,46 +13,18 @@ class MyAlgorithm():
 
     def __init__(self, sensor):
         self.sensor = sensor
-        self.imageRight=None
-        self.imageLeft=None
+        self.imageRight=np.zeros((320,240,3), np.uint8)
+        self.imageLeft=np.zeros((320,240,3), np.uint8)
         self.lock = threading.Lock()
-        self.camLeftP=Progeo("cameras/camA.json")
-        self.camRightP=Progeo("cameras/camB.json")
+
+        print("Left Camera Configuration File:")
+        self.camLeftP=Progeo(sys.argv[1], "CamACalibration")
+        print("Rigth Camera Configuration File:")
+        self.camRightP=Progeo(sys.argv[1], "CamBCalibration")
+
         self.done=False
 
-    def execute(self):
-        #GETTING THE IMAGES
-        imageLeft = self.sensor.getImageLeft()
-        imageRight = self.sensor.getImageRight()
-
-        if self.done:
-            return
-
-        # Add your code here
-        # pointIn=np.array([502,21,1])
-        # pointInOpt=self.camLeftP.graficToOptical(pointIn)
-        # point3d=self.camLeftP.backproject(pointInOpt)
-        # projected1 = self.camRightP.project(point3d)
-        # print (self.camRightP.opticalToGrafic(projected1))
-
-
-
-
-
-        #EXAMPLE OF HOW TO SEND INFORMATION TO THE ROBOT ACTUATORS
-        #self.sensor.setV(10)
-        #self.sensor.setW(5)
-
-
-        #SHOW THE FILTERED IMAGE ON THE GUI
-        # self.setRightImageFiltered(imageRight)
-        # self.setLeftImageFiltered(imageLeft)
-
-        #PLOT 3D data on the viewer
-        #point=np.array([1, 1, 1])
-        #self.sensor.drawPoint(point,(255,255,255))
-
-
+        self.counter=0
 
     def setRightImageFiltered(self, image):
         self.lock.acquire()
@@ -67,14 +43,33 @@ class MyAlgorithm():
         self.imageLeft=image
         self.lock.release()
 
-    def getRightImageFiltered(self):
-        self.lock.acquire()
-        tempImage=self.imageRight
-        self.lock.release()
-        return tempImage
+    def execute(self):
+      
+        #OBTENCIÓN DE IMÁGENES
+        imageLeft = self.sensor.getImageLeft()
+        imageRight = self.sensor.getImageRight()
+        
+        if self.done:
+            return
 
-    def getLeftImageFiltered(self):
-        self.lock.acquire()
-        tempImage=self.imageLeft
-        self.lock.release()
-        return tempImage
+
+        # Add your code here
+        # pointIn=np.array([502,21,1])
+        # pointInOpt=self.camLeftP.graficToOptical(pointIn)
+        # point3d=self.camLeftP.backproject(pointInOpt)
+        # projected1 = self.camRightP.project(point3d)
+        # print (self.camRightP.opticalToGrafic(projected1))
+
+        #EXAMPLE OF HOW TO SEND INFORMATION TO THE ROBOT ACTUATORS
+        #self.sensor.setV(10)
+        #self.sensor.setW(5)
+
+
+        #SHOW THE FILTERED IMAGE ON THE GUI
+        # self.setRightImageFiltered(imageRight)
+        # self.setLeftImageFiltered(imageLeft)
+
+        #PLOT 3D data on the viewer
+        #point=np.array([1, 1, 1])
+        #self.sensor.drawPoint(point,(255,255,255))
+
