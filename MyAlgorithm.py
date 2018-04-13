1164b9035db1855a7e45ea9c4ffb2ec94c44ada9
Pablo Moreno Vera <pablomvera14@gmail.com>
2018-04-13 12:23:16 +0200

    Add files via upload

---
+++ MyAlgorithm.py
@@ -0,0 +1,83 @@
+import threading
+import time
+from datetime import datetime
+import cv2
+import numpy as np
+
+from parallelIce.navDataClient import NavDataClient
+from parallelIce.cmdvel import CMDVel
+from parallelIce.extra import Extra
+from parallelIce.pose3dClient import Pose3DClient
+
+
+time_cycle = 80
+
+class MyAlgorithm(threading.Thread):
+
+    def __init__(self, camera, navdata, pose, cmdvel, extra):
+        self.camera = camera
+        self.navdata = navdata
+        self.pose = pose
+        self.cmdvel = cmdvel
+        self.extra = extra
+
+        self.image=None
+
+        self.stop_event = threading.Event()
+        self.kill_event = threading.Event()
+        self.lock = threading.Lock()
+        threading.Thread.__init__(self, args=self.stop_event)
+
+    def setImageFiltered(self, image):
+        self.lock.acquire()
+        self.image=image
+        self.lock.release()
+
+    def getImageFiltered(self):
+        self.lock.acquire()
+        tempImage=self.image
+        self.lock.release()
+        return tempImage
+
+    def run (self):
+
+        self.stop_event.clear()
+
+        while (not self.kill_event.is_set()):
+
+            start_time = datetime.now()
+
+            if not self.stop_event.is_set():
+                self.execute()
+
+            finish_Time = datetime.now()
+
+            dt = finish_Time - start_time
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            #print (ms)
+            if (ms < time_cycle):
+                time.sleep((time_cycle - ms) / 1000.0)
+
+    def stop (self):
+        self.stop_event.set()
+
+    def play (self):
+        if self.is_alive():
+            self.stop_event.clear()
+        else:
+            self.start()
+
+    def kill (self):
+        self.kill_event.set()
+
+
+    def execute(self):
+       # Add your code here
+
+        input_image = self.camera.getImage().data
+        if input_image is not None:
+            self.setImageFiltered(input_image)
+            '''
+            If you want show a thresold image (black and white image)
+            self.camera.setThresholdImage(bk_image)
+            '''
