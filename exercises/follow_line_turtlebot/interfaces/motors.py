c086da97d9a41f79f2a8397a06e16303a4964525
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2019-02-13 10:45:44 +0100

    [#271] Removed unnecessary imports and prepared for the real kobuki

--- exercises/follow_line_turtlebot/interfaces/motors.py
+++ exercises/follow_line_turtlebot/interfaces/motors.py
@@ -15,34 +15,8 @@ from geometry_msgs.msg import Twist
 import threading
 from math import pi as PI
 from .threadPublisher import ThreadPublisher
+from copy import deepcopy
 
-def cmdvel2Twist(vel):
-    tw = Twist()
-    tw.linear.x = vel.vx
-    tw.linear.y = vel.vy
-    tw.linear.z = vel.vz
-    tw.angular.x = vel.ax
-    tw.angular.y = vel.ay
-    tw.angular.z = vel.az
-    return tw
-
-class CMDVel ():
-
-    def __init__(self):
-        self.vx = 0 # vel in x[m/s] (use this for V in wheeled robots)
-        self.vy = 0 # vel in y[m/s]
-        self.vz = 0 # vel in z[m/s]
-        self.ax = 0 # angular vel in X axis [rad/s]
-        self.ay = 0 # angular vel in X axis [rad/s]
-        self.az = 0 # angular vel in Z axis [rad/s] (use this for W in wheeled robots)
-        self.timeStamp = 0 # Time stamp [s]
-
-    def __str__(self):
-        s = "CMDVel: {\n   vx: " + str(self.vx) + "\n   vy: " + str(self.vy)
-        s = s + "\n   vz: " + str(self.vz) + "\n   ax: " + str(self.ax) 
-        s = s + "\n   ay: " + str(self.ay) + "\n   az: " + str(self.az)
-        s = s + "\n   timeStamp: " + str(self.timeStamp)  + "\n}"
-        return s 
 
 class PublisherMotors:
  
@@ -50,7 +24,7 @@ class PublisherMotors:
         self.maxW = maxW
         self.maxV = maxV
         self.topic = topic
-        self.data = CMDVel()
+        self.tw = Twist()
         self.pub = self.pub = rospy.Publisher(self.topic, Twist, queue_size=1)
         rospy.init_node("FollowLineTurtlebot")
         self.lock = threading.Lock()
@@ -61,7 +35,7 @@ class PublisherMotors:
  
     def publish (self):
         self.lock.acquire()
-        tw = cmdvel2Twist(self.data)
+        tw = deepcopy(self.tw)
         self.lock.release()
         self.pub.publish(tw)
         
@@ -81,7 +55,7 @@ class PublisherMotors:
 
     def sendVelocities(self, vel):
         self.lock.acquire()
-        self.data = vel
+        self.tw = deepcopy(vel)
         self.lock.release()
 
     def sendV(self, v):
@@ -95,16 +69,16 @@ class PublisherMotors:
 
     def sendVX(self, vx):
         self.lock.acquire()
-        self.data.vx = vx
+        self.tw.linear.x = vx
         self.lock.release()
 
     def sendVY(self, vy):
         self.lock.acquire()
-        self.data.vy = vy
+        self.tw.linear.y = vy
         self.lock.release()
 
     def sendAZ(self, az):
         self.lock.acquire()
-        self.data.az = az
+        self.tw.angular.z = az
         self.lock.release()
 
