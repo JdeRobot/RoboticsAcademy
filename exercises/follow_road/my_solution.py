be59e56b3297bedfdd3599a664a9a2abf9246bb8
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2019-07-09 03:44:52 +0530

    added ros version and updated readme

---
+++ exercises/follow_road/my_solution.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import cv2
+from drone_wrapper import DroneWrapper
+from std_msgs.msg import Bool, Float64
+from sensor_msgs.msg import Image
+
+code_live_flag = False
+
+def gui_takeoff_cb(msg):
+	if msg.data:
+		drone.takeoff()
+	else:
+		drone.land()
+
+def gui_play_stop_cb(msg):
+	global code_live_flag, code_live_timer
+	if msg.data == True:
+		if not code_live_flag:
+			code_live_flag = True
+			code_live_timer = rospy.Timer(rospy.Duration(nsecs=50000000), execute)
+	else:
+		if code_live_flag:
+			code_live_flag = False
+			code_live_timer.shutdown()
+		
+def gui_alt_slider_cb(msg):
+	global drone
+	drone.set_cmd_vel(vz = msg.data)
+
+def gui_rotation_dial_cb(msg):
+	global drone
+	drone.set_cmd_vel(az = msg.data)
+
+def set_image_filtered(img):
+	gui_filtered_img_pub.publish(drone.bridge.cv2_to_imgmsg(img))
+
+def set_image_threshed(img):
+	gui_threshed_img_pub.publish(drone.bridge.cv2_to_imgmsg(img))
+
+def execute(event):
+	global drone
+	img_frontal = drone.get_frontal_image()
+	img_ventral = drone.get_ventral_image()
+	# Both the above images are cv2 images
+	################# Insert your code here #################################
+
+	set_image_filtered(img_frontal)
+	set_image_threshed(img_ventral)
+
+	#########################################################################
+
+if __name__ == "__main__":
+	drone = DroneWrapper()
+	rospy.Subscriber('gui/takeoff_land', Bool, gui_takeoff_cb)
+	rospy.Subscriber('gui/play_stop', Bool, gui_play_stop_cb)
+	rospy.Subscriber('gui/alt_slider', Float64, gui_alt_slider_cb)
+	rospy.Subscriber('gui/rotation_dial', Float64, gui_rotation_dial_cb)
+	gui_filtered_img_pub = rospy.Publisher('interface/filtered_img', Image, queue_size = 1)
+	gui_threshed_img_pub = rospy.Publisher('interface/threshed_img', Image, queue_size = 1)
+	code_live_flag = False
+	code_live_timer = rospy.Timer(rospy.Duration(nsecs=50000000), execute)
+	code_live_timer.shutdown()
+	while not rospy.is_shutdown():
+		rospy.spin()
