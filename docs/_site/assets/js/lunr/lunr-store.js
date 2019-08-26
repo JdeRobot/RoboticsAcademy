678f36044fd2eb607fee26d50a864579a62bda3f
nachoaz <naxo.elderane@gmail.com>
2019-08-26 20:42:29 +0200

    Installation guide for local running added

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "3D Reconstruction",
+        "excerpt":"In this practice, the intention is to program the necessary logic to allow kobuki robot to generate a 3D reconstruction of the scene that it is receiving throughout its left and right cameras.                                                  3D Reconstruction.       ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/3D-Reconstruction/",
+        "teaser":"http://localhost:4000/images/3d_reconstruction.png"},{
+        "title": "Autonomous robot control in the Amazon Warehouse",
+        "excerpt":"The objective of this practice is to implement the autonomous robot navigation and pick-and-place logic in warehouse. Amazon Warehouse. The students program an Amazon robot to navigate to the seleceted pallet, pick it, and deliver to the required zone. After completion of the task, go the charging area, and wait...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Amazon-Warehouse/",
+        "teaser":"http://localhost:4000/images/amazon_warehouse.png"},{
+        "title": "Color Filter",
+        "excerpt":"In this practice the intention is to develop a color filter that allow us to segment some object in the image. You will have to get in contact with RGB and HSV color spaces, and OpenCV (python) library. Color Filter. For the realization of the practice, you are provided of...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Color-Filter/",
+        "teaser":"http://localhost:4000/images/color_filter.png"},{
+        "title": "Follow Face",
+        "excerpt":"In this practice, the intention is to use your knowledge in image processing to segment the faces of people and follow them through a camera connected by USB to your computer. For this, you must have the right hardware (Sony model EVI d100p), and then implement the logic that performs...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Follow-Face/",
+        "teaser":"http://localhost:4000/images/follow_face.png"},{
+        "title": "Visual follow-line behavior on a Formula1",
+        "excerpt":"The objective of this practice is to perform a PID reactive control capable of following the line painted on the racing circuit.                                                  Racing circuit.       The students program a Formula1 car in a race circuit to follow the red line in the middle of the road.            ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Follow-Line/",
+        "teaser":"http://localhost:4000/images/f1.png"},{
+        "title": "Local navigation of a Formula1 with VFF",
+        "excerpt":"The objective of this practice is to implement the logic of the VFF navigation algorithm. Navigation using VFF (Virtual Force Field), consists of: Each object in the environment generates a repulsive force towards the robot. Destiny generates an attractive force in the robot. This makes it possible for the robot...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Obstacle-Avoidance/",
+        "teaser":"http://localhost:4000/images/obstacle_avoidance.png"},]
