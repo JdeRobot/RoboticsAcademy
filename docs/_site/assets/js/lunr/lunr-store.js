32b32796d8a35a8e7d447ae7d95e5ef18d0f4c9d
nachoaz <naxo.elderane@gmail.com>
2019-08-27 21:54:29 +0200

    Updated exercises from upstream

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "3D Reconstruction",
-        "excerpt":"In this practice, the intention is to program the necessary logic to allow kobuki robot to generate a 3D reconstruction of the scene that it is receiving throughout its left and right cameras.                                                  3D Reconstruction.       ","categories": [],
+        "excerpt":"In this practice, the intention is to program the necessary logic to allow kobuki robot to generate a 3D reconstruction of the scene that it is receiving throughout its left and right cameras.                                                  3D Reconstruction.                ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/portfolio/3D-Reconstruction/",
         "teaser":"http://localhost:4000/images/3d_reconstruction.png"},{
@@ -9,6 +9,26 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/portfolio/Amazon-Warehouse/",
         "teaser":"http://localhost:4000/images/amazon_warehouse.png"},{
+        "title": "Robot self-localization using particle filter and laser sensor",
+        "excerpt":"The goal of this practice is to implement the logic that allows a robot to self locate in space. In particular, it is about getting the robot to carry out an algorithm of self-location, using only a laser sensor, to estimate its position in a given space (of which a...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Autoloc-Laser/",
+        "teaser":"http://localhost:4000/images/autoloc.png"},{
+        "title": "Autoparking",
+        "excerpt":"The goal of this practice is to implement the logic of a navigation algorithm for an automated vehicle. The vehicle must find a parking space and park properly.                                                  Autoparking.                ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Autoparking/",
+        "teaser":"http://localhost:4000/images/autoparking.png"},{
+        "title": "Bump and go",
+        "excerpt":"The intention of this excersise is to program a basic behaviour of bump-spin using a finite state machine. For that, we will use JdeRobot visualStates tool, that allows you to create your own states machine in an intuitive way. There is a Kobuki robot inside a labyrinth or scenario. The...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Bump-And-Go/",
+        "teaser":"http://localhost:4000/images/bump_go.png"},{
+        "title": "Car Junction",
+        "excerpt":"The goal of this practice is to implement the logic of a navigation algorithm for an automated vehicle. The vehicle must Stop at the T joint, where there is a stop sign, wait until there are no cars and pass once the road is clear.                                                  Car Junction.                ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Car-Junction/",
+        "teaser":"http://localhost:4000/images/car_junction.png"},{
         "title": "Color Filter",
         "excerpt":"In this practice the intention is to develop a color filter that allow us to segment some object in the image. You will have to get in contact with RGB and HSV color spaces, and OpenCV (python) library. Color Filter. For the realization of the practice, you are provided of...","categories": [],
         "tags": [],
@@ -24,8 +44,23 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/portfolio/Follow-Line/",
         "teaser":"http://localhost:4000/images/f1.png"},{
+        "title": "Global navigation of a TeleTaxi",
+        "excerpt":"The objective of this practice is to implement the logic of a Gradient Path Planning (GPP) algorithm. Global navigation through GPP, consists of: Selected a destination, the GPP algorithm is responsible for finding the shortest path to it, avoiding, in the case of this practice, everything that is not road....","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Global-Navigation/",
+        "teaser":"http://localhost:4000/images/global_navigation.png"},{
         "title": "Local navigation of a Formula1 with VFF",
         "excerpt":"The objective of this practice is to implement the logic of the VFF navigation algorithm. Navigation using VFF (Virtual Force Field), consists of: Each object in the environment generates a repulsive force towards the robot. Destiny generates an attractive force in the robot. This makes it possible for the robot...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/portfolio/Obstacle-Avoidance/",
-        "teaser":"http://localhost:4000/images/obstacle_avoidance.png"},]
+        "teaser":"http://localhost:4000/images/obstacle_avoidance.png"},{
+        "title": "Qualifying Formula1",
+        "excerpt":"Program a Formula1 car to autonomously complete a lap to the Nürburgring circuit as fast as it can. Compare the evolution of your car with the record along the whole circuit!.                                                  Chrono.                ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Qualifying-Formula1/",
+        "teaser":"http://localhost:4000/images/chrono.png"},{
+        "title": "Vacuum-cleaner",
+        "excerpt":"The objective of this practice is to implement the logic of a navigation algorithm for an autonomous vacuum. The main objective will be to cover the largest area of ​​a house using the programmed algorithm. Vacuum cleaner. One possible solution is to implement the logic of a navigation algorithm for...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/Vacuum-Cleaner/",
+        "teaser":"http://localhost:4000/images/vacuum.png"},]
