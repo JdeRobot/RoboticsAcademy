9550f355641069ec2e8dffc1bbc9b76d1cccef21
SakshayMahna <sakshum19@gmail.com>
2020-09-10 19:32:35 +0530

    Laser and Arrows

--- exercises/obstacle_avoidance/web-template/assets/map_view.js
+++ exercises/obstacle_avoidance/web-template/assets/map_view.js
@@ -15,7 +15,7 @@ function paintEvent(target, car, obs, avg, laser, max_range){
 	drawTarget(target[0], target[1]);
 	
 	// Draw arrows
-	drawArrow(car[0], car[1], "#32CD32");
+	drawArrow(car[0], car[1], "#7CFC00");
 	drawArrow(obs[0], obs[1], "#DC143C");
 	drawArrow(avg[0], avg[1], "#000000");
 }
@@ -43,11 +43,11 @@ function drawCar(){
 	ctx.fillRect(155, 139, 5, 8);
 	
 	ctx.stroke();
+	
+	ctx.closePath();
 }
 
-function drawLaser(laser){
-	max_range = 110;
-	
+function drawLaser(laser, max_range){
 	for (let d of laser){
 		if (d[0] > max_range){
 			py = 120 - max_range * Math.sin(d[1]);
@@ -58,12 +58,14 @@ function drawLaser(laser){
 			px = 146.5 + d[0] * Math.cos(d[1]);
 		}
 		
+		ctx.beginPath();
+		
 		ctx.strokeStyle = "#6897BB";
 		ctx.moveTo(146.5, 120);
 		ctx.lineTo(px, py);
 		ctx.stroke();
 		
-		console.log(py);
+		ctx.closePath();
 	}
 }
 
@@ -73,8 +75,10 @@ function drawArrow(posx, posy, color){
 		return;
 	}
 	
-	px = 146.5 + posx * 2;
-	py = 120 - posy * 2;
+	px = 146.5 + posx * 20;
+	py = 120 - posy * 20;
+	
+	ctx.beginPath();
 	
 	// The main line
 	ctx.strokeStyle = color;
@@ -82,47 +86,45 @@ function drawArrow(posx, posy, color){
 	ctx.lineTo(px, py);
 	
 	// Sides
-	sidex = Math.hypot(px, py) / 5;
-	sidey = Math.hypot(px, py) / 5;
+	side = 3 * Math.hypot(posx, posy);
 	
-	if(px != 0){
-		ang = Math.atan2(py, px);
+	if(posx != 0){
+		ang = Math.atan2(posy, posx);
 	}
 	else{
 		ang = Math.PI / 2;
 	}
 	
-	if(posx > 0.0){
-		px1 = px + sidex * Math.cos(Math.PI + ang - 0.5);
-		py1 = py + sidey * Math.sin(Math.PI + ang - 0.5);
-		px2 = px + sidex * Math.cos(Math.PI + ang + 0.5);
-		py2 = py + sidey * Math.sin(Math.PI + ang + 0.5);
-	}
-	else if(posx == 0){
-		px1 = px + sidex * Math.cos(ang - 0.5);
-		py1 = py + sidey * Math.sin(ang - 0.5);
-		px2 = px + sidex * Math.cos(ang + 0.5);
-		py2 = py + sidey * Math.sin(ang + 0.5);
+	if(posx == 0.0){
+		px1 = px + side * Math.cos(ang - 0.5);
+		py1 = py + side * Math.sin(ang - 0.5);
+		px2 = px + side * Math.cos(ang + 0.5);
+		py2 = py + side * Math.sin(ang + 0.5);
 	}
 	else{
-		px1 = px - sidex * math.cos(ang - 0.5);
-		py1 = py - sidey * math.sin(ang - 0.5);
-		px2 = px - sidex * math.cos(ang + 0.5);
-		py2 = py - sidey * math.sin(ang + 0.5);
+		px1 = px + side * Math.cos(5 * Math.PI / 6 + ang);
+		py1 = py - side * Math.sin(5 * Math.PI / 6 + ang);
+		px2 = px + side * Math.cos(5 * Math.PI / 6 - ang);
+		py2 = py + side * Math.sin(5 * Math.PI / 6 - ang);
 	}
 	
 	ctx.moveTo(px, py);
 	ctx.lineTo(px1, py1);
+	//ctx.moveTo(px, py);
 	ctx.lineTo(px2, py2);
+	ctx.lineTo(px, py);
 	
 	ctx.stroke();
 	
+	ctx.closePath();
 }
 
 function drawTarget(posx, posy){
 	if(posx == 0 && posy == 0){
 		return;
 	}
+	
+	ctx.beginPath();
 	
 	ctx.strokeStyle = 'yellow';
 	ctx.lineWidth = 2;
@@ -144,10 +146,12 @@ function drawTarget(posx, posy){
 	ctx.lineTo(ex, ey);
 	
 	ctx.stroke();
+	
+	ctx.closePath();
 }
 
 
-
+//drawArrow(-2, 2, "#7CFC00");
 //drawCar();
 //drawLaser([[130, Math.PI / 2]]);
 //drawTarget(120, 90);
