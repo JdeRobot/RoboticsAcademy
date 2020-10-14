9afc2c1ffd059fb31a2c02a527a1b3ada6ebc40d
SakshayMahna <sakshum19@gmail.com>
2020-10-14 12:14:46 +0530

    Update offline manager

--- exercises/follow_line/web-template/assets/launcher.js
+++ exercises/follow_line/web-template/assets/launcher.js
@@ -2,7 +2,7 @@ ws_manager = new WebSocket("ws://" + websocket_address + ":8765/");
 exercise = "follow_line"
 
 ws_manager.onopen = function(event){
+    alert("Connected!")
     ws_manager.send(JSON.stringify({"command": "exit", "exercise": ""}));
+    ws_manager.send(JSON.stringify({"command": "open", "exercise": exercise}));
 }
-
-ws_manager.send(JSON.stringify({"command": "open", "exercise": exercise}));
