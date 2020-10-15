eefee7b8f0ae6fa39f8661a43e2c60f0c18a4f0b
SakshayMahna <sakshum19@gmail.com>
2020-10-15 10:09:51 +0530

    Follow Line automated

--- exercises/follow_line/web-template/assets/ws_gui.js
+++ exercises/follow_line/web-template/assets/ws_gui.js
@@ -8,26 +8,80 @@ function decode_utf8(s){
 }
 
 // Websocket and other variables for image display
-var websocket_gui = new WebSocket("ws://" + websocket_address + ":2303/"),
-    canvas = document.getElementById("gui_canvas"),
+var websocket_gui;
+function declare_gui(){
+	websocket_gui = new WebSocket("ws://" + websocket_address + ":2303/");
+
+	websocket_gui.onopen = function(event){
+		alert("[open] Connection established!");
+	}
+	
+	websocket_gui.onclose = function(event){
+		if(event.wasClean){
+			alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+		}
+		else{
+			alert("[close] Connection closed!");
+		}
+	}
+
+	// What to do when a message from server is received
+	websocket_gui.onmessage = function(event){
+		var operation = event.data.substring(0, 4);
+
+		if(operation == "#img"){
+			var data = JSON.parse(event.data.substring(4, )),
+				source = decode_utf8(data.image),
+				shape = data.shape;
+
+			canvas.width = shape[1];
+			canvas.height = shape[0];
+
+			image.src = "data:image/jpeg;base64," + source;
+
+			websocket_gui.send("Image Displayed!")
+		}
+		
+		else if(operation == "#lap"){
+			var lap_time = event.data.substring(4, );
+			lap_time_display.textContent = lap_time;
+		}
+		
+		else if(operation == "#map"){
+			// To slice off the ()
+			var pose = event.data.substring(5, event.data.length - 1);
+			var content = pose.split(',').map(function(item) {
+				return parseFloat(item);
+			})
+			drawCircle(content[0], content[1]);
+		}
+		
+		else if(operation == "#cop"){
+			// Set the value of command
+			var command_input = event.data.substring(4, );
+			command.value = command_input;
+			// Go to next command line
+			next_command();
+		}
+		
+		else if(operation == "#cor"){
+			// Set the value of command
+			var command_input = event.data.substring(4, );
+			command.value = command_input;
+			// Go to next command line
+			next_command();
+			// Focus on the next line
+			command.focus();
+		}
+	}
+}
+
+var canvas = document.getElementById("gui_canvas"),
     context = canvas.getContext('2d');
     image = new Image();
     
 // Lap time DOM
 var lap_time_display = document.getElementById("lap_time");
-
-websocket_gui.onopen = function(event){
-    alert("[open] Connection established!");
-}
-
-websocket_gui.onclose = function(event){
-    if(event.wasClean){
-        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    }
-    else{
-        alert("[close] Connection closed!");
-    }
-}
 
 // For image object
 image.onload = function(){
@@ -39,54 +93,3 @@ function update_image(){
 	window.requestAnimationFrame(update_image);
 	context.drawImage(image, 0, 0);
 }
-
-// What to do when a message from server is received
-websocket_gui.onmessage = function(event){
-    var operation = event.data.substring(0, 4);
-
-    if(operation == "#img"){
-		var data = JSON.parse(event.data.substring(4, )),
-		 	source = decode_utf8(data.image),
-		    shape = data.shape;
-
-		canvas.width = shape[1];
-		canvas.height = shape[0];
-
-		image.src = "data:image/jpeg;base64," + source;
-
-		websocket_gui.send("Image Displayed!")
-	}
-	
-	else if(operation == "#lap"){
-		var lap_time = event.data.substring(4, );
-		lap_time_display.textContent = lap_time;
-	}
-	
-	else if(operation == "#map"){
-		// To slice off the ()
-		var pose = event.data.substring(5, event.data.length - 1);
-		var content = pose.split(',').map(function(item) {
-			return parseFloat(item);
-		})
-		console.log(content)
-		drawCircle(content[0], content[1]);
-	}
-	
-	else if(operation == "#cop"){
-		// Set the value of command
-		var command_input = event.data.substring(4, );
-		command.value = command_input;
-		// Go to next command line
-		next_command();
-	}
-	
-	else if(operation == "#cor"){
-		// Set the value of command
-		var command_input = event.data.substring(4, );
-		command.value = command_input;
-		// Go to next command line
-		next_command();
-		// Focus on the next line
-		command.focus();
-	}
-};
