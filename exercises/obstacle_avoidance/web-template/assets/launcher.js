26130f3167b613764105fd634dbb844e369af779
SakshayMahna <sakshum19@gmail.com>
2020-10-15 13:13:36 +0530

    Obstacle Avoidance Automated

--- exercises/obstacle_avoidance/web-template/assets/launcher.js
+++ exercises/obstacle_avoidance/web-template/assets/launcher.js
@@ -3,6 +3,12 @@ exercise = "obstacle_avoidance"
 
 ws_manager.onopen = function(event){
     ws_manager.send(JSON.stringify({"command": "exit", "exercise": ""}));
+    ws_manager.send(JSON.stringify({"command": "open", "exercise": exercise}));
 }
 
-ws_manager.send(JSON.stringify({"command": "open", "exercise": exercise}));
+ws_manager.onmessage = function(event){
+    if(event.data == "OPENED"){
+        declare_code();
+        declare_gui();
+    }
+}
