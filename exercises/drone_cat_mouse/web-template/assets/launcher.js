b58937dc78459dcb2005132972a6b2622eccac76
pariaspe <pedroariasperez96@gmail.com>
2021-02-01 16:47:12 +0100

    Drones templates first version

---
+++ exercises/drone_cat_mouse/web-template/assets/launcher.js
@@ -0,0 +1,16 @@
+ws_manager = new WebSocket("ws://" + websocket_address + ":8765/");
+exercise = "follow_line"
+
+ws_manager.onopen = function(event){
+    ws_manager.send(JSON.stringify({"command": "exit", "exercise": ""}));
+    ws_manager.send(JSON.stringify({"command": "open", "exercise": exercise}));
+}
+
+ws_manager.onmessage = function(event){
+    console.log(event.data);
+}
+
+setTimeout(function(){
+    declare_code(websocket_address);
+    declare_gui(websocket_address);
+}, 10000);
