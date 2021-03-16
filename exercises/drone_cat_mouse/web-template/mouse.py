fe3d05e9433ef7c7f0e56e70376e7abe40398889
pariaspe <pedroariasperez96@gmail.com>
2021-03-16 18:30:14 +0100

    Moved mouse cmds to thread

--- exercises/drone_cat_mouse/web-template/mouse.py
+++ exercises/drone_cat_mouse/web-template/mouse.py
@@ -1,6 +1,7 @@
-import rospy
+import threading
 
 from drone_wrapper import DroneWrapper
+from interfaces.autonomous_mouse import path_obfuscated
 
 
 class Mouse:
@@ -17,11 +18,15 @@ class Mouse:
     def start_mouse(self, path_level):
         self.mouse.takeoff(h=5)
 
-        # while True:
-        #     v = path_obfuscated(path_level)
-        #     if v is not None:
-        #         vx, vy, vz, yaw = v
-        #         self.mouse.set_cmd_vel(vx, vy, vz, yaw)
-        #     else:
-        #         print("[Mouse] Path {} not available".format(path_level))
-        #         break
+        self.thread = threading.Thread(target=self.autonomous_mouse, args=[path_level])
+        self.thread.start()
+
+    def autonomous_mouse(self, path_level):
+        while True:
+            v = path_obfuscated(path_level)
+            if v is not None:
+                vx, vy, vz, yaw = v
+                self.mouse.set_cmd_vel(vx, vy, vz, yaw)
+            else:
+                print("[Mouse] Path {} not available".format(path_level))
+                break
