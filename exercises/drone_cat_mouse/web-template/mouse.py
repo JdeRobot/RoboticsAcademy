8bd4a35ff688587d4df92e38739fb53961c0dbad
pariaspe <pedroariasperez96@gmail.com>
2021-03-28 13:42:15 +0200

    Fixing some bugs on pause and reset btns (#786)

--- exercises/drone_cat_mouse/web-template/mouse.py
+++ exercises/drone_cat_mouse/web-template/mouse.py
@@ -11,6 +11,7 @@ from gazebo_msgs.msg import ModelState
 class Mouse:
     def __init__(self):
         self.mouse = DroneWrapper(name="rqt", ns="mouse/")
+        self.reset_state = rospy.ServiceProxy('/gazebo/set_model_state', GetModelState)
 
     # Explicit initialization functions
     # Class method, so user can call it without instantiation
@@ -32,27 +33,24 @@ class Mouse:
 
     def stop_mouse(self):
         self.thread.stop()
-        self.mouse.set_cmd_mix(z=self.mouse.get_position()[2])
+        self.mouse.set_cmd_mix(vx=0, vy=0, z=self.mouse.get_position()[2], yaw_rate=0)
 
     def reset_mouse(self):
         self.thread.stop()
         self.mouse.land()
 
-        reset_state = rospy.ServiceProxy('/gazebo/set_model_state', GetModelState)
         req = ModelState()
         req.model_name = "iris_red"
         req.pose.position = [0.0, 0.0, 0.05]
         req.pose.orientation = [0.0, 2.0, 0.0, 1.0]
         req.twist.linear = [0.0, 0.0, 0.0]
         req.twist.angular = [0.0, 0.0, 0.0]
-        reset_state(req)
+        self.reset_state(req)
 
     def autonomous_mouse(self, path_level):
-        while True:
-            v = path_obfuscated(path_level)
-            if v is not None:
-                vx, vy, vz, yaw = v
-                self.mouse.set_cmd_vel(vx, vy, vz, yaw)
-            else:
-                print("[Mouse] Path {} not available".format(path_level))
-                break
+        v = path_obfuscated(path_level)
+        if v is not None:
+            vx, vy, vz, yaw = v
+            self.mouse.set_cmd_vel(vx, vy, vz, yaw)
+        else:
+            print("[Mouse] Path {} not available".format(path_level))
