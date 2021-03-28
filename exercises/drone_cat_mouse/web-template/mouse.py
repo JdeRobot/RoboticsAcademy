095726037631955d8892282fc2bb69137ac82ae5
pariaspe <pedroariasperez96@gmail.com>
2021-03-28 14:10:12 +0200

    new bug on reset btn fixed (#786)

--- exercises/drone_cat_mouse/web-template/mouse.py
+++ exercises/drone_cat_mouse/web-template/mouse.py
@@ -4,14 +4,14 @@ import rospy
 from drone_wrapper import DroneWrapper
 from interfaces.autonomous_mouse import path_obfuscated
 from interfaces.threadStoppable import StoppableThread
-from gazebo_msgs.srv import GetModelState
+from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.msg import ModelState
 
 
 class Mouse:
     def __init__(self):
         self.mouse = DroneWrapper(name="rqt", ns="mouse/")
-        self.reset_state = rospy.ServiceProxy('/gazebo/set_model_state', GetModelState)
+        self.reset_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
     # Explicit initialization functions
     # Class method, so user can call it without instantiation
@@ -41,10 +41,19 @@ class Mouse:
 
         req = ModelState()
         req.model_name = "iris_red"
-        req.pose.position = [0.0, 0.0, 0.05]
-        req.pose.orientation = [0.0, 2.0, 0.0, 1.0]
-        req.twist.linear = [0.0, 0.0, 0.0]
-        req.twist.angular = [0.0, 0.0, 0.0]
+        req.pose.position.x = 2.0
+        req.pose.position.y = 0.0
+        req.pose.position.z = 0.05
+        req.pose.orientation.x = 0.0
+        req.pose.orientation.y = 0.0
+        req.pose.orientation.z = 0.0
+        req.pose.orientation.w = 1.0
+        req.twist.linear.x = 0.0
+        req.twist.linear.y = 0.0
+        req.twist.linear.z = 0.0
+        req.twist.angular.x = 0.0
+        req.twist.angular.y = 0.0
+        req.twist.angular.z = 0.0
         self.reset_state(req)
 
     def autonomous_mouse(self, path_level):
