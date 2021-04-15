0bc3397f2b3ee66b81cfd2eab27023185b81a164
Arka <abasak_be18@thapar.edu>
2021-04-15 10:45:45 +0530

    fixed auto start of turtlebot

--- exercises/follow_turtlebot/web-template/turtlebot.py
+++ exercises/follow_turtlebot/web-template/turtlebot.py
@@ -1,18 +1,19 @@
-import threading
-from threading import Event
-import rospy
 import sys
+import rospy
+from threading import Event
 
-from interfaces.threadStoppable import StoppableThread
+from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import GetModelState
 from gazebo_msgs.srv import SetModelState
-from gazebo_msgs.msg import ModelState
+from interfaces.threadStoppable import StoppableThread
 
 class Turtlebot():
     def __init__(self):
         self.set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
         self.get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-        self.play = Event()
+        self.play_event = Event()
+        rospy.sleep(2)
+        self.stop_turtlebot()
 
     # Explicit initialization functions
     # Class method, so user can call it without instantiation
@@ -23,19 +24,18 @@ class Turtlebot():
 
     def start_turtlebot(self):
         try:
-            self.play.set() # play=True
-            # wait for stop_turtlebot_thread to close
+            self.play_event.set()
             rospy.sleep(0.5)
             self.thread.join()
         except:
             print("Thread not yet started")
 
     def stop_turtlebot(self):
-        self.play.clear() # play=False
-        self.thread = StoppableThread(name="stop_turtlebot_thread", target=self.halt_turtlebot, args=[])
+        self.play_event.clear()
+        self.thread = StoppableThread(target=self.__stop__, args=[])
         self.thread.start()
 
-    def halt_turtlebot(self):
+    def __stop__(self):
         rec = self.get_state("turtlebot3", "base_link")
         req = ModelState()
         req.model_name = "turtlebot3"
@@ -53,14 +53,13 @@ class Turtlebot():
         req.pose.orientation.z = rec.pose.orientation.z
         req.pose.orientation.w = 1.0
         while True:
-            if self.play.is_set():
-                sys.exit() # kill stop_turtlebot_thread if play==True
+            if self.play_event.is_set():
+                sys.exit() # kill stop_turtlebot thread
             else:
                 self.set_state(req)
 
     def reset_turtlebot(self):
-        self.start_turtlebot() # start turtlebot first
-
+        self.start_turtlebot()
         req = ModelState()
         req.model_name = "turtlebot3"
         req.twist.linear.x = 0.0
@@ -70,13 +69,14 @@ class Turtlebot():
         req.twist.angular.y = 0.0
         req.twist.angular.z = 0.0
         req.pose.position.x = 0.0
-        req.pose.position.y = 3.0
+        req.pose.position.y = 0.0
         req.pose.position.z = 0.0
         req.pose.orientation.x = 0.0
         req.pose.orientation.y = 0.0
         req.pose.orientation.z = 0.0
         req.pose.orientation.w = 0.0
         self.set_state(req)
+        self.stop_turtlebot()
 
 
 
