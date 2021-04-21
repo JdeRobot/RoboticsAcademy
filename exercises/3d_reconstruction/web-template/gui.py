f4f79c766d14f47c5c932509ff8035bdde4ca3a6
dvalladaresv <d.valladaresvigara@gmail.com>
2021-04-21 13:16:17 +0200

    Update camera.py

--- exercises/3d_reconstruction/web-template/gui.py
+++ exercises/3d_reconstruction/web-template/gui.py
@@ -57,9 +57,6 @@ class GUI:
             return payload
 
         shape = image.shape
-        if image.ndim == 3:
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
         frame = cv2.imencode('.JPEG', image)[1]
         encoded_image = base64.b64encode(frame)
 
