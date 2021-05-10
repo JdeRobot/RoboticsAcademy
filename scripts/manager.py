41d3ee9b923c8e9d4e0a8c3255f461da79832f24
iamarkaj <abasak_be18@thapar.edu>
2021-05-10 05:49:41 +0530

    drone-cat-mouse: change paths

--- scripts/manager.py
+++ scripts/manager.py
@@ -40,9 +40,9 @@ instructions = {
         "instructions_host": "python /RoboticsAcademy/exercises/color_filter/web-template/exercise.py 0.0.0.0"
     },
     "drone_cat_mouse": {
-        "gazebo_path": "/RoboticsAcademy/exercises/drone_cat_mouse/web-template/launch",
-        "instructions_ros": ["/opt/ros/melodic/bin/roslaunch ./RoboticsAcademy/exercises/drone_cat_mouse/web-template/launch/drone_cat_mouse.launch"],
-        "instructions_host": "python /RoboticsAcademy/exercises/drone_cat_mouse/web-template/exercise.py 0.0.0.0"
+        "gazebo_path": "/RoboticsAcademy/exercises/static/exercises/drone_cat_mouse/web-template/launch",
+        "instructions_ros": ["/opt/ros/melodic/bin/roslaunch ./RoboticsAcademy/exercises/static/exercises/drone_cat_mouse/web-template/launch/drone_cat_mouse.launch"],
+        "instructions_host": "python /RoboticsAcademy/exercises/static/exercises/drone_cat_mouse/web-template/exercise.py 0.0.0.0"
     },
     "3dreconstruction": {
         "gazebo_path": "/RoboticsAcademy/exercises/3d_reconstruction/web-template/launch",
