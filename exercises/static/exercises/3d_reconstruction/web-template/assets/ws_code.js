615a9606fd4fd9143b85f5798d1c85da7059f764
ReyDoran <manetoscopio@gmail.com>
2021-05-10 10:20:00 +0200

    submitCode function error handling

--- exercises/static/exercises/3d_reconstruction/web-template/assets/ws_code.js
+++ exercises/static/exercises/3d_reconstruction/web-template/assets/ws_code.js
@@ -55,18 +55,23 @@ function declare_code(websocket_address){
 // Function that sends/submits the code!
 // Function that sends/submits the code!
 function submitCode(){
-	// Get the code from editor and add headers
-    var python_code = editor.getValue();
-    python_code = "#code\n" + python_code
+    try {
+        // Get the code from editor and add headers
+        var python_code = editor.getValue();
+        python_code = "#code\n" + python_code
 
-    console.log("Code Sent! Check terminal for more information!");
-    websocket_code.send(python_code);
+        websocket_code.send(python_code);
+        console.log("Code Sent! Check terminal for more information!");
 
-    stop_button.disabled = false;
-    stop_button.style.opacity = "1.0";
-	stop_button.style.cursor = "default";
+        stop_button.disabled = false;
+        stop_button.style.opacity = "1.0";
+        stop_button.style.cursor = "default";
 
-	running = true;
+        running = true;
+    }
+	catch {
+		alert("Connection must be established before sending the code.")
+	}
 }
 
 // Function that send/submits an empty string
