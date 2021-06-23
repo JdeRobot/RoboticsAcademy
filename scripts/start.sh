0719a6a06fa2cedb5c3c5092fa85e2b14826e6cb
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-23 14:21:31 +0530

    Included radi-entrypoint in start.sh script; Updated its shebang to a logical path instead of the earlier absolute path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# This file is intended to be run from the root
+# of the Docker container
+
+
+# radi-entrypoint gets populated solely through
+# the Dockerfile.
+# Its primary purpose is to source the ROS setup files.
+source radi-entrypoint.sh
+
+# Start the Django server. Load the list of exercises
+# from db.sqlite3
 python3 RoboticsAcademy/manage.py runserver 0.0.0.0:8000 &
+
+# Start RADI
 python3.8 manager.py
