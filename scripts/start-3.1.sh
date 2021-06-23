2fb4667c1d0079bc89fcd0ca1c086887fff20a8a
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-23 14:23:02 +0530

    Deleted outdated manager.py (current is manager-4.0.py) and script files

--- scripts/start-3.1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-source /catkin_ws/devel/setup.bash
-python3 RoboticsAcademy/manage.py runserver 0.0.0.0:8000 &
-python3.8 manager-3.1.py
--- scripts/start_manager.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-python3.8 manager-3.1.py
