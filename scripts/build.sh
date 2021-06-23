cee5b620ca30ed1121f22ef3ef22df8f03c37f3d
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-23 14:35:40 +0530

    Updated build.sh to build Dockerfile-foxy (added run commands for testing)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker build -f Dockerfile-noetic-3.1 -t jderobot/robotics-academy:3.1 .
+docker build -f Dockerfile-foxy -t foxy-radi . && \
+docker run -it \
+      --rm \
+      -v /tmp/.X11-unix:/tmp/.X11-unix \
+      --name foxy_radi_container \
+      -v $PWD/../:/RoboticsAcademy \
+      -p 8000:8000 -p 2303:2303 -p 1905:1905 -p 8765:8765 -p 6080:6080 -p 1108:1108 \
+      foxy-radi ./start.sh
