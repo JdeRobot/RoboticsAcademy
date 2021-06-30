b31e29c4d81e15746c1540572142a9dd6286bb2d
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-30 11:39:18 +0530

    radi-entrypoint.sh sources amazon_warehouse packages inside Custom Robots

--- scripts/radi-entrypoint.sh
+++ scripts/radi-entrypoint.sh
@@ -3,10 +3,14 @@ source /opt/ros/$ROS_DISTRO/local_setup.bash
 source /usr/share/gazebo-11/setup.sh
 export TURTLEBOT3_MODEL=waffle
 
-# Convenience variable used inside GAZEBO_MODEL_PATH
-export AMAZON_ROBOT_PATH=/opt/jderobot/CustomRobots/amazon_robot
+# For Amazon Warehouse exercise
+source /opt/jderobot/CustomRobots/install/setup.sh
 
+# Convenience variable used to simplify GAZEBO_MODEL_PATH
+export AMAZON_ROBOT_PATH=/opt/jderobot/CustomRobots/amazon_robot
+# Make all Gazebo models accessible
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${AMAZON_ROBOT_PATH}/amazon_robot_gazebo/models:${AMAZON_ROBOT_PATH}/aws-robomaker-small-warehouse-world/models
+
 
 
 # Picked these from Noetic Dockerfile. Keep whatever's relevant after testing
