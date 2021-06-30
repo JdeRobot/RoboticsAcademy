75b18fd675a466871e4ec9b0ab438de85891e024
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-30 18:32:16 +0530

    Minor updates to files in scripts/ directory to make it PR-ready

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile-foxy.base -t foxy-radi-base . > build_log && \
-docker build -f Dockerfile-foxy -t foxy-radi . && \
-### TODO: Add --no-cache=true in above step
-docker run -it \
-      --rm \
-      -v /tmp/.X11-unix:/tmp/.X11-unix \
-      --name foxy_radi_container \
-      -v $PWD/../:/RoboticsAcademy \
-      -p 8000:8000 -p 2303:2303 -p 1905:1905 -p 8765:8765 -p 6080:6080 -p 1108:1108 \
-      foxy-radi ./start.sh
+docker build -f Dockerfile-foxy.base -t foxy-radi-base . && \
+docker build --no-cache=true -f Dockerfile-foxy -t foxy-radi .
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,13 +4,10 @@
 # of the Docker container
 
 
-# radi-entrypoint gets populated solely through
-# the Dockerfile.
-# Its primary purpose is to source the ROS setup files.
+# radi-entrypoint's primary purpose is to source the ROS setup files.
 source radi-entrypoint.sh
 
-# Start the Django server. Load the list of exercises
-# from db.sqlite3
+# Start the Django server. Load the list of exercises from db.sqlite3
 python3 RoboticsAcademy/manage.py runserver 0.0.0.0:8000 &
 
 # Start RADI
