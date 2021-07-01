d8ad8d18525c47ea0f4c60123328ffac4d89aa7a
Siddharth Saha <sahasiddharth611@gmail.com>
2021-07-01 19:11:53 +0530

    Cleared all the To-do's in Dockerfiles (which depends on PR already being merged); Updated build.sh with JdeRobot's naming conventions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile-foxy.base -t foxy-radi-base . && \
-docker build --no-cache=true -f Dockerfile-foxy -t foxy-radi .
+docker build -f Dockerfile-foxy.base -t jderobot/base:4.0.0 . && \
+docker build --no-cache=true -f Dockerfile-foxy -t jderobot/robotics-academy:4.0.0 .
