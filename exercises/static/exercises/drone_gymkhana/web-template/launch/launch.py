231d7847bf6b2029b179fd6825913620055c0e60
iamarkaj <abasak_be18@thapar.edu>
2021-07-06 17:41:58 +0530

    changed the log names

--- exercises/static/exercises/drone_gymkhana/web-template/launch/launch.py
+++ exercises/static/exercises/drone_gymkhana/web-template/launch/launch.py
@@ -51,7 +51,7 @@ class Launch(Tests):
         if insert_roslaunch: args.insert(0, "/opt/ros/noetic/bin/roslaunch")
         if insert_vglrun and ACCELERATION_ENABLED: args.insert(0, "vglrun")
         if len(sys.argv)>=2 and sys.argv[1] == "log":
-            with open("/logs/launch_logs.txt", "a+") as f:
+            with open("/logs/launch.log", "a+") as f:
                 subprocess.Popen(args, stdout=f, bufsize=4096, universal_newlines=True)
         else:
             subprocess.Popen(args, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
--- scripts/start_logs.sh
+++ scripts/start_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /logs
-script -q -c "./start-3.1.sh log" /logs/stderr_logs.txt
+script -q -c "./start-3.1.sh log" /logs/stderr.log
