cedfc167470f287fadcd6e62e291fda14aa409f5
ReyDoran <manetoscopio@gmail.com>
2021-07-15 09:45:22 +0200

    stopCode function call restored

--- exercises/static/exercises/follow_line/web-template/assets/controller.js
+++ exercises/static/exercises/follow_line/web-template/assets/controller.js
@@ -17,6 +17,9 @@ function start(){
 
 // Function to stop the student solution
 function stop(){
+    // Code Websocket
+    stopCode();
+    
     // Manager Websocket
     if (running == true) {
         stopSimulation();
@@ -28,6 +31,9 @@ function stop(){
 
 // Function to reset the simulation
 function reset(){
+    // Code Websocket
+    stopCode();
+
     // Manager Websocket
     resetSimulation();
 
