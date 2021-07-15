0ae51ef3bfa9b56979ff306322da8cf234f957e1
iamarkaj <abasak_be18@thapar.edu>
2021-07-15 17:20:57 +0530

    fix other bugs

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@
 source /opt/ros/noetic/setup.bash
 source /catkin_ws/devel/setup.bash
 python3 RoboticsAcademy/manage.py runserver 0.0.0.0:8000 &
-python3.8 manager-3.1.py $1
+python3.8 manager.py $1
--- scripts/start_logs.sh
+++ scripts/start_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /logs
-script -q -c "./start-3.1.sh log" /logs/stderr.log
+script -q -c "./start.sh log" /logs/stderr.log
