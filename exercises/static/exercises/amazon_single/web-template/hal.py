b172e11ffeac166faa22c2fa8b6e9ae1c1c9233a
Siddharth Saha <sahasiddharth611@gmail.com>
2021-07-22 02:27:49 +0530

    Added amazon_single exercise template files based on tb3_nav exercise

---
+++ exercises/static/exercises/amazon_single/web-template/hal.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+class HAL:
+
+    # def __init__(self):
+    #     self.cameraCapture = cv2.VideoCapture(0)
+
+    # def getImage(self):
+    #     success, frame = self.cameraCapture.read()
+    #     return frame
+
+    pass
