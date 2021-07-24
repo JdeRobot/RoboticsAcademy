6b8bcbf4d2e713b70104ff542a1fb81e04e9168a
predator4hack <chandankum2440@gmail.com>
2021-07-24 19:54:57 +0530

    Added commands to install eband local planner

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,7 @@ rm -rf instructions.json
 cp /RoboticsAcademy/scripts/instructions.json /instructions.json
 sudo apt-get update
 sudo apt-get install ros-melodic-navigation -y
+sudo apt-get install ros-melodic-eband-local-planner
 source /opt/ros/melodic/setup.bash
 source /catkin_ws/devel/setup.bash
 python3 RoboticsAcademy/manage.py runserver 0.0.0.0:8000 &
