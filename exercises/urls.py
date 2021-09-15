2d8d3b00bffd52144b416c70a9534742fc9a9292
Daniel Hervás Rodao <danielhervasrodao@gmail.com>
2021-09-15 20:00:21 +0200

    added webserver view to return mouse path

--- exercises/urls.py
+++ exercises/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("exercises/<slug:exercise_id>/", views.load_exercise, name="load_exercise"),
+    path("exercise/request/<slug:exercise_id>", views.request_code, name="request_code"),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Exercise
+from django.http import HttpResponse
+from django.conf import settings
+import os
 
 # Create your views here.
 def index(request):
@@ -10,3 +13,17 @@ def index(request):
 def load_exercise(request, exercise_id):
 	context = { "exercise": str(exercise_id) }
 	return render(request, 'exercises/' + exercise_id + '/exercise.html', context)
+	
+def request_code(request, exercise_id):
+    difficulty = request.GET.get('diff')
+    path = '/exercises/static/exercises/{}/web-template/assets/{}.py'.format(exercise_id,difficulty)
+    path = str(settings.BASE_DIR)+path
+    print('PATH: ',path)
+    with open(path) as f:
+    	data = f.read().replace('\\n', '\n')
+    	
+    print(data)
+
+    if difficulty != None:
+        print('EXERCISE: ', exercise_id, 'DIFFICULTY: ', difficulty)
+        return HttpResponse(data, content_type="text/plain")
