5eb42bf14f6ed743f3b5307666bca1b0b694decc
ReyDoran <manetoscopio@gmail.com>
2021-10-18 11:41:13 +0200

    Delay in reloading brain on Drone exercises

--- exercises/static/exercises/drone_cat_mouse/web-template/exercise.py
+++ exercises/static/exercises/drone_cat_mouse/web-template/exercise.py
@@ -25,6 +25,7 @@ class Template:
     # self.time_cycle to run an execution for at least 1 second
     # self.process for the current running process
     def __init__(self):
+        self.measure_thread = None
         self.thread = None
         self.reload = False
 
@@ -165,7 +166,7 @@ class Template:
     def measure_frequency(self):
         previous_time = datetime.now()
         # An infinite loop
-        while not self.reload:
+        while True:
             # Sleep for 2 seconds
             time.sleep(2)
 
@@ -236,16 +237,14 @@ class Template:
         # Keep checking until the thread is alive
         # The thread will die when the coming iteration reads the flag
         if self.thread is not None:
-            while self.thread.is_alive() or self.measure_thread.is_alive():
-                pass
+            while self.thread.is_alive():
+                time.sleep(0.2)
 
         # Turn the flag down, the iteration has successfully stopped!
         self.reload = False
         # New thread execution
-        self.measure_thread = threading.Thread(target=self.measure_frequency)
         self.thread = threading.Thread(target=self.process_code, args=[source_code])
         self.thread.start()
-        self.measure_thread.start()
         self.send_code_message()
         print("New Thread Started!")
 
@@ -270,7 +269,6 @@ class Template:
             frequency_message = message[5:]
             self.read_frequency_message(frequency_message)
             time.sleep(1)
-            self.send_frequency_message()
             return
 
         elif(message[:5] == "#ping"):
@@ -298,8 +296,9 @@ class Template:
         self.stats_thread = threading.Thread(target=self.track_stats)
         self.stats_thread.start()
 
-        # Initialize the ping message
-        self.send_frequency_message()
+        # Start measure frequency
+        self.measure_thread = threading.Thread(target=self.measure_frequency)
+        self.measure_thread.start()
 
         print(client, 'connected')
 
