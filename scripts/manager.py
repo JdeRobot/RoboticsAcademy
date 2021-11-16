89f8b94de6da4e55cc64c50c51d7425585b66731
ReyDoran <manetoscopio@gmail.com>
2021-11-16 17:23:09 +0100

    Lowered timeout value

--- scripts/manager.py
+++ scripts/manager.py
@@ -324,7 +324,7 @@ class Commands:
         # Wait disarming
         cmd_wait = ["rostopic", "echo", "--filter", "'m.armed==False'", "/mavros/state", "-n", "1"]
         try:
-            subprocess.call(cmd_wait, stdout=subprocess.PIPE, bufsize=1024, universal_newlines=True, timeout=10)
+            subprocess.call(cmd_wait, stdout=subprocess.PIPE, bufsize=1024, universal_newlines=True, timeout=5)
         except Exception:
             print("Timeout reached")
             pass
