214e105dc79f83311ae192a199f607f89acdc52e
Danielin <sowerdani@gmail.com>
2022-04-05 14:14:31 +0200

    laser_loc_2

---
+++ static/exercises/laser_loc_2/js/controller.js
@@ -0,0 +1,46 @@
+// Main message controller for websockets
+var running = false;
+
+// Function to execute the student solution
+function start(){
+    // Manager Websocket
+    if (running == false) {
+        resumeSimulation();
+    }
+
+    // Code Websocket
+    submitCode();
+    deactivateTeleop();
+}
+
+function check() {
+    checkCode();
+}
+
+// Function to stop the student solution
+function stop(){
+    // Code Websocket
+    stopCode();
+
+    // Manager Websocket
+    if (running == true) {
+        stopSimulation();
+    }
+}
+
+// Function to reset the simulation
+function resetSim(){
+    // Code Websocket
+    stopCode();
+
+    // Manager Websocket
+    resetSimulation();
+
+    // Code Websocket
+    if (running == true) {
+        submitCode();
+    }
+
+    // GUI
+    clearMap();
+}
