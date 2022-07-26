9e942e54458b7fde97f3aba2cffe13227b8d056c
RUFFY-369 <prakarshkaushik369@gmail.com>
2022-07-27 00:45:47 +0530

    add new bigger drone model as  crazyfile doesn't size well,adding rotors driver, getting back HAL & GUI

--- exercises/static/exercises/drone_cat_mouse/web-template/hal.py
+++ exercises/static/exercises/drone_cat_mouse/web-template/hal.py
@@ -13,7 +13,7 @@ class HAL:
         rospy.init_node("HAL_cat")
 
         self.image = None
-        # self.cat = DroneWrapper(name="rqt", ns="cat/")
+        self.cat = DroneWrapper(name="rqt", ns="cat/")
 
     # Explicit initialization functions
     # Class method, so user can call it without instantiation
@@ -23,59 +23,59 @@ class HAL:
         return new_instance
 
     # Get Image from ROS Driver Camera
-    # def get_frontal_image(self):
-    #     image = self.cat.get_frontal_image()
-    #     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #     return image_rgb
+    def get_frontal_image(self):
+        image = self.cat.get_frontal_image()
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image_rgb
 
-    # def get_ventral_image(self):
-    #     image = self.cat.get_ventral_image()
-    #     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #     return image_rgb
+    def get_ventral_image(self):
+        image = self.cat.get_ventral_image()
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image_rgb
 
-    # def get_position(self):
-    #     pos = self.cat.get_position()
-    #     return pos
+    def get_position(self):
+        pos = self.cat.get_position()
+        return pos
 
-    # def get_velocity(self):
-    #     vel = self.cat.get_velocity()
-    #     return vel
+    def get_velocity(self):
+        vel = self.cat.get_velocity()
+        return vel
 
-    # def get_yaw_rate(self):
-    #     yaw_rate = self.cat.get_yaw_rate()
-    #     return yaw_rate
+    def get_yaw_rate(self):
+        yaw_rate = self.cat.get_yaw_rate()
+        return yaw_rate
 
-    # def get_orientation(self):
-    #     orientation = self.cat.get_orientation()
-    #     return orientation
+    def get_orientation(self):
+        orientation = self.cat.get_orientation()
+        return orientation
 
-    # def get_roll(self):
-    #     roll = self.cat.get_roll()
-    #     return roll
+    def get_roll(self):
+        roll = self.cat.get_roll()
+        return roll
 
-    # def get_pitch(self):
-    #     pitch = self.cat.get_pitch()
-    #     return pitch
+    def get_pitch(self):
+        pitch = self.cat.get_pitch()
+        return pitch
 
-    # def get_yaw(self):
-    #     yaw = self.cat.get_yaw()
-    #     return yaw
+    def get_yaw(self):
+        yaw = self.cat.get_yaw()
+        return yaw
 
-    # def get_landed_state(self):
-    #     state = self.cat.get_landed_state()
-    #     return state
+    def get_landed_state(self):
+        state = self.cat.get_landed_state()
+        return state
 
-    # def set_cmd_pos(self, x, y, z, az):
-    #     self.cat.set_cmd_pos(x, y, z, az)
+    def set_cmd_pos(self, x, y, z, az):
+        self.cat.set_cmd_pos(x, y, z, az)
 
-    # def set_cmd_vel(self, vx, vy, vz, az):
-    #     self.cat.set_cmd_vel(vx, vy, vz, az)
+    def set_cmd_vel(self, vx, vy, vz, az):
+        self.cat.set_cmd_vel(vx, vy, vz, az)
 
-    # def set_cmd_mix(self, vx, vy, z, az):
-    #     self.cat.set_cmd_mix(vx, vy, z, az)
+    def set_cmd_mix(self, vx, vy, z, az):
+        self.cat.set_cmd_mix(vx, vy, z, az)
 
-    # def takeoff(self, h=3):
-    #     self.cat.takeoff(h)
+    def takeoff(self, h=3):
+        self.cat.takeoff(h)
 
-    # def land(self):
-    #     self.cat.land()
+    def land(self):
+        self.cat.land()
--- exercises/static/exercises/drone_cat_mouse/web-template/hal_guest.py
+++ exercises/static/exercises/drone_cat_mouse/web-template/hal_guest.py
@@ -13,7 +13,7 @@ class HAL:
         rospy.init_node("HAL_mouse")
 
         self.image = None
-        # self.mouse = DroneWrapper(name="rqt", ns="mouse/")
+        self.mouse = DroneWrapper(name="rqt", ns="mouse/")
 
     # Explicit initialization functions
     # Class method, so user can call it without instantiation
@@ -23,59 +23,59 @@ class HAL:
         return new_instance
 
     # Get Image from ROS Driver Camera
-    # def get_frontal_image(self):
-    #     image = self.mouse.get_frontal_image()
-    #     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #     return image_rgb
+    def get_frontal_image(self):
+        image = self.mouse.get_frontal_image()
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image_rgb
 
-    # def get_ventral_image(self):
-    #     image = self.mouse.get_ventral_image()
-    #     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #     return image_rgb
+    def get_ventral_image(self):
+        image = self.mouse.get_ventral_image()
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image_rgb
 
-    # def get_position(self):
-    #     pos = self.mouse.get_position()
-    #     return pos
+    def get_position(self):
+        pos = self.mouse.get_position()
+        return pos
 
-    # def get_velocity(self):
-    #     vel = self.mouse.get_velocity()
-    #     return vel
+    def get_velocity(self):
+        vel = self.mouse.get_velocity()
+        return vel
 
-    # def get_yaw_rate(self):
-    #     yaw_rate = self.mouse.get_yaw_rate()
-    #     return yaw_rate
+    def get_yaw_rate(self):
+        yaw_rate = self.mouse.get_yaw_rate()
+        return yaw_rate
 
-    # def get_orientation(self):
-    #     orientation = self.mouse.get_orientation()
-    #     return orientation
+    def get_orientation(self):
+        orientation = self.mouse.get_orientation()
+        return orientation
 
-    # def get_roll(self):
-    #     roll = self.mouse.get_roll()
-    #     return roll
+    def get_roll(self):
+        roll = self.mouse.get_roll()
+        return roll
 
-    # def get_pitch(self):
-    #     pitch = self.mouse.get_pitch()
-    #     return pitch
+    def get_pitch(self):
+        pitch = self.mouse.get_pitch()
+        return pitch
 
-    # def get_yaw(self):
-    #     yaw = self.mouse.get_yaw()
-    #     return yaw
+    def get_yaw(self):
+        yaw = self.mouse.get_yaw()
+        return yaw
 
-    # def get_landed_state(self):
-    #     state = self.mouse.get_landed_state()
-    #     return state
+    def get_landed_state(self):
+        state = self.mouse.get_landed_state()
+        return state
 
-    # def set_cmd_pos(self, x, y, z, az):
-    #     self.mouse.set_cmd_pos(x, y, z, az)
+    def set_cmd_pos(self, x, y, z, az):
+        self.mouse.set_cmd_pos(x, y, z, az)
 
-    # def set_cmd_vel(self, vx, vy, vz, az):
-    #     self.mouse.set_cmd_vel(vx, vy, vz, az)
+    def set_cmd_vel(self, vx, vy, vz, az):
+        self.mouse.set_cmd_vel(vx, vy, vz, az)
 
-    # def set_cmd_mix(self, vx, vy, z, az):
-    #     self.mouse.set_cmd_mix(vx, vy, z, az)
+    def set_cmd_mix(self, vx, vy, z, az):
+        self.mouse.set_cmd_mix(vx, vy, z, az)
 
-    # def takeoff(self, h=3):
-    #     self.mouse.takeoff(h)
+    def takeoff(self, h=3):
+        self.mouse.takeoff(h)
 
-    # def land(self):
-    #     self.mouse.land()
+    def land(self):
+        self.mouse.land()
