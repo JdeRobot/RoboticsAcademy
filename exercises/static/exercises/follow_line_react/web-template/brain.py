1cc1e9247d26eb18434f3a1765a052778a88c623
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2022-08-04 18:59:58 +0530

    making a generalized react based exercise pattern

--- exercises/static/exercises/follow_line_react/web-template/brain.py
+++ exercises/static/exercises/follow_line_react/web-template/brain.py
@@ -59,9 +59,8 @@ class BrainProcess(multiprocessing.Process):
 
         # Reference Environment for the exec() function
         iterative_code, sequential_code = self.iterative_code, self.sequential_code
-
-        # print(sequential_code)
-        # print(iterative_code)
+        print(sequential_code)
+        print(iterative_code)
 
         # Whatever the code is, first step is to just stop!
         self.hal.sendV(0)
