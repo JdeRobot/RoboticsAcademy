f1862eb124c0ff47611ab6edc48aa19a81c3f862
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2022-08-06 02:07:17 +0530

    CircuitSelector component

--- react_frontend/webpack.config.js
+++ react_frontend/webpack.config.js
@@ -1,24 +1,26 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const BundleTrackerPlugin = require("webpack-bundle-tracker");
-const path = require('path');
-const glob = require('glob');
+const path = require("path");
+const glob = require("glob");
 
 const aliases = () => {
-  const exercises_folder = '../exercises/static/exercises/**/web-template/react-components/';
+  const exercises_folder =
+    "../exercises/static/exercises/**/web-template/react-components/";
   const exercises = glob.sync(path.resolve(__dirname, exercises_folder));
   const aliasConfig = {
     _: [
-          './src/components',
-          './src/components/exercises'
-      ],
-    'exercise': []
+      "./src/components",
+      "./src/components/exercises",
+      "./src/components/contexts",
+    ],
+    exercise: [],
   };
 
   exercises.map((exercise) => {
     const exercisePath = exercise.split(path.sep);
-    const exerciseName = exercisePath[exercisePath.length-3];
+    const exerciseName = exercisePath[exercisePath.length - 3];
     aliasConfig[`exercise`].push(exercise);
-  })
+  });
 
   return aliasConfig;
 };
@@ -26,18 +28,15 @@ const aliases = () => {
 module.exports = {
   entry: {
     index: "./src/index.js",
-    exercise: "./src/exercise-index.js"
+    exercise: "./src/exercise-index.js",
   },
   output: {
     filename: "js/[name].[contenthash:8].js",
-    clean: true
+    clean: true,
   },
   resolve: {
     alias: aliases(),
-    modules: [
-        'node_modules',
-        path.resolve(__dirname, 'node_modules')
-    ]
+    modules: ["node_modules", path.resolve(__dirname, "node_modules")],
   },
   module: {
     rules: [
@@ -51,36 +50,36 @@ module.exports = {
             loader: "css-loader",
             options: {
               url: false,
-              sourceMap: true
-            }
+              sourceMap: true,
+            },
           },
-          "sass-loader"
-        ]
+          "sass-loader",
+        ],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [
-            {
-              loader: "babel-loader",
-              options: {
-                "presets": [
-                    "@babel/preset-env",
-                    [ "@babel/preset-react", {"runtime": "automatic"} ]
-                ]
-              }
-            }
-        ]
-      }
-    ]
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+              ],
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-      new BundleTrackerPlugin({
-        filename: './webpack-stats.json'
-      }),
-      new MiniCssExtractPlugin({
-        filename: 'css/[name].css'
-      })
+    new BundleTrackerPlugin({
+      filename: "./webpack-stats.json",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
   ],
-  devtool: 'inline-source-map'
+  devtool: "inline-source-map",
 };
