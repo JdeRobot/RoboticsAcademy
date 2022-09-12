eeee72fbee807f889cacfdbdc20403d9ab1a5b2c
RUFFY-369 <prakarshkaushik369@gmail.com>
2022-09-12 18:57:44 +0530

    multiprocessing final for visual lander

--- exercises/static/exercises/visual_lander/web-template/gui.py
+++ exercises/static/exercises/visual_lander/web-template/gui.py
@@ -5,98 +5,76 @@ import threading
 import time
 from datetime import datetime
 from websocket_server import WebsocketServer
+import logging
+import rospy
+import cv2
+import sys
+import numpy as np
+import multiprocessing
 
+from interfaces.pose3d import ListenerPose3d
+from shared.image import SharedImage
+from shared.value import SharedValue
+
+from shared.car import Car
 
 # Graphical User Interface Class
 class GUI:
     # Initialization function
     # The actual initialization
     def __init__(self, host, car):
-        t = threading.Thread(target=self.run_server)
-
+        
+        rospy.init_node("GUI")
         self.payload = {'image': ''}
         self.left_payload = {'image': ''}
         self.server = None
         self.client = None
-
+        
         self.host = host
 
-        # Image variables
-        self.image_to_be_shown = None
-        self.image_to_be_shown_updated = False
-        self.image_show_lock = threading.Lock()
-
-        self.left_image_to_be_shown = None
-        self.left_image_to_be_shown_updated = False
-        self.left_image_show_lock = threading.Lock()
-
-        self.acknowledge = False
-        self.acknowledge_lock = threading.Lock()
-
+        # Image variable host
+        self.shared_image = SharedImage("guifrontalimage")
+        self.shared_left_image = SharedImage("guiventralimage")
+        
+        # Event objects for multiprocessing
+        self.ack_event = multiprocessing.Event()
+        self.cli_event = multiprocessing.Event()
         # Take the console object to set the same websocket and client
         self.car = car
+
+        # Start server thread
+        t = threading.Thread(target=self.run_server)
         t.start()
 
-    # Explicit initialization function
-    # Class method, so user can call it without instantiation
-    @classmethod
-    def initGUI(cls, host):
-        # self.payload = {'image': '', 'shape': []}
-        new_instance = cls(host)
-        return new_instance
 
     # Function to prepare image payload
     # Encodes the image as a JSON string and sends through the WS
     def payloadImage(self):
-        self.image_show_lock.acquire()
-        image_to_be_shown_updated = self.image_to_be_shown_updated
-        image_to_be_shown = self.image_to_be_shown
-        self.image_show_lock.release()
-
-        image = image_to_be_shown
+        image = self.shared_image.get()
         payload = {'image': '', 'shape': ''}
-
-        if not image_to_be_shown_updated:
-            return payload
-
+    	
         shape = image.shape
         frame = cv2.imencode('.JPEG', image)[1]
         encoded_image = base64.b64encode(frame)
-
+        
         payload['image'] = encoded_image.decode('utf-8')
         payload['shape'] = shape
-
-        self.image_show_lock.acquire()
-        self.image_to_be_shown_updated = False
-        self.image_show_lock.release()
-
+        
         return payload
-
+        
     # Function to prepare image payload
     # Encodes the image as a JSON string and sends through the WS
     def payloadLeftImage(self):
-        self.left_image_show_lock.acquire()
-        left_image_to_be_shown_updated = self.left_image_to_be_shown_updated
-        left_image_to_be_shown = self.left_image_to_be_shown
-        self.left_image_show_lock.release()
-
-        image = left_image_to_be_shown
+        image = self.shared_left_image.get()
         payload = {'image': '', 'shape': ''}
-
-        if not left_image_to_be_shown_updated:
-            return payload
-
+    	
         shape = image.shape
         frame = cv2.imencode('.JPEG', image)[1]
         encoded_image = base64.b64encode(frame)
-
+        
         payload['image'] = encoded_image.decode('utf-8')
         payload['shape'] = shape
-
-        self.left_image_show_lock.acquire()
-        self.left_image_to_be_shown_updated = False
-        self.left_image_show_lock.release()
-
+        
         return payload
 
     # Function for student to call
@@ -117,27 +95,17 @@ class GUI:
     # Called when a new client is received
     def get_client(self, client, server):
         self.client = client
+        self.cli_event.set()
 
-    # Function to get value of Acknowledge
-    def get_acknowledge(self):
-        self.acknowledge_lock.acquire()
-        acknowledge = self.acknowledge
-        self.acknowledge_lock.release()
-
-        return acknowledge
-
-    # Function to get value of Acknowledge
-    def set_acknowledge(self, value):
-        self.acknowledge_lock.acquire()
-        self.acknowledge = value
-        self.acknowledge_lock.release()
-
+        print(client, 'connected')
+        
+        
     # Update the gui
     def update_gui(self):
         # Payload Image Message
         payload = self.payloadImage()
         self.payload["image"] = json.dumps(payload)
-
+        
         message = "#gui" + json.dumps(self.payload)
         self.server.send_message(self.client, message)
 
@@ -147,24 +115,34 @@ class GUI:
 
         message = "#gul" + json.dumps(self.left_payload)
         self.server.send_message(self.client, message)
-
+            
     # Function to read the message from websocket
     # Gets called when there is an incoming message from the client
     def get_message(self, client, server, message):
         # Acknowledge Message for GUI Thread
-        if message[:4] == "#ack":
-            self.set_acknowledge(True)
+
+        if(message[:4] == "#ack"):
+            # Set acknowledgement flag
+            self.ack_event.set()
         elif message[:4] == "#car":
-            self.car.start_car(int(message[4:5]))
+            self.car.start_car()
         elif message[:4] == "#stp":
             self.car.stop_car()
+        # Reset message
         elif message[:4] == "#rst":
             self.car.reset_car()
+    # Function that gets called when the connected closes
+    def handle_close(self, client, server):
+        print(client, 'closed')
+
+
+
     # Activate the server
     def run_server(self):
         self.server = WebsocketServer(port=2303, host=self.host)
         self.server.set_fn_new_client(self.get_client)
         self.server.set_fn_message_received(self.get_message)
+        self.server.set_fn_client_left(self.handle_close)
 
         logged = False
         while not logged:
@@ -181,36 +159,50 @@ class GUI:
     # Function to reset
     def reset_gui(self):
         pass
-
+        
 
 # This class decouples the user thread
 # and the GUI update thread
-class ThreadGUI:
-    def __init__(self, gui):
-        self.gui = gui
+class ProcessGUI(multiprocessing.Process):
+    def __init__(self):
+        super(ProcessGUI, self).__init__()
 
+        self.host = sys.argv[1]
+        self.car = Car()
         # Time variables
-        self.ideal_cycle = 80
-        self.measured_cycle = 80
+        self.time_cycle = SharedValue("gui_time_cycle")
+        self.ideal_cycle = SharedValue("gui_ideal_cycle")
         self.iteration_counter = 0
 
+    # Function to initialize events
+    def initialize_events(self):
+        # Events
+        self.ack_event = self.gui.ack_event
+        self.cli_event = self.gui.cli_event
+        self.exit_signal = multiprocessing.Event()
+
     # Function to start the execution of threads
-    def start(self):
+    def run(self):
+        # Initialize GUI
+        self.gui = GUI(self.host, self.car)
+        self.initialize_events()
+
+        # Wait for client before starting
+        self.cli_event.wait()
         self.measure_thread = threading.Thread(target=self.measure_thread)
-        self.thread = threading.Thread(target=self.run)
+        self.thread = threading.Thread(target=self.run_gui)
 
         self.measure_thread.start()
         self.thread.start()
 
-        print("GUI Thread Started!")
+        print("GUI Process Started!")
+
+        self.exit_signal.wait()
 
     # The measuring thread to measure frequency
     def measure_thread(self):
-        while self.gui.client is None:
-            pass
-
         previous_time = datetime.now()
-        while True:
+        while(True):
             # Sleep for 2 seconds
             time.sleep(2)
 
@@ -223,32 +215,40 @@ class ThreadGUI:
             # Get the time period
             try:
                 # Division by zero
-                self.measured_cycle = ms / self.iteration_counter
+                self.ideal_cycle.add(ms / self.iteration_counter)
             except:
-                self.measured_cycle = 0
+                self.ideal_cycle.add(0)
 
             # Reset the counter
             self.iteration_counter = 0
 
     # The main thread of execution
-    def run(self):
-        while self.gui.client is None:
-            pass
-
-        while True:
+    def run_gui(self):
+        while(True):
             start_time = datetime.now()
+            # Send update signal
             self.gui.update_gui()
-            acknowledge_message = self.gui.get_acknowledge()
 
-            while not acknowledge_message:
-                acknowledge_message = self.gui.get_acknowledge()
-
-            self.gui.set_acknowledge(False)
-
+            # Wait for acknowldege signal
+            self.ack_event.wait()
+            self.ack_event.clear()
+            
             finish_time = datetime.now()
             self.iteration_counter = self.iteration_counter + 1
-
+            
             dt = finish_time - start_time
             ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-            if ms < self.ideal_cycle:
-                time.sleep((self.ideal_cycle-ms) / 1000.0)
+            time_cycle = self.time_cycle.get()
+
+            if(ms < time_cycle):
+                time.sleep((time_cycle-ms) / 1000.0)
+
+        self.exit_signal.set()
+    
+    # Functions to handle auxillary GUI functions
+    def reset_gui(self):
+        self.gui.reset_gui()
+
+if __name__ == "__main__":
+    gui = ProcessGUI()
+    gui.start()
