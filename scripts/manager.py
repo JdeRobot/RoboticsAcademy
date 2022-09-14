c2be8816f08b74085d35d27afda370a0b1a3d568
RUFFY-369 <prakarshkaushik369@gmail.com>
2022-09-14 17:50:18 +0530

    final manager.py for each drone ex working with sim

--- scripts/manager.py
+++ scripts/manager.py
@@ -24,9 +24,7 @@ def check_device(device_path):
 RADI_VERSION = "3.2.3"
 DRI_PATH = "/dev/dri/card0"
 ACCELERATION_ENABLED = check_device(DRI_PATH)
-DRONE_EX = ["follow_road", "labyrinth_escape", "position_control", 
-            "rescue_people", "drone_hangar", "drone_gymkhana", "visual_lander", "drone_cat_mouse_game",
-            "package_delivery", "power_tower_inspection"]
+DRONE_EX = []
 CIRCUIT_EX = ["follow_line", "follow_line_game"]
 HARD_RESET_EX = ["obstacle_avoidance"]
 STDR_EX = ["laser_mapping", "laser_loc"]
