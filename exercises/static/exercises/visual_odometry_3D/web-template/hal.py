87fb2264c42cc76a74f85189dc7b0eea0467cff7
PabloAsensio <p.asensio97@gmail.com>
2022-12-06 22:03:34 +0100

    update hal

--- exercises/static/exercises/visual_odometry_3D/web-template/hal.py
+++ exercises/static/exercises/visual_odometry_3D/web-template/hal.py
@@ -12,47 +12,42 @@ from interfaces.pinhole_camera import PinholeCamera
 
 # Hardware Abstraction Layer
 class HAL:
-    # Dataset data
-    SEQUENCE_NUMBER = "01"
-    DATASET = 'kitti'
-
-    # Paths
-    DATASET_PATH = "/RoboticsAcademy/exercises/static/exercises/assets/kitti/dataset/"
-    SEQUENCE_PATH = DATASET_PATH + "sequences/" + SEQUENCE_NUMBER
-    LEFT_CAMERA_IMAGES_PATH = SEQUENCE_PATH + "/image_0/*.png"
-    RIGHT_CAMERA_IMAGES_PATH = SEQUENCE_PATH + "/image_1/*.png"
-   
-    # Files
-    GROUNDTRUTH_FILE = DATASET_PATH + "poses/" + SEQUENCE_NUMBER + "/.txt"
-    CALIBRATION_FILE = SEQUENCE_PATH + "/calib.txt"
-
-    # Class global variables
-    IMAGE_COUNTER = 0
-    LEFT_IMAGE_FILES_ARRAY = sorted(glob(LEFT_CAMERA_IMAGES_PATH))
-    RIGHT_IMAGE_FILES_ARRAY = sorted(glob(RIGHT_CAMERA_IMAGES_PATH))
-
     def __init__(self):
 
-        self.image = None
-        self.counter = IMAGE_COUNTER
+        # Dataset data
+        SEQUENCE_NUMBER = "01"
+        DATASET = 'kitti'
 
+        # Paths
+        DATASET_PATH = "/RoboticsAcademy/exercises/static/exercises/assets/kitti/dataset/"
+        SEQUENCE_PATH = DATASET_PATH + "sequences/" + SEQUENCE_NUMBER
+        LEFT_CAMERA_IMAGES_PATH = SEQUENCE_PATH + "/image_0/*.png"
+        RIGHT_CAMERA_IMAGES_PATH = SEQUENCE_PATH + "/image_1/*.png"
+    
+        # Files
+        GROUNDTRUTH_FILE = DATASET_PATH + "poses/" + SEQUENCE_NUMBER + "/.txt"
+        CALIBRATION_FILE = SEQUENCE_PATH + "/calib.txt"
+
+        self.image = None
+
+        self.image_counter = 0
+        self.left_image_files_array = sorted(glob(LEFT_CAMERA_IMAGES_PATH))
+        self.right_image_files_array = sorted(glob(RIGHT_CAMERA_IMAGES_PATH))
 
     # Get Image from ROS Driver Camera
     def getImage(self, lr):
 
-        print("\t< LEFT_IMAGE_FILES_ARRAY >", LEFT_IMAGE_FILES_ARRAY)
-        print("\t< RIGHT_IMAGE_FILES_ARRAY >", RIGHT_IMAGE_FILES_ARRAY)
+        print("\t< LEFT_IMAGE_FILES_ARRAY >", self.left_image_files_array)
+        print("\t< RIGHT_IMAGE_FILES_ARRAY >", self.right_image_files_array)
 
         if (lr == 'left'):
-            image = cv2.imread(LEFT_IMAGE_FILES_ARRAY[IMAGE_COUNTER], 0)
+            image = cv2.imread(self.left_image_files_array[self.image_counter], 0)
         elif (lr == 'right'):
-            image = cv2.imread(RIGHT_IMAGE_FILES_ARRAY[IMAGE_COUNTER], 0)
+            image = cv2.imread(self.right_image_files_array[self.image_counter], 0)
         else:
             print("Invalid camera")
-        
-        self.counter += 1
 
         return image
 
     def advance(self):
-        IMAGE_COUNTER += 1
+        self.image_counter += 1
