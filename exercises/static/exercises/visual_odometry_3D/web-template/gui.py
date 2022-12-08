ecaf9995e880267492bf06e69043731406ab76d0
PabloAsensio <p.asensio97@gmail.com>
2022-12-08 12:20:50 +0100

    hal message

--- exercises/static/exercises/visual_odometry_3D/web-template/gui.py
+++ exercises/static/exercises/visual_odometry_3D/web-template/gui.py
@@ -52,7 +52,7 @@ class GUI:
         self.image_show_lock.release()
 
         image = image_to_be_shown
-        payload = {'image': '', 'shape': '', 'counter': '-1'}
+        payload = {'image': '', 'shape': '', 'counter': self.hal.left_image_files_array}
 
         if(image_to_be_shown_updated == False):
             return payload
