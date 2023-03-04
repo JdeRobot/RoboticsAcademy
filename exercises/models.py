9a2081b78021d5f11acc37162491a8afa47c9af6
pariaspe <pedro.ariasp@upm.es>
2023-03-04 11:25:44 +0100

    fixes #1961

--- exercises/models.py
+++ exercises/models.py
@@ -1,5 +1,9 @@
-from django.db import models
+"""
+models.py
+"""
+
 import json
+from django.db import models
 
 
 StatusChoice = (
@@ -12,19 +16,35 @@ StatusChoice = (
 # Create your models here.
 
 class Exercise(models.Model):
+    """
+    RoboticsCademy Exercise model
+    """
     exercise_id = models.CharField(max_length=40, blank=False, unique=True)
     name = models.CharField(max_length=40, blank=False, unique=True)
     description = models.CharField(max_length=400, blank=False)
-    assets = models.CharField(max_length=2000, default=json.dumps({"notebook": ""}))  # JSON Object.
-    tags = models.CharField(max_length=2000, default=json.dumps({'tags': ""}))  # JSON Object for tags
-    status = models.CharField(max_length=20, choices=StatusChoice, default="ACTIVE")
-    configuration = models.TextField(default=json.dumps("{}"))
+    assets = models.CharField(
+        max_length=2000,
+        default=json.dumps({"notebook": ""})
+    )
+    tags = models.CharField(
+        max_length=2000,
+        default=json.dumps({'tags': ""})
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=StatusChoice,
+        default="ACTIVE"
+    )
+    configuration = models.TextField(default=json.dumps({}))
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     @property
     def context(self):
+        """
+        Build and return context
+        """
         exercise_configuration = json.loads(self.configuration)
 
         # extend exercise configuration with some useful stuff
