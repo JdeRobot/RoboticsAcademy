10e769881e6e1bb0abcc104f9682b401756ebb60
pariaspe <pedro.ariasp@upm.es>
2023-04-17 12:50:59 +0200

    views cleaned

--- exercises/views.py
+++ exercises/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
-from .models import Exercise
-import ast
-import sys
 import json
 import tempfile
 from pylint import epylint as lint
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from .models import Exercise
 
-#TODO: Too many hardcoded strings, review
+
 def index(request):
     exercises = Exercise.objects.all()
     context = {"exercises": exercises}
@@ -23,17 +21,18 @@ def load_exercise(request, exercise_id):
 
 def request_code(request, exercise_id):
     difficulty = request.GET.get('diff')
-    path = '/exercises/static/exercises/{}/web-template/assets/{}.py'.format(exercise_id, difficulty)
+    path = f'/exercises/static/exercises/{exercise_id}/assets/{difficulty}.py'
     path = str(settings.BASE_DIR) + path
     print('PATH: ', path)
-    with open(path) as f:
-        data = f.read().replace('\\n', '\n')
+    with open(path, encoding='utf-8') as file:
+        data = file.read().replace('\\n', '\n')
 
     print(data)
 
-    if difficulty != None:
+    if difficulty is not None:
         print('EXERCISE: ', exercise_id, 'DIFFICULTY: ', difficulty)
         return HttpResponse(data, content_type="text/plain")
+
 
 @csrf_exempt
 def evaluate_style(request):
@@ -58,15 +57,14 @@ def evaluate_style(request):
             init_index += len('rated at ')
             final_index = result.find('/10', init_index)
             score = round(float(result[init_index:final_index]) * 10, 2)
-        response = HttpResponse(result+"\n"+str(score), content_type="text/plain")
+        response = HttpResponse(result+"\n"+str(score),
+                                content_type="text/plain")
         return response
     except Exception as ex:
         print("2º")
         print(ex)
         response = HttpResponse("Error", content_type="text/plain")
         return response
-
-
 
 
 def get_python_code(request):
