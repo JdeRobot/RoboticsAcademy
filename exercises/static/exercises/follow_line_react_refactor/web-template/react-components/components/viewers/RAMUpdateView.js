7007e0501426d6c7ae5bd0d36af5346571326869
David Maria Arribas <dmariaa70@gmail.com>
2023-05-14 13:12:13 +0200

    New: Refectored rendering code New: Added timer to rendering code

--- exercises/static/exercises/follow_line_react_refactor/web-template/react-components/components/viewers/RAMUpdateView.js
+++ exercises/static/exercises/follow_line_react_refactor/web-template/react-components/components/viewers/RAMUpdateView.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React from "react";
 import updateRenderer from "./UpdateRenderer";
 
 import defaultCircuit from "../../images/default_circuit.png"
@@ -7,24 +7,13 @@ import "../../css/viewers/RAMUpdateView.css";
 const RAMUpdateView = (props) => {
   const { width, height } = props;
   const canvasRef = React.useRef(null);
-  const updateImageRef = React.useRef(new Image());
-  const circuitImageRef = React.useRef(new Image());
-  const carPositionRef = React.useRef([0, 0]);
+  const updateRef = React.useRef({});
   const rendererRef = React.useRef(new updateRenderer());
-  const noImage = "https://via.placeholder.com/800x600.png?text=No%20image%20received%20from%20exercise";
-
 
   React.useEffect(() => {
     // Callback doesn't update renderer, it only stores the new image and position
     const callback = (message) => {
-      const update = message.data.update;
-      if (update.image) {
-        const image = JSON.parse(update.image);
-        updateImageRef.current.src = `data:image/png;base64,${image.image}`;
-        carPositionRef.current = update.map.slice(1,).slice(0,-1).split(',');
-      } else if(!update.brain) {
-        updateImageRef.current.src = noImage;
-      }
+      updateRef.current = message.data.update;
     };
 
     // Subscribe update event
@@ -35,11 +24,8 @@ const RAMUpdateView = (props) => {
       callback);
 
     // Init renderer when circuit image is loaded
-    circuitImageRef.current.src = defaultCircuit;
-    circuitImageRef.current.onload = () => {
-      rendererRef.current.init(width, height, canvasRef.current, updateImageRef, circuitImageRef, carPositionRef);
-      rendererRef.current.run();
-    }
+    rendererRef.current.init(width, height, canvasRef.current, updateRef, defaultCircuit);
+    rendererRef.current.run();
 
     // Unsubscribe update event and stop renderer
     return () => {
--- exercises/static/exercises/follow_line_react_refactor/web-template/react-components/components/viewers/UpdateRenderer.js
+++ exercises/static/exercises/follow_line_react_refactor/web-template/react-components/components/viewers/UpdateRenderer.js
@@ -7,14 +7,16 @@ function UpdateRenderer()
   const circuitMarginX = 16;
   const circuitMarginY = 16;
 
-  let currentTick = null;
+  const cameraImage = new Image();
+  const circuitImage = new Image();
 
-  this.init = (width, height, canvas, imageRef, circuitImageRef, circuitPositionRef) => {
+  let currentTick = null;
+  let circuitImageLoaded = false;
+
+  this.init = (width, height, canvas, updateRef, circuitImageSrc) => {
     this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
-    this.imageRef = imageRef;
-    this.circuitImageRef = circuitImageRef;
-    this.circuitPositionRef = circuitPositionRef;
+    this.updateRef = updateRef;
 
     this.width = width;
     this.height = height;
@@ -23,25 +25,38 @@ function UpdateRenderer()
 
     this.scaleX = this.circuitWidth / circuitReferenceWidth;
     this.scaleY = this.circuitHeight / circuitReferenceHeight;
-  }
+
+    cameraImage.onload = this.loadCameraImage;
+    
+    circuitImage.onload = () => circuitImageLoaded = true;
+    circuitImage.src = circuitImageSrc;
+  };
 
   this.run = () => {
     currentTick = requestAnimationFrame(this.tick);
-  }
+  };
 
   this.stop = () => {
     if(currentTick != null) {
       cancelAnimationFrame(currentTick);
     }
-  }
+  };
 
   this.tick = () => {
+    if(this.updateRef.current.image) {
+      const image = JSON.parse(this.updateRef.current.image);
+      cameraImage.src = `data:image/png;base64,${image.image}`;
+    } else {
+      currentTick = requestAnimationFrame(this.tick);
+    }
+  };
+
+  this.loadCameraImage = () => {
     // clear canvas
     this.ctx.canvas.width = this.width;
     this.ctx.canvas.height = this.height;
 
-    // Draw current image
-    this.drawCameraImage();
+    this.ctx.drawImage(cameraImage, 0, 0, this.width, this.height);
 
     // Draw circuit image and car position point
     this.drawAerialView();
@@ -49,17 +64,22 @@ function UpdateRenderer()
     currentTick = requestAnimationFrame(this.tick);
   };
 
-  this.drawCameraImage = () => {
-    this.ctx.drawImage(this.imageRef.current, 0, 0, this.width, this.height);
-  };
-
   this.drawAerialView = () => {
-    this.ctx.drawImage(this.circuitImageRef.current, circuitMarginX, circuitMarginY, this.circuitWidth,
-      this.circuitHeight);
+    if(circuitImageLoaded) {
+      this.ctx.drawImage(circuitImage, circuitMarginX, circuitMarginY, this.circuitWidth, this.circuitHeight);
 
-    const position = this.circuitPositionRef.current;
-    this.drawCar(circuitMarginX + (position[0] * this.scaleX), circuitMarginY + (position[1] * this.scaleY));
-  }
+      if(this.updateRef.current.map)
+      {
+        const position = this.updateRef.current.map.slice(1,).slice(0,-1).split(',');
+        this.drawCar(circuitMarginX + (position[0] * this.scaleX), circuitMarginY + (position[1] * this.scaleY));
+      }
+
+      if(this.updateRef.current.lap)
+      {
+        this.drawLapTime(this.updateRef.current.lap);
+      }
+    }
+  };
 
   this.drawCar = (x, y) => {
     this.ctx.beginPath();
@@ -69,13 +89,20 @@ function UpdateRenderer()
     this.ctx.stroke();
     this.ctx.fillStyle = "#FF0000";
     this.ctx.fill();
-  }
+  };
+
+  this.drawLapTime = (time) => {
+    this.ctx.font = "bold 24px sans-serif";
+    this.ctx.textAlign = "right";
+    this.ctx.textBaseline = "top";
+    this.ctx.fillText(time, this.ctx.canvas.width - 16, 16);
+  };
 
   return {
     init: this.init,
     run: this.run,
     stop: this.stop
-  }
+  };
 }
 
 export default UpdateRenderer;
