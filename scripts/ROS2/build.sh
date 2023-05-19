1597ada962d58bd56bb8b6a8e52a7d403abedc8c
ReyDoran <manetoscopio@gmail.com>
2023-05-19 11:09:45 +0200

    Removed duplicates

--- scripts/ROS2/build.sh
+++ scripts/ROS2/build.sh
@@ -1,46 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-############################################################
-# Help                                                     #
-############################################################
-Help()
-{
-   # Display Help
-   echo "Syntax: build.sh [-h] [-f]"
-   echo "options:"
-   echo "-h   Print this Help."
-   echo "-f   Force creation of base image. If ommited, the base image is created only if "
-   echo "     it doestn exist."
-   echo
-}
-
-force_build=false
-while getopts ":hf" option; do
-   case $option in
-      h) # display Help
-         echo "Generates RoboticsAcademy RADI image"
-         echo
-         Help
-         exit 0
-         ;;
-      f)
-        force_build=true
-        ;;
-      \?)
-        echo "Invalid Option: -$OPTARG" 1>&2
-        Help
-        exit 1
-        ;;
-   esac
-done
-shift $(($OPTIND - 1))
-
-if $force_build || [[ "$(docker images -q jderobot/robotics-applications:pre-base 2> /dev/null)" == "" ]]; then
-  echo "BUILDING Jderobot PRE-BASE IMAGE ====================="
+image_tag="$1"
+if [ -z "$image_tag" ]; then
+  echo "You have to specify a tag for the image"
   echo
-  docker build -f Dockerfile.pre-base -t jderobot/robotics-applications:pre-base .
+  Help
+  exit 2
 fi
 
-echo "BUILDING RoboticsAcademy BASE IMAGE ====================="
+echo "BUILDING RoboticsAcademy RADI IMAGE ====================="
 echo
-docker build -f Dockerfile.base -t jderobot/robotics-applications:base .
+docker build -f Dockerfile.academy --no-cache=true -t jderobot/robotics-academy:$image_tag .
