e0b82ea587877ec2b3b52b509ceafa86d28158ed
Pablo Asensio Martinez <p.asensio97@gmail.com>
2023-05-22 12:41:54 +0200

    Update

--- static/exercises/visual_odometry_3D/js/ws_gui.js
+++ static/exercises/visual_odometry_3D/js/ws_gui.js
@@ -5,59 +5,54 @@ function decode_utf8(s) {
 
 // Websocket and other variables for image display
 var websocket_gui, animation_id;
+var canvas = document.getElementById("gui_canvas"),
+    context = canvas.getContext('2d');
+var image = new Image();
 
 function declare_gui(websocket_address) {
     websocket_gui = new WebSocket(websocket_address);
-
+  
     websocket_gui.onopen = function (event) {
-        //alert("[open] Connection established!");
-        set_launch_level(get_launch_level() + 1);
-        if (websocket_code.readyState == 1) {
-            alert("[open] Connection established!");
-            connectionUpdate({ connection: 'exercise', command: 'up' }, '*');
-        }
+      set_launch_level(get_launch_level() + 1);
+      if (websocket_code.readyState == 1) {
+        alert("[open] Connection established!");
+        connectionUpdate({ connection: 'exercise', command: 'up' }, '*');
+      }
     }
-
+  
     websocket_gui.onclose = function (event) {
-        connectionUpdate({ connection: 'exercise', command: 'down' }, '*');
-        if (event.wasClean) {
-            //alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-        }
-        else {
-            //alert("[close] Connection closed!");
-        }
+      connectionUpdate({ connection: 'exercise', command: 'down' }, '*');
+      if (event.wasClean) {
+        //alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+      } else {
+        //alert("[close] Connection closed!");
+      }
     }
-
+  
     // What to do when a message from server is received
     websocket_gui.onmessage = function (event) {
-        operation = event.data.substring(0, 4);
-
-        if (operation == "#gui") {
-            // Parse the entire Object
-            var data = JSON.parse(event.data.substring(4,));
-
-            // Parse the Image Data
-            var image_data = JSON.parse(data.image),
-                source = decode_utf8(image_data.image),
-                shape = image_data.shape;
-
-            if (source != "") {
-                image.src = "data:image/jpeg;base64," + source;
-                canvas.width = shape[1];
-                canvas.height = shape[0];
-            }
-
-            // Send the Acknowledgment Message
-            websocket_gui.send("#ack");
+      operation = event.data.substring(0, 4);
+  
+      if (operation == "#gui") {
+        // Parse the entire Object
+        var data = JSON.parse(event.data.substring(4));
+  
+        // Parse the Image Data
+        var image_data = JSON.parse(data.image),
+            source = decode_utf8(image_data.image),
+            shape = image_data.shape;
+  
+        if (source != "") {
+            image.src = "data:image/jpeg;base64," + source;
+            canvas.width = shape[1];
+            canvas.height = shape[0];
         }
-
+  
+        // Send the Acknowledgment Message
+        websocket_gui.send("#ack");
+      }
     }
-}
-
-var canvas = document.getElementById("odometry-frame.odometry-frame"),
-    context = canvas.getContext('2d');
-image = new Image();
-
+  }
 
 // For image object
 image.onload = function () {
@@ -66,5 +61,6 @@ image.onload = function () {
 // Request Animation Frame to remove the flickers
 function update_image() {
     animation_id = window.requestAnimationFrame(update_image);
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(image, 0, 0);
 }
