bdf006f50d412bee9c6d5daca1f0683c03a9a41b
Pablo Asensio Martinez <p.asensio97@gmail.com>
2023-05-22 12:52:33 +0200

    update

--- static/exercises/visual_odometry_3D/js/ws_gui.js
+++ static/exercises/visual_odometry_3D/js/ws_gui.js
@@ -13,44 +13,50 @@ function declare_gui(websocket_address) {
     websocket_gui = new WebSocket(websocket_address);
   
     websocket_gui.onopen = function (event) {
-      set_launch_level(get_launch_level() + 1);
-      if (websocket_code.readyState == 1) {
-        alert("[open] Connection established!");
-        connectionUpdate({ connection: 'exercise', command: 'up' }, '*');
-      }
+        set_launch_level(get_launch_level() + 1);
+        if (websocket_code.readyState == 1) {
+            alert("[open] Connection established!");
+            connectionUpdate({ connection: 'exercise', command: 'up' }, '*');
+        }
+        image.src = "/static/exercises/assets/kitti/dataset/sequences/01/image_0/000000.png";
+        canvas.width = 1241;
+        canvas.height = 376;
+        update_image();
     }
   
     websocket_gui.onclose = function (event) {
-      connectionUpdate({ connection: 'exercise', command: 'down' }, '*');
-      if (event.wasClean) {
-        //alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-      } else {
-        //alert("[close] Connection closed!");
-      }
+        connectionUpdate({ connection: 'exercise', command: 'down' }, '*');
+        if (event.wasClean) {
+            //alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        } else {
+            //alert("[close] Connection closed!");
+        }
     }
   
     // What to do when a message from server is received
     websocket_gui.onmessage = function (event) {
-      operation = event.data.substring(0, 4);
-  
-      if (operation == "#gui") {
-        // Parse the entire Object
-        var data = JSON.parse(event.data.substring(4));
-  
-        // Parse the Image Data
-        var image_data = JSON.parse(data.image),
-            source = decode_utf8(image_data.image),
-            shape = image_data.shape;
-  
-        if (source != "") {
-            image.src = "data:image/jpeg;base64," + source;
-            canvas.width = shape[1];
-            canvas.height = shape[0];
+        operation = event.data.substring(0, 4);
+    
+        if (operation == "#gui") {
+            // Parse the entire Object
+            var data = JSON.parse(event.data.substring(4));
+    
+            // Parse the Image Data
+            var image_data = JSON.parse(data.image),
+                source = decode_utf8(image_data.image),
+                shape = image_data.shape;
+    
+            if (source != "") {
+                image.src = "data:image/jpeg;base64," + source;
+                canvas.width = shape[1];
+                canvas.height = shape[0];
+
+                console.log(canvas.width, canvas.height)
+            }
+    
+            // Send the Acknowledgment Message
+            websocket_gui.send("#ack");
         }
-  
-        // Send the Acknowledgment Message
-        websocket_gui.send("#ack");
-      }
     }
   }
 
