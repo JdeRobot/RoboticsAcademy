13ab867815656c287ad0264b3bb2e8b55be17f98
Pablo Asensio Martinez <p.asensio97@gmail.com>
2023-05-28 14:37:27 +0200

    another try

--- static/exercises/visual_odometry_3D/js/ws_gui.js
+++ static/exercises/visual_odometry_3D/js/ws_gui.js
@@ -51,7 +51,7 @@ function declare_gui(websocket_address) {
                 estimated_position = image_data.estimated_position;
     
                 console.log("Counter: ", counter);
-                // console.log(true_euler_angles);
+                console.log("True Euler Angles: ",true_euler_angles);
                 // console.log(true_position);
                 // console.log(estimated_euler_angles);
                 // console.log(estimated_position);
