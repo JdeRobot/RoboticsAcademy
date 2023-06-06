87ae226719ad1a797bc6e1c952d39cca622558aa
ango1994 <ango19941994@gmail.com>
2023-06-06 09:21:16 +0200

    Add file name input

--- react_frontend/src/components/buttons/SaveButton.js
+++ react_frontend/src/components/buttons/SaveButton.js
@@ -1,25 +1,39 @@
 import * as React from "react";
 import SaveIcon from "@mui/icons-material/Save";
-import { Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { saveCode } from "../../helpers/utils";
 import PropTypes from "prop-types";
 
 const SaveFileButton = (props) => {
+  const [fileName, setFileName] = React.useState("myCode");
   const saveFile = () => {
     const userCode = RoboticsReactComponents.CodeEditor.getCode();
-    saveCode("test", userCode);
+    saveCode(fileName, userCode);
   };
   return (
-    <Button
-      id={"save"}
-      variant="contained"
-      color={"secondary"}
-      startIcon={<SaveIcon />}
-      sx={{ m: 1 }}
-      onClick={saveFile}
-    >
-      Save file
-    </Button>
+    <Box sx={{ display: "flex" }}>
+      <Button
+        id={"save"}
+        variant="contained"
+        color={"secondary"}
+        startIcon={<SaveIcon />}
+        sx={{ m: 1 }}
+        onClick={saveFile}
+      >
+        Save file
+      </Button>
+      <TextField
+        sx={{ m: 1 }}
+        size={"small"}
+        id="filename"
+        label="Filename"
+        color={"secondary"}
+        value={fileName}
+        onChange={(e) => {
+          setFileName(e.target.value);
+        }}
+      />
+    </Box>
   );
 };
 
