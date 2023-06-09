7aa4998f0e002f87bfc8b4731151248f2fc4c577
Blanca <blancasoriaru@gmail.com>
2023-06-09 11:43:09 +0200

    modifies launchers
    Signed-off-by: Blanca <blancasoriaru@gmail.com>

--- exercises/static/exercises/follow_person_newmanager/launch/ros2_humble/follow_person_followingcam.launch.py
+++ exercises/static/exercises/follow_person_newmanager/launch/ros2_humble/follow_person_followingcam.launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
     description="Position on the axis z of Turtlebot2"
   )
 
-  world_name = "hospital_follow_person.world"
+  world_name = "hospital_follow_person_followingcam.world"
   current_dir = "/RoboticsAcademy/exercises/static/exercises/follow_person_newmanager/launch/ros2_humble"
   world_path = os.path.join(current_dir, world_name)
 
