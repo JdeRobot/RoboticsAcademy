5e2aee973985150b79f2a793b09a428f0f788c95
zahid hussain <zahidhusssainzahid43@gmail.com>
2023-06-16 20:01:34 +0500

    Update asgi.py
    This title accurately reflects the change made in the code, which involves updating the ASGI configuration for a Django application. It provides a concise summary of the modification made in the commit.

--- academy/asgi.py
+++ academy/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academy.settings')
-
+django.setup()
 application = get_asgi_application()
+
