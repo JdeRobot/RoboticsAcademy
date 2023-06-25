565894bee8390499fd41d2beae905d8fc35908c8
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2023-06-25 19:21:26 +0530

    New: Global Navigation RR

---
+++ exercises/static/exercises/global_navigation_newmanager/react-components/GlobalNavigationRR.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Fragment} from "react";
+
+import "./css/GlobalNavigationRR.css";
+
+const GlobalNavigationRR = (props) => {
+  return (
+    <Fragment>
+    {props.children}
+    </Fragment>
+  );
+};
+
+export default GlobalNavigationRR;
