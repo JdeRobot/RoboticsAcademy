77e8a5f71102d71f31e841c8c77e627ed2d51240
ango1994 <ango19941994@gmail.com>
2023-06-26 12:21:49 +0200

    Fix exercise.py lo vac cleaner

--- exercises/static/exercises/vacuum_cleaner_loc_newmanager/python_template/ros2_humble/exercise.py
+++ exercises/static/exercises/vacuum_cleaner_loc_newmanager/python_template/ros2_humble/exercise.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function
 
+import os
+
 from websocket_server import WebsocketServer
 import time
 import threading
@@ -11,7 +13,8 @@ from datetime import datetime
 import re
 import json
 import importlib
-import os
+
+from std_srvs.srv import Empty
 
 from gui import GUI, ThreadGUI
 from hal import HAL
@@ -44,11 +47,83 @@ class Template:
         self.hal = HAL()
         self.gui = GUI(self.host, self.hal)
 
-    ################ --- BRAIN --- ################
+    # Function for saving
+    def save_code(self, source_code):
+        with open('code/academy.py', 'w') as code_file:
+            code_file.write(source_code)
+
+    # Function for loading
+    def load_code(self):
+        with open('code/academy.py', 'r') as code_file:
+            source_code = code_file.read()
+
+        return source_code
+
+    # Function to parse the code
+    # A few assumptions:
+    # 1. The user always passes sequential and iterative codes
+    # 2. Only a single infinite loop
+    def parse_code(self, source_code):
+        # Check for save/load
+        if(source_code[:5] == "#save"):
+            source_code = source_code[5:]
+            self.save_code(source_code)
+
+            return "", ""
+
+        elif(source_code[:5] == "#load"):
+            source_code = source_code + self.load_code()
+            self.server.send_message(self.client, source_code)
+
+            return "", ""
+
+        else:
+            sequential_code, iterative_code = self.seperate_seq_iter(source_code)
+            return iterative_code, sequential_code
+
+    # Function to parse code according to the debugging level
+
+    def debug_parse(self, source_code, debug_level):
+        if(debug_level == 1):
+            # If debug level is 0, then all the GUI operations should not be called
+            source_code = re.sub(r'GUI\..*', '', source_code)
+
+        return source_code
+
+    # Function to seperate the iterative and sequential code
+    def seperate_seq_iter(self, source_code):
+        if source_code == "":
+            return "", ""
+
+        # Search for an instance of while True
+        infinite_loop = re.search(r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', source_code)
+
+        # Seperate the content inside while True and the other
+        # (Seperating the sequential and iterative part!)
+        try:
+            start_index = infinite_loop.start()
+            iterative_code = source_code[start_index:]
+            sequential_code = source_code[:start_index]
+
+            # Remove while True: syntax from the code
+            # And remove the the 4 spaces indentation before each command
+            iterative_code = re.sub(r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', '', iterative_code)
+            # Add newlines to match line on bug report
+            extra_lines = sequential_code.count('\n')
+            while (extra_lines >= 0):
+                iterative_code = '\n' + iterative_code
+                extra_lines -= 1
+            iterative_code = re.sub(r'^[ ]{4}', '', iterative_code, flags=re.M)
+
+        except:
+            sequential_code = source_code
+            iterative_code = ""
+
+        return sequential_code, iterative_code
 
     # The process function
-    def process_code(self, source_code):
 
+    def process_code(self, source_code):
         # Redirect the information to console
         start_console()
 
@@ -71,9 +146,9 @@ class Template:
 
         # Run the iterative part inside template
         # and keep the check for flag
-        while not self.reload:
-            while (self.stop_brain):
-                if (self.reload):
+        while self.reload == False:
+            while (self.stop_brain == True):
+                if (self.reload == True):
                     break
                 time.sleep(0.1)
 
@@ -85,8 +160,7 @@ class Template:
             # Template specifics to run!
             finish_time = datetime.now()
             dt = finish_time - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
-                1000 + dt.microseconds / 1000.0
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
 
             # Keep updating the iteration counter
             if (iterative_code == ""):
@@ -103,29 +177,26 @@ class Template:
         print("Current Thread Joined!")
 
     # Function to generate the modules for use in ACE Editor
-
     def generate_modules(self):
         # Define HAL module
-        hal_module = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("HAL", None))
-        hal_module.HAL = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("HAL", None))
-        hal_module.HAL.motors = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("motors", None))
+        hal_module = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("HAL", None))
+        hal_module.HAL = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("HAL", None))
+        hal_module.HAL.motors = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("motors", None))
 
         # Add HAL functions
-        hal_module.HAL.getPose3d = self.hal.getPose3d
-        hal_module.HAL.setV = self.hal.setV
-        hal_module.HAL.setW = self.hal.setW
+        hal_module.HAL.getPose3d = self.hal.pose3d.getPose3d
+        hal_module.HAL.setV = self.hal.motors.sendV
+        hal_module.HAL.setW = self.hal.motors.sendW
         hal_module.HAL.laser = self.hal.laser
-        hal_module.HAL.getLaserData = self.hal.getLaserData
-        hal_module.HAL.getBumperData = self.hal.getBumperData
+        hal_module.HAL.getLaserData = self.hal.laser.getLaserData
+        hal_module.HAL.getBumperData = self.hal.bumper.getBumperData
 
         # Define GUI module
-        gui_module = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("GUI", None))
-        gui_module.GUI = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("GUI", None))
+        gui_module = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("GUI", None))
+        gui_module.GUI = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("GUI", None))
+
+        # Add GUI functions
+        gui_module.GUI.showNumpy = self.gui.showNumpy
 
         # Add GUI functions
         # gui_module.GUI.showImage = self.gui.showImage
@@ -149,8 +220,7 @@ class Template:
             # Measure the current time and subtract from the previous time to get real time interval
             current_time = datetime.now()
             dt = current_time - previous_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
-                1000 + dt.microseconds / 1000.0
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
             previous_time = current_time
 
             # Get the time period
@@ -165,85 +235,6 @@ class Template:
 
             # Send to client
             self.send_frequency_message()
-
-
-    ################ --- EXERCISE --- ################
-
-    # Function for saving
-    def save_code(self, source_code):
-        with open('code/academy.py', 'w') as code_file:
-            code_file.write(source_code)
-
-    # Function for loading
-    def load_code(self):
-        with open('code/academy.py', 'r') as code_file:
-            source_code = code_file.read()
-
-        return source_code
-
-    # Function to parse the code
-    # A few assumptions:
-    # 1. The user always passes sequential and iterative codes
-    # 2. Only a single infinite loop
-    def parse_code(self, source_code):
-        # Check for save/load
-        if (source_code[:5] == "#save"):
-            source_code = source_code[5:]
-            self.save_code(source_code)
-
-            return "", ""
-
-        elif (source_code[:5] == "#load"):
-            source_code = source_code + self.load_code()
-            self.server.send_message(self.client, source_code)
-
-            return "", ""
-
-        else:
-            sequential_code, iterative_code = self.seperate_seq_iter(
-                source_code)
-            return iterative_code, sequential_code
-
-    # Function to parse code according to the debugging level
-    def debug_parse(self, source_code, debug_level):
-        if (debug_level == 1):
-            # If debug level is 0, then all the GUI operations should not be called
-            source_code = re.sub(r'GUI\..*', '', source_code)
-
-        return source_code
-
-    # Function to seperate the iterative and sequential code
-    def seperate_seq_iter(self, source_code):
-        if source_code == "":
-            return "", ""
-
-        # Search for an instance of while True
-        infinite_loop = re.search(
-            r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', source_code)
-
-        # Seperate the content inside while True and the other
-        # (Seperating the sequential and iterative part!)
-        try:
-            start_index = infinite_loop.start()
-            iterative_code = source_code[start_index:]
-            sequential_code = source_code[:start_index]
-
-            # Remove while True: syntax from the code
-            # And remove the the 4 spaces indentation before each command
-            iterative_code = re.sub(
-                r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', '', iterative_code)
-            # Add newlines to match line on bug report
-            extra_lines = sequential_code.count('\n')
-            while (extra_lines >= 0):
-                iterative_code = '\n' + iterative_code
-                extra_lines -= 1
-            iterative_code = re.sub(r'^[ ]{4}', '', iterative_code, flags=re.M)
-
-        except:
-            sequential_code = source_code
-            iterative_code = ""
-
-        return sequential_code, iterative_code
 
     # Function to generate and send frequency messages
     def send_frequency_message(self):
@@ -267,9 +258,10 @@ class Template:
         message = "#freq" + json.dumps(self.frequency_message)
         self.server.send_message(self.client, message)
 
+    # Function to send ping message. Sends a boolean along to notify when the user code was executed
     def send_ping_message(self):
         self.server.send_message(self.client, "#ping")
-
+        
     # Function to notify the front end that the code was received and sent to execution
     def send_code_message(self):
         self.server.send_message(self.client, "#exec")
@@ -293,20 +285,20 @@ class Template:
     def execute_thread(self, source_code):
         # Keep checking until the thread is alive
         # The thread will die when the coming iteration reads the flag
-        if (self.thread != None):
+        if(self.thread != None):
             while self.thread.is_alive():
                 time.sleep(0.2)
 
         # Turn the flag down, the iteration has successfully stopped!
         self.reload = False
         # New thread execution
-        self.thread = threading.Thread(
-            target=self.process_code, args=[source_code])
+        self.thread = threading.Thread(target=self.process_code, args=[source_code])
         self.thread.start()
         self.send_code_message()
         print("New Thread Started!")
 
     # Function to read and set frequency from incoming message
+
     def read_frequency_message(self, message):
         frequency_message = json.loads(message)
 
@@ -322,15 +314,16 @@ class Template:
 
     # The websocket function
     # Gets called when there is an incoming message from the client
+
     def handle(self, client, server, message):
-        if (message[:5] == "#freq"):
+        if(message[:5] == "#freq"):
             frequency_message = message[5:]
             self.read_frequency_message(frequency_message)
             time.sleep(1)
-            self.send_frequency_message()
+            #self.send_frequency_message()
             return
 
-        elif (message[:5] == "#ping"):
+        elif(message[:5] == "#ping"):
             time.sleep(1)
             self.send_ping_message()
             return
@@ -341,7 +334,6 @@ class Template:
                 self.user_code = message[6:]
                 # print(repr(code))
                 self.reload = True
-                self.stop_brain = True
                 self.execute_thread(self.user_code)
             except:
                 pass
@@ -376,7 +368,7 @@ class Template:
         self.measure_thread.start()
 
         # Initialize the ping message
-        # self.send_frequency_message()
+        #self.send_frequency_message()
 
         print(client, 'connected')
 
