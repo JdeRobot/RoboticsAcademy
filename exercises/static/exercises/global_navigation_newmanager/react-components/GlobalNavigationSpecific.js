ae586ef791ea2e2407e9ae8ea27d3405c650c7a9
ango1994 <ango19941994@gmail.com>
2023-07-03 12:28:40 +0200

    Send pick to gui.py

--- exercises/static/exercises/global_navigation_newmanager/react-components/GlobalNavigationSpecific.js
+++ exercises/static/exercises/global_navigation_newmanager/react-components/GlobalNavigationSpecific.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { draw } from "./helpers/birds_eye_global_navigation";
+import {  draw } from "./helpers/birds_eye_global_navigation";
 
 function SpecificGlobalNavigation(props) {
   const guiCanvasRef = React.useRef();
@@ -8,21 +8,39 @@ function SpecificGlobalNavigation(props) {
   React.useEffect(() => {
     console.log("TestShowScreen subscribing to ['update'] events");
 
-    const callback = (message) => {
-      const data = message.data.update;
+    const getMapDataAndDraw = (data) => {
       if (data.map) {
         const pose = data.map.substring(1, data.map.length - 1);
-        const content = pose.split(",").map(function (item) {
+        const content = pose.split(',').map(function(item) {
           return parseFloat(item);
-        });
-        draw(
-          guiCanvasRef.current,
-          content[0],
-          content[1],
-          content[2],
-          content[3]
-        );
+        })
+        draw((content[0] ), (content[1] ), content[2], content[3]);
       }
+    }
+    const getImageAndDisplay = (data) => {
+      if(data.image) {
+        console.log(data.image)
+      let canvas = document.getElementById("gui-canvas-numpy");
+        //Parse encoded image data and decode it
+      function decode_utf8(s) {
+          return decodeURIComponent(escape(s))
+      }
+			var image_data = JSON.parse(data.image),
+			source = decode_utf8(image_data.image),
+			shape = image_data.shape;
+
+			if(source !== ""){
+				canvas.src = "data:image/png;base64," + source;
+				canvas.width = shape[1];
+				canvas.height = shape[0];
+			}
+      }
+    }
+
+    const callback = (message) => {
+      const data = message.data.update;
+      getMapDataAndDraw(data)
+      getImageAndDisplay(data)
     };
 
     window.RoboticsExerciseComponents.commsManager.subscribe(
@@ -39,19 +57,51 @@ function SpecificGlobalNavigation(props) {
     };
   }, []);
 
+  function destinationPicker(event){
+    let mapCanvas = document.getElementById("globalnav-eye");
+    let cursorX = (event.clientX - mapCanvas.getBoundingClientRect().left);
+    let cursorY = (event.clientY - mapCanvas.getBoundingClientRect().top);
+    let cursorXMap = cursorX/mapCanvas.width * 400;
+    let cursorYMap = cursorY/mapCanvas.height * 400;
+    return [cursorXMap, cursorYMap];
+}
+
   return (
+    <div style={{display: "flex",   width: "100%",
+    height: "100%"}}>
     <canvas
       ref={guiCanvasRef}
+      id="globalnav-eye"
       style={{
         backgroundImage:
           "url('/static/exercises/global_navigation_newmanager/resources/images/cityLargenBin.png')",
-        border: "2px solid #d3d3d3",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100% 100%",
-        width: "100%",
-        height: "100%"
+          border: "2px solid #d3d3d3",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100% 100%",
+          width: "100%",
+          height: "100%"
       }}
+      width= "400"
+      height= "400"
+      onClick={function pickLoc(){
+        var data = destinationPicker(event)
+        console.log(data);
+        let coords = {"data" : data};
+        try {
+          window.RoboticsExerciseComponents.commsManager.send("#pick", data)
+        } catch (error) {
+          console.error(error)
+        }
+        
+}}
     />
+    <img id="gui-canvas-numpy" width="400" height="400" style={{
+      	marginTop: "5px",
+        width: "100%",
+        height: "100%",
+        margin: "auto"
+    }}></img>
+    </div>
   );
 }
 
--- exercises/static/exercises/global_navigation_newmanager/react-components/helpers/birds_eye_global_navigation.js
+++ exercises/static/exercises/global_navigation_newmanager/react-components/helpers/birds_eye_global_navigation.js
@@ -1,75 +1,57 @@
-// Retreive the canvas elements and context
-let mapCanvas;
-let ctx;
-let initialPosition;
+// Retrieve the canvas elements and context
 
-// Complete draw function
 
-export function draw(canvas, x, y, ax, ay) {
-  mapCanvas = canvas;
-  ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
-  coords = drawTriangle(x, y, ax, ay);
+export function draw(x, y, ax, ay){
+ 
+	let coords = drawTriangle(x, y, ax, ay); // declare variable 'coords'
 }
 
+function drawTriangle(posx, posy, angx, angy){
+  var mapCanvas = document.getElementById("globalnav-eye"),
+  ctx = mapCanvas.getContext("2d");
+	ctx.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
+	ctx.beginPath();
 
-// Testing to be carried out with Python interface
-function drawTriangle(posx, posy, angx, angy) {
-  // Store initial position
-  if (initialPosition == null) {
-    initialPosition = [posx, posy, angx, angy];
-  }
+	let px = posx;
+	let py = posy;
 
-  ctx.beginPath();
+	// The main line
+	ctx.strokeStyle = '#FF0000';
+	//ctx.moveTo(px, py);
+	//ctx.lineTo(px, py);
 
-  let px = posx;
-  let py = posy;
+	// Sides
+	let side = 1.5 * Math.hypot(2, 2);
+	let ang;
 
-  // The main line
-  ctx.strokeStyle = "#FF0000";
-  //ctx.moveTo(px, py);
-  //ctx.lineTo(px, py);
+	if(angx != 0){
+		ang = Math.atan2(angy, angx);
+	}
+	else{
+		ang = Math.PI / 2;
+	}
 
-  // Sides
-  const side = 1.5 * Math.hypot(2, 2);
-  let ang;
-  if (angx !== 0) {
-    ang = Math.atan2(angy, angx);
-  } else {
-    ang = Math.PI / 2;
-  }
+	let px1 = posx + side * Math.cos(2 * Math.PI / 3 + ang);
+	let py1 = posy - side * Math.sin(2 * Math.PI / 3 + ang);
+	let px2 = posx + side * Math.cos(2 * Math.PI / 3 - ang);
+	let py2 = posy + side * Math.sin(2 * Math.PI / 3 - ang);
+	let px3 = posx + side * Math.cos(ang);
+	let py3 = posy - side * Math.sin(ang);
 
-  let px1 = posx + side * Math.cos((2 * Math.PI) / 3 + ang);
-  let py1 = posy - side * Math.sin((2 * Math.PI) / 3 + ang);
-  let px2 = posx + side * Math.cos((2 * Math.PI) / 3 - ang);
-  let py2 = posy + side * Math.sin((2 * Math.PI) / 3 - ang);
-  let px3 = posx + side * Math.cos(ang);
-  let py3 = posy - side * Math.sin(ang);
+	ctx.moveTo(px3, py3);
+	ctx.lineTo(px1, py1);
+	//ctx.moveTo(px, py);
+	ctx.lineTo(px2, py2);
+	ctx.lineTo(px3, py3);
 
-  ctx.moveTo(px3, py3);
-  ctx.lineTo(px1, py1);
-  //ctx.moveTo(px, py);
-  ctx.lineTo(px2, py2);
-  ctx.lineTo(px3, py3);
+	let rx = px;
+	let ry = py;
 
-  const rx = px;
-  const ry = py;
+	ctx.stroke();
+	ctx.closePath();
 
-  ctx.stroke();
-  ctx.closePath();
+	ctx.fillStyle = "#FF0000";
+	ctx.fill();
 
-  ctx.fillStyle = "#FF0000";
-  ctx.fill();
-
-  return [rx, ry];
-}
-
-
-export function restoreInitialPosition() {
-  draw(
-    initialPosition[0],
-    initialPosition[1],
-    initialPosition[2],
-    initialPosition[3]
-  );
+	return [rx, ry];
 }
