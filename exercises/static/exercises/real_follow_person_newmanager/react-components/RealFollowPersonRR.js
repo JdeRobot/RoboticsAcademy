23ee6069d4aeef4f0017d34fb004a18b773013d7
lucia <lucia.chen189@gmail.com>
2023-07-05 16:24:45 +0200

    update to new frontend

---
+++ exercises/static/exercises/real_follow_person_newmanager/react-components/RealFollowPersonRR.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Fragment} from "react";
+
+import "./css/RealFollowPersonRR.css";
+
+const RealFollowPersonRR = (props) => {
+  return (
+    <Fragment>
+    {props.children}
+    </Fragment>
+  );
+};
+
+export default RealFollowPersonRR;
