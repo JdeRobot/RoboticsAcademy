c959f34487282c22f7add251d01e542b8a4af903
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2023-07-14 08:33:44 +0530

    New: Rescue People New Manager

---
+++ exercises/static/exercises/rescue_people_newmanager/react-components/RescuePeopleRR.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Fragment} from "react";
+
+import "./css/RescuePeopleRR.css";
+
+const RescuePeopleRR = (props) => {
+  return (
+    <Fragment>
+    {props.children}
+    </Fragment>
+  );
+};
+
+export default RescuePeopleRR;
