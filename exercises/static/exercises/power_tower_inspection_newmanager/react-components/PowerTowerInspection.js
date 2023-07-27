b32c117ff5ab0e67dc3a8ca674a12744ed879eaf
ango1994 <ango19941994@gmail.com>
2023-07-27 11:05:47 +0200

    Add power tower inspection files

---
+++ exercises/static/exercises/power_tower_inspection_newmanager/react-components/PowerTowerInspection.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Fragment} from "react";
+
+import "./css/PowerTowerInspection.css";
+
+const PowerTowerInspection = (props) => {
+  return (
+    <Fragment>
+    {props.children}
+    </Fragment>
+  );
+};
+
+export default PowerTowerInspection;
