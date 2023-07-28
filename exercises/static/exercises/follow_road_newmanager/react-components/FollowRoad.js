19607ebee3f834401cedca5c5b6725a3267b8b7c
ango1994 <ango19941994@gmail.com>
2023-07-28 11:09:02 +0200

    Add base

---
+++ exercises/static/exercises/follow_road_newmanager/react-components/FollowRoad.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Fragment} from "react";
+
+import "./css/FollowRoad.css";
+
+const FollowRoad = (props) => {
+  return (
+    <Fragment>
+    {props.children}
+    </Fragment>
+  );
+};
+
+export default FollowRoad;
