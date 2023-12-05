0c88cc7c9447eeeb5db89c619750e74b38c820fd
ango1994 <ango19941994@gmail.com>
2023-12-05 11:14:56 +0100

    Update template autoparking

--- exercises/static/exercises/autoparking_newmanager/python_template/ros1_noetic/exercise.py
+++ exercises/static/exercises/autoparking_newmanager/python_template/ros1_noetic/exercise.py
@@ -2,9 +2,7 @@
 
 from __future__ import print_function
 
-import os
-
-from websocket_server import WebsocketServer
+import websocket
 import time
 import threading
 import subprocess
@@ -14,7 +12,6 @@ import re
 import json
 import importlib
 
-import rospy
 from std_srvs.srv import Empty
 
 from gui import GUI, ThreadGUI
@@ -40,13 +37,81 @@ class Template:
         self.real_time_factor = 0
         self.frequency_message = {'brain': '', 'gui': '',  'rtf': ''}
 
-        self.server = None
         self.client = None
         self.host = sys.argv[1]
 
         # Initialize the GUI, HAL and Console behind the scenes
         self.hal = HAL()
         self.gui = GUI(self.host, self.hal)
+        self.client = websocket.WebSocketApp('ws://127.0.0.1:1905',
+                                on_open=self.on_open,
+                                on_message=self.on_message,
+                                on_error=self.on_error,
+                                on_close=self.on_close)
+        self.client.run_forever(ping_timeout=None, ping_interval=0)
+        
+    # Function that gets called when the server is connected
+    def on_open(self, client, server):
+        self.client = client
+        # Start the GUI update thread
+        self.thread_gui = ThreadGUI(self.gui)
+        self.thread_gui.start()
+
+        # Start the real time factor tracker thread
+        self.stats_thread = threading.Thread(target=self.track_stats)
+        self.stats_thread.start()
+
+        # Start measure frequency
+        self.measure_thread = threading.Thread(target=self.measure_frequency)
+        self.measure_thread.start()
+        client.send("#con")
+
+        print(client, 'connected')
+
+        # Function that gets called when the connected closes
+    def on_close(self, client, server):
+        print(client, 'closed')
+
+    # The websocket function
+    # Gets called when there is an incoming message from the client
+    def on_message(self, ws, message):
+        if (message[:5] == "#freq"):
+            frequency_message = message[5:]
+            self.read_frequency_message(frequency_message)
+            time.sleep(1)
+            return
+
+        elif (message[:5] == "#ping"):
+            time.sleep(1)
+            self.send_ping_message()
+            return
+
+        elif (message[:5] == "#code"):
+            try:
+                # Once received turn the reload flag up and send it to execute_thread function
+                self.user_code = message[6:]
+                # print(repr(code))
+                self.reload = True
+                self.execute_thread(self.user_code)
+            except:
+                pass
+
+        elif (message[:5] == "#rest"):
+            try:
+                self.reload = True
+                self.stop_brain = True
+                self.execute_thread(self.user_code)
+            except:
+                pass
+
+        elif (message[:5] == "#stop"):
+            self.stop_brain = True
+
+        elif (message[:5] == "#play"):
+            self.stop_brain = False
+            
+    def on_error(ws, error):
+        print("Error: ", error)
 
     # Function for saving
     def save_code(self, source_code):
@@ -66,25 +131,25 @@ class Template:
     # 2. Only a single infinite loop
     def parse_code(self, source_code):
         # Check for save/load
-        if(source_code[:5] == "#save"):
+        if (source_code[:5] == "#save"):
             source_code = source_code[5:]
             self.save_code(source_code)
 
             return "", ""
 
-        elif(source_code[:5] == "#load"):
+        elif (source_code[:5] == "#load"):
             source_code = source_code + self.load_code()
-            self.server.send_message(self.client, source_code)
 
             return "", ""
 
         else:
-            sequential_code, iterative_code = self.seperate_seq_iter(source_code)
+            sequential_code, iterative_code = self.seperate_seq_iter(
+                source_code)
             return iterative_code, sequential_code
 
     # Function to parse code according to the debugging level
     def debug_parse(self, source_code, debug_level):
-        if(debug_level == 1):
+        if (debug_level == 1):
             # If debug level is 0, then all the GUI operations should not be called
             source_code = re.sub(r'GUI\..*', '', source_code)
 
@@ -96,7 +161,8 @@ class Template:
             return "", ""
 
         # Search for an instance of while True
-        infinite_loop = re.search(r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', source_code)
+        infinite_loop = re.search(
+            r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', source_code)
 
         # Seperate the content inside while True and the other
         # (Seperating the sequential and iterative part!)
@@ -107,7 +173,8 @@ class Template:
 
             # Remove while True: syntax from the code
             # And remove the the 4 spaces indentation before each command
-            iterative_code = re.sub(r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', '', iterative_code)
+            iterative_code = re.sub(
+                r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', '', iterative_code)
             # Add newlines to match line on bug report
             extra_lines = sequential_code.count('\n')
             while (extra_lines >= 0):
@@ -161,7 +228,8 @@ class Template:
             # Template specifics to run!
             finish_time = datetime.now()
             dt = finish_time - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
+                1000 + dt.microseconds / 1000.0
 
             # Keep updating the iteration counter
             if (iterative_code == ""):
@@ -181,9 +249,12 @@ class Template:
 
     def generate_modules(self):
         # Define HAL module
-        hal_module = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("HAL", None))
-        hal_module.HAL = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("HAL", None))
-        hal_module.HAL.motors = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("motors", None))
+        hal_module = importlib.util.module_from_spec(
+            importlib.machinery.ModuleSpec("HAL", None))
+        hal_module.HAL = importlib.util.module_from_spec(
+            importlib.machinery.ModuleSpec("HAL", None))
+        hal_module.HAL.motors = importlib.util.module_from_spec(
+            importlib.machinery.ModuleSpec("motors", None))
 
         # Add HAL functions
         hal_module.HAL.getPose3d = self.hal.pose3d.getPose3d
@@ -194,8 +265,10 @@ class Template:
         hal_module.HAL.setW = self.hal.motors.sendW
 
         # Define GUI module
-        gui_module = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("GUI", None))
-        gui_module.GUI = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("GUI", None))
+        gui_module = importlib.util.module_from_spec(
+            importlib.machinery.ModuleSpec("GUI", None))
+        gui_module.GUI = importlib.util.module_from_spec(
+            importlib.machinery.ModuleSpec("GUI", None))
 
         # Add GUI functions
         gui_module.GUI.map = self.gui.map
@@ -219,7 +292,8 @@ class Template:
             # Measure the current time and subtract from the previous time to get real time interval
             current_time = datetime.now()
             dt = current_time - previous_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
+                1000 + dt.microseconds / 1000.0
             previous_time = current_time
 
             # Get the time period
@@ -255,14 +329,14 @@ class Template:
         self.frequency_message["rtf"] = self.real_time_factor
 
         message = "#freq" + json.dumps(self.frequency_message)
-        self.server.send_message(self.client, message)
 
     def send_ping_message(self):
-        self.server.send_message(self.client, "#ping")
+        """pass"""
+        
 
     # Function to notify the front end that the code was received and sent to execution
     def send_code_message(self):
-        self.server.send_message(self.client, "#exec")
+        
 
     # Function to track the real time factor from Gazebo statistics
     # https://stackoverflow.com/a/17698359
@@ -283,14 +357,15 @@ class Template:
     def execute_thread(self, source_code):
         # Keep checking until the thread is alive
         # The thread will die when the coming iteration reads the flag
-        if(self.thread != None):
+        if (self.thread != None):
             while self.thread.is_alive():
                 time.sleep(0.2)
 
         # Turn the flag down, the iteration has successfully stopped!
         self.reload = False
         # New thread execution
-        self.thread = threading.Thread(target=self.process_code, args=[source_code])
+        self.thread = threading.Thread(
+            target=self.process_code, args=[source_code])
         self.thread.start()
         self.send_code_message()
         print("New Thread Started!")
@@ -309,87 +384,12 @@ class Template:
 
         return
 
-    # The websocket function
-    # Gets called when there is an incoming message from the client
-    def handle(self, client, server, message):
-        if(message[:5] == "#freq"):
-            frequency_message = message[5:]
-            self.read_frequency_message(frequency_message)
-            time.sleep(1)
-            return
-
-        elif(message[:5] == "#ping"):
-            time.sleep(1)
-            self.send_ping_message()
-            return
-
-        elif (message[:5] == "#code"):
-            try:
-                # Once received turn the reload flag up and send it to execute_thread function
-                self.user_code = message[6:]
-                # print(repr(code))
-                self.reload = True
-                self.execute_thread(self.user_code)
-            except:
-                pass
-
-        elif (message[:5] == "#rest"):
-            try:
-                self.reload = True
-                self.stop_brain = True
-                self.execute_thread(self.user_code)
-            except:
-                pass
-
-        elif (message[:5] == "#stop"):
-            self.stop_brain = True
-
-        elif (message[:5] == "#play"):
-            self.stop_brain = False
-
-    # Function that gets called when the server is connected
-    def connected(self, client, server):
-        self.client = client
-        # Start the GUI update thread
-        self.thread_gui = ThreadGUI(self.gui)
-        self.thread_gui.start()
-
-        # Start the real time factor tracker thread
-        self.stats_thread = threading.Thread(target=self.track_stats)
-        self.stats_thread.start()
-
-        # Start measure frequency
-        self.measure_thread = threading.Thread(target=self.measure_frequency)
-        self.measure_thread.start()
-
-        print(client, 'connected')
 
     # Function that gets called when the connected closes
     def handle_close(self, client, server):
         print(client, 'closed')
 
-    def run_server(self):
-        self.server = WebsocketServer(port=1905, host=self.host)
-        self.server.set_fn_new_client(self.connected)
-        self.server.set_fn_client_left(self.handle_close)
-        self.server.set_fn_message_received(self.handle)
-
-        home_dir = os.path.expanduser('~')
-
-        logged = False
-        while not logged:
-            try:
-                f = open(f"{home_dir}/ws_code.log", "w")
-                f.write("websocket_code=ready")
-                f.close()
-                logged = True
-            except:
-                time.sleep(0.1)
-
-        self.server.run_forever()
-
 
 # Execute!
 if __name__ == "__main__":
     server = Template()
-    server.run_server()
--- exercises/static/exercises/autoparking_newmanager/python_template/ros1_noetic/gui.py
+++ exercises/static/exercises/autoparking_newmanager/python_template/ros1_noetic/gui.py
@@ -1,13 +1,10 @@
 import os
 import json
-import cv2
-import base64
+
 import threading
 import time
 from datetime import datetime
-from websocket_server import WebsocketServer
-import logging
-
+import websocket
 from interfaces.pose3d import ListenerPose3d
 from interfaces.laser import ListenerLaser
 
@@ -22,7 +19,6 @@ class GUI:
     # Initialization function
     # The actual initialization
     def __init__(self, host, hal):
-        t = threading.Thread(target=self.run_server)
 
         self.payload = {'map': ''}
         self.server = None
@@ -35,14 +31,18 @@ class GUI:
 
         # Take the console object to set the same websocket and client
         self.hal = hal
-        t.start()
+        self.client = websocket.WebSocketApp('ws://127.0.0.1:1905',
+                                             on_open=self.on_open,
+                                             on_message=self.on_message)
+        self.client.run_forever(ping_timeout=None, ping_interval=0)
 
         # Create the map object
         laser_object_f = ListenerLaser("/F1ROS/laser_f/scan")
         laser_object_r = ListenerLaser("/F1ROS/laser_r/scan")
         laser_object_b = ListenerLaser("/F1ROS/laser_b/scan")
         pose3d_object = ListenerPose3d("/F1ROS/odom")
-        self.map = Map(laser_object_f, laser_object_r, laser_object_b, pose3d_object)
+        self.map = Map(laser_object_f, laser_object_r,
+                       laser_object_b, pose3d_object)
 
     # Explicit initialization function
     # Class method, so user can call it without instantiation
@@ -53,8 +53,8 @@ class GUI:
 
     # Function to get the client
     # Called when a new client is received
-    def get_client(self, client, server):
-        self.client = client
+    def on_open(self, server, client):
+        self.server = server
 
     # Function to get value of Acknowledge
     def get_acknowledge(self):
@@ -81,32 +81,13 @@ class GUI:
 
     # Function to read the message from websocket
     # Gets called when there is an incoming message from the client
-    def get_message(self, client, server, message):
+    def on_message(self, server, client, message):
         # Acknowledge Message for GUI Thread
-        if(message[:4] == "#ack"):
+        if (message[:4] == "#ack"):
             self.set_acknowledge(True)
 
-    # Activate the server
-    def run_server(self):
-        self.server = WebsocketServer(port=2303, host=self.host)
-        self.server.set_fn_new_client(self.get_client)
-        self.server.set_fn_message_received(self.get_message)
-
-        home_dir = os.path.expanduser('~')
-
-        logged = False
-        while not logged:
-            try:
-                f = open(f"{home_dir}/ws_gui.log", "w")
-                f.write("websocket_gui=ready")
-                f.close()
-                logged = True
-            except:
-                time.sleep(0.1)        
-
-        self.server.run_forever()
-
     # Function to reset
+
     def reset_gui(self):
         self.map.reset()
 
@@ -134,18 +115,19 @@ class ThreadGUI:
 
     # The measuring thread to measure frequency
     def measure_thread(self):
-        while(self.gui.client == None):
+        while (self.gui.client == None):
             pass
 
         previous_time = datetime.now()
-        while(True):
+        while (True):
             # Sleep for 2 seconds
             time.sleep(2)
 
             # Measure the current time and subtract from previous time to get real time interval
             current_time = datetime.now()
             dt = current_time - previous_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
+                1000 + dt.microseconds / 1000.0
             previous_time = current_time
 
             # Get the time period
@@ -160,15 +142,15 @@ class ThreadGUI:
 
     # The main thread of execution
     def run(self):
-        while(self.gui.client == None):
+        while (self.gui.client == None):
             pass
 
-        while(True):
+        while (True):
             start_time = datetime.now()
             self.gui.update_gui()
             acknowledge_message = self.gui.get_acknowledge()
 
-            while(acknowledge_message == False):
+            while (acknowledge_message == False):
                 acknowledge_message = self.gui.get_acknowledge()
 
             self.gui.set_acknowledge(False)
@@ -177,6 +159,7 @@ class ThreadGUI:
             self.iteration_counter = self.iteration_counter + 1
 
             dt = finish_time - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-            if(ms < self.ideal_cycle):
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
+                1000 + dt.microseconds / 1000.0
+            if (ms < self.ideal_cycle):
                 time.sleep((self.ideal_cycle-ms) / 1000.0)
