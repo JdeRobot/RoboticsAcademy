e42b7c4d519de2e7f943475879e853a86d23673a
ango1994 <ango19941994@gmail.com>
2023-12-15 15:49:13 +0100

    Remove unnecessary arguments

--- exercises/static/exercises/autoparking_newmanager/entry_point/exercise.py
+++ exercises/static/exercises/autoparking_newmanager/entry_point/exercise.py
@@ -5,12 +5,10 @@ from src.manager.libs.applications.compatibility.exercise_wrapper import Compati
 
 
 class Exercise(CompatibilityExerciseWrapper):
-    def __init__(self, circuit: str, exercise_server: Any, gui_server: Any, update_callback: Callable):
+    def __init__(self,  gui_server: Any, update_callback: Callable):
         current_path = os.path.dirname(__file__)
 
         super(Exercise, self).__init__(exercise_command=f"{current_path}/../python_template/ros1_noetic/exercise.py",
-                                       gui_command=f"{current_path}/../python_template/ros1_noetic/gui.py 0.0.0.0 {circuit}",
                                        update_callback=update_callback,
-                                       exercise_server=exercise_server,
                                        gui_server=gui_server
                                        )
