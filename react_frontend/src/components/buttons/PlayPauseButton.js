56e56e7ee3176c2c593d110e38498aecb634a01b
ango1994 <ango19941994@gmail.com>
2023-12-22 14:27:13 +0100

    Update playPause Button

--- react_frontend/src/components/buttons/PlayPauseButton.js
+++ react_frontend/src/components/buttons/PlayPauseButton.js
@@ -1,19 +1,24 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
 
 const PlayPause = (props) => {
   const [loading, setLoading] = useState(false);
-  const [paused, setPaused] = useState(true);
+  const [applicationRunning, setApplicationRunning] = useState(true);
+  const [applicationPaused, setApplicationPaused] = useState(false)
   const [disabled, setDisabled] = useState(true);
   const [editorChanged, setEditorChanged] = useState(false)
+
+  const commsManager = window.RoboticsExerciseComponents.commsManager;
+  const config = JSON.parse(document.getElementById("exercise-config").textContent);
 
   useEffect(() => {
     const callback = (message) => {
       const state = message.data.state;
-      setPaused(
-        state === "paused" 
+      setApplicationPaused(state === "paused")
+      setApplicationRunning(
+        state === "application_running" 
       );
       setDisabled(
         !(
@@ -24,64 +29,58 @@ const PlayPause = (props) => {
       );
     };
 
-    window.RoboticsExerciseComponents.commsManager.subscribe(
-      [window.RoboticsExerciseComponents.commsManager.events.STATE_CHANGED],
+    commsManager.subscribe(
+      [commsManager.events.STATE_CHANGED],
       callback
     );
 
     return () => {
-      window.RoboticsExerciseComponents.commsManager.unsubscribe(
-        [window.RoboticsExerciseComponents.commsManager.events.STATE_CHANGED],
+      commsManager.unsubscribe(
+        [commsManager.events.STATE_CHANGED],
         callback
       );
     };
   }, []);
 
+  React.useEffect(() => {
+    RoboticsReactComponents.CodeEditor.OnEditorCodeChanged(() => {
+      setEditorChanged(true)
+    }
+    )
+  }, []);
+
   const play = () => {
-    console.log(`Play run`);
     setLoading(true);
     const editorCode = RoboticsReactComponents.CodeEditor.getCode();
-    if (editorChanged && paused){
-      window.RoboticsExerciseComponents.commsManager
+    if (!editorChanged && applicationPaused){
+      commsManager
       .resume()
       
     } else {
       runCode(editorCode)
     }
-  
+    setLoading(false)
   };
 
-  const config = JSON.parse(
-    document.getElementById("exercise-config").textContent)
-
   const runCode = (code) => {
-    window.RoboticsExerciseComponents.commsManager
+    window.RoboticsExerciseComponents.commsManager.terminate().then(() => {
+      window.RoboticsExerciseComponents.commsManager
       .run({code: code, template:config[0].template, exercise_id: config[0].exercise_id})
       .then(() => {
-        console.log("running");
-        setLoading(false);
       })
       .catch((response) => {
         let linterMessage = JSON.stringify(response.data.message).split("\\n");
         RoboticsReactComponents.MessageSystem.Alert.showAlert(linterMessage);
         console.log(`Received linter message ·${linterMessage}`);
-        setLoading(false);
       });
+    })
   };
 
-  React.useEffect(() => {
-    RoboticsReactComponents.CodeEditor.OnEditorCodeChanged(() => setEditorChanged(true)
-    )
-  }, []);
-
   const pause = () => {
-    console.log(`Pause run`);
     setLoading(true);
-
     window.RoboticsExerciseComponents.commsManager
       .pause()
       .then(() => {
-        console.log("paused");
       })
       .catch((response) => console.log(response))
       .finally(() => setLoading(false));
@@ -93,11 +92,11 @@ const PlayPause = (props) => {
       id={"loadIntoRobot"}
       loading={loading}
       color={"secondary"}
-      onClick={paused ? play : pause}
+      onClick={applicationRunning ? pause :  play }
       sx={{ m: 0.5 }}
       variant={"outlined"}
     >
-      {paused ? <PlayArrowIcon /> : <PauseIcon />}
+      {applicationRunning ? <PauseIcon /> : <PlayArrowIcon />}
     </LoadingButton>
   );
 };
