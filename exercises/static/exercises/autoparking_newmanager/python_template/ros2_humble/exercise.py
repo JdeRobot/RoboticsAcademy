bca93f780b09781077c747e1452541c2654badc5
ReyDoran <manetoscopio@gmail.com>
2024-01-02 11:46:25 +0100

    Slim template autoparking ROS2

--- exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/exercise.py
+++ exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/exercise.py
@@ -23,246 +23,51 @@ class Template:
     # self.ideal_cycle to run an execution for atleast 1 second
     # self.process for the current running process
     def __init__(self):
-        self.measure_thread = None
         self.thread = None
-        self.reload = False
-        self.stop_brain = False
-        self.user_code = ""
-
-        # Time variables
-        self.ideal_cycle = 20
 
         # Initialize the GUI, HAL and Console behind the scenes
         self.hal = HAL()
         self.gui = GUI("0.0.0.0", self.hal)
-        self.execute_thread()
+        self.execute_user_code()
 
-    ################ --- BRAIN --- ################
+    def add_frequency_control(self, code):
+        frequency_control_code_imports = """
+from datetime import datetime
+ideal_cycle = 20
+"""
+        code = frequency_control_code_imports + code
+        infinite_loop = re.search(
+            r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', code)        
+        frequency_control_code_pre = """
+    start_time = datetime.now()
+            """
+        code = code[:infinite_loop.end()] + frequency_control_code_pre + code[infinite_loop.end():]
+        frequency_control_code_post = """
+    finish_time = datetime.now()
+    dt = finish_time - start_time
+    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
 
-    # The process function
+    if (ms < ideal_cycle):
+        time.sleep((ideal_cycle - ms) / 1000.0)
+"""        
+        code = code + frequency_control_code_post
+        return code
+
+
     def process_code(self, source_code):
-
-        # Redirect the information to console
+        """
+        Processes the provided source code, executing the sequential part first and then the iterative part.
+        """
         start_console()
 
-        # Reference Environment for the exec() function
-        iterative_code, sequential_code = self.parse_code(source_code)
-
-        # Whatever the code is, first step is to just stop!
         self.hal.motors.sendV(0)
         self.hal.motors.sendW(0)
 
-        # print("The debug level is " + str(debug_level)
-        # print(sequential_code)
-        # print(iterative_code)
-
-        # The Python exec function
-        # Run the sequential part
-        gui_module, hal_module = self.generate_modules()
-        reference_environment = {"GUI": gui_module, "HAL": hal_module}
-        exec(sequential_code, reference_environment)
-
-        # Run the iterative part inside template
-        # and keep the check for flag
-        while not self.reload:
-            while (self.stop_brain):
-                if (self.reload):
-                    break
-                time.sleep(0.1)
-
-            start_time = datetime.now()
-
-            # Execute the iterative portion
-            exec(iterative_code, reference_environment)
-
-            # Template specifics to run!
-            finish_time = datetime.now()
-            dt = finish_time - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
-                1000 + dt.microseconds / 1000.0
-
-            # Keep updating the iteration counter
-            if (iterative_code == ""):
-                self.iteration_counter = 0
-            else:
-                self.iteration_counter = self.iteration_counter + 1
-
-            # The code should be run for atleast the target time step
-            # If it's less put to sleep
-            if (ms < self.ideal_cycle):
-                time.sleep((self.ideal_cycle - ms) / 1000.0)
-
-        close_console()
-        print("Current Thread Joined!")
-
-    # Function to generate the modules for use in ACE Editor
-
-    def generate_modules(self):
-        # Define HAL module
-        hal_module = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("HAL", None))
-        hal_module.HAL = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("HAL", None))
-        hal_module.HAL.motors = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("motors", None))
-
-        # Add HAL functions
-        hal_module.HAL.getPose3d = self.hal.pose3d.getPose3d
-        hal_module.HAL.getFrontLaserData = self.hal.laser_front.getLaserData
-        hal_module.HAL.getRightLaserData = self.hal.laser_right.getLaserData
-        hal_module.HAL.getBackLaserData = self.hal.laser_back.getLaserData
-        hal_module.HAL.setV = self.hal.motors.sendV
-        hal_module.HAL.setW = self.hal.motors.sendW
-
-        # Define GUI module
-        gui_module = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("GUI", None))
-        gui_module.GUI = importlib.util.module_from_spec(
-            importlib.machinery.ModuleSpec("GUI", None))
-
-        # Add GUI functions
-        gui_module.GUI.map = self.gui.map
-
-        # Adding modules to system
-        # Protip: The names should be different from
-        # other modules, otherwise some errors
-        sys.modules["HAL"] = hal_module
-        sys.modules["GUI"] = gui_module
-
-        return gui_module, hal_module
-
-    # Function to measure the frequency of iterations
-    def measure_frequency(self):
-        previous_time = datetime.now()
-        # An infinite loop
-        while True:
-            # Sleep for 2 seconds
-            time.sleep(2)
-
-            # Measure the current time and subtract from the previous time to get real time interval
-            current_time = datetime.now()
-            dt = current_time - previous_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
-                1000 + dt.microseconds / 1000.0
-            previous_time = current_time
-
-            # Get the time period
-            try:
-                # Division by zero
-                self.measured_cycle = ms / self.iteration_counter
-            except:
-                self.measured_cycle = 0
-
-            # Reset the counter
-            self.iteration_counter = 0
-
-            # Send to client
-            self.send_frequency_message()
-
-
-    ################ --- EXERCISE --- ################
-
-    # Function for saving
-    def save_code(self, source_code):
-        with open('code/academy.py', 'w') as code_file:
-            code_file.write(source_code)
-
-    # Function for loading
-    def load_code(self):
-        with open('code/academy.py', 'r') as code_file:
-            source_code = code_file.read()
-
-        return source_code
-
-    # Function to parse the code
-    # A few assumptions:
-    # 1. The user always passes sequential and iterative codes
-    # 2. Only a single infinite loop
-    def parse_code(self, source_code):
-        """
-        Parses the provided source code, separating the sequential and iterative parts.
-        """
-        sequential_code, iterative_code = self.seperate_seq_iter(source_code)
-        return iterative_code, sequential_code
-
-    # Function to parse code according to the debugging level
-    def debug_parse(self, source_code, debug_level):
-        if (debug_level == 1):
-            # If debug level is 0, then all the GUI operations should not be called
-            source_code = re.sub(r'GUI\..*', '', source_code)
-
-        return source_code
-
-    # Function to seperate the iterative and sequential code
-    def seperate_seq_iter(self, source_code):
-        """
-        Separates the source code into sequential and iterative components.
-        """
-        if source_code == "":
-            return "", ""
-
-        infinite_loop = re.search(
-            r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', source_code)
-        try:
-            start_index = infinite_loop.start()
-            iterative_code = source_code[start_index:]
-            sequential_code = source_code[:start_index]
-
-            iterative_code = re.sub(
-                r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', '', iterative_code)
-            extra_lines = sequential_code.count('\n')
-            while (extra_lines >= 0):
-                iterative_code = '\n' + iterative_code
-            return sequential_code, iterative_code
-        except Exception as e:
-            return e
-
-    # Function to generate and send frequency messages
-    def send_frequency_message(self):
-        # This function generates and sends frequency measures of the brain and gui
-        brain_frequency = 0
-        gui_frequency = 0
-        try:
-            brain_frequency = round(1000 / self.measured_cycle, 1)
-        except ZeroDivisionError:
-            brain_frequency = 0
-
-        try:
-            gui_frequency = round(1000 / self.thread_gui.measured_cycle, 1)
-        except ZeroDivisionError:
-            gui_frequency = 0
-
-        self.frequency_message["brain"] = brain_frequency
-        self.frequency_message["gui"] = gui_frequency
-        self.frequency_message["rtf"] = self.real_time_factor
-
-        message = "#freq" + json.dumps(self.frequency_message)
-        self.server.send_message(self.client, message)
-
-    def send_ping_message(self):
-        self.server.send_message(self.client, "#ping")
-
-    # Function to notify the front end that the code was received and sent to execution
-    def send_code_message(self):
-        self.server.send_message(self.client, "#exec")
-
-    # Function to track the real time factor from Gazebo statistics
-    # https://stackoverflow.com/a/17698359
-    # (For reference, Python3 solution specified in the same answer)
-    def track_stats(self):
-        args = ["gz", "stats", "-p"]
-        # Prints gz statistics. "-p": Output comma-separated values containing-
-        # real-time factor (percent), simtime (sec), realtime (sec), paused (T or F)
-        stats_process = subprocess.Popen(args, stdout=subprocess.PIPE)
-        # bufsize=1 enables line-bufferred mode (the input buffer is flushed
-        # automatically on newlines if you would write to process.stdin )
-        with stats_process.stdout:
-            for line in iter(stats_process.stdout.readline, b''):
-                stats_list = [x.strip() for x in line.split(b',')]
-                self.real_time_factor = stats_list[0].decode("utf-8")
+        processed_code = self.add_frequency_control(source_code)
+        exec(processed_code)
 
     # Function to maintain thread execution
-    def execute_thread(self):
+    def execute_user_code(self):
         """
         Executes the provided user code, initializing the graphical interface and processing the code.
         """
@@ -275,21 +80,6 @@ class Template:
             target=self.process_code, args=[source_code])
         self.thread.start()
         print("Código del usuario en ejecución.")
-
-    # Function to read and set frequency from incoming message
-    def read_frequency_message(self, message):
-        frequency_message = json.loads(message)
-
-        # Set brain frequency
-        frequency = float(frequency_message["brain"])
-        self.ideal_cycle = 1000.0 / frequency
-
-        # Set gui frequency
-        frequency = float(frequency_message["gui"])
-        self.thread_gui.ideal_cycle = 1000.0 / frequency
-
-        return
-
 
 # Execute!
 if __name__ == "__main__":
--- exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/hal.py
+++ exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/hal.py
@@ -13,50 +13,38 @@ from interfaces.laser import ListenerLaser
 class HAL:
     IMG_WIDTH = 320
     IMG_HEIGHT = 240
-    
-    def __init__(self):
-        rclpy.init(args=sys.argv)
-        rclpy.create_node('HAL')
 
-        self.motors = PublisherMotors("/prius_autoparking/cmd_vel", 4, 0.3)
-        self.laser_front = ListenerLaser("/prius_autoparking/scan_front")
-        self.laser_right = ListenerLaser("/prius_autoparking/scan_side")
-        self.laser_back = ListenerLaser("/prius_autoparking/scan_back")
-        self.pose3d = ListenerPose3d("/prius_autoparking/odom")
+    rclpy.init(args=sys.argv)
+    rclpy.create_node('HAL')
 
-        # Spin nodes so that subscription callbacks load topic data
-        # Bumper has to be spinned differently so that GetEntityState works
-        executor = rclpy.executors.MultiThreadedExecutor()
-        executor.add_node(self.pose3d)
-        executor.add_node(self.laser_front)
-        executor.add_node(self.laser_right)
-        executor.add_node(self.laser_back)
-        executor_thread = threading.Thread(target=executor.spin, daemon=True)
-        executor_thread.start()
-
-        print("HAL-Nodes Thread Started")
+    motors = PublisherMotors("/prius_autoparking/cmd_vel", 4, 0.3)
+    laser_front = ListenerLaser("/prius_autoparking/scan_front")
+    laser_right = ListenerLaser("/prius_autoparking/scan_side")
+    laser_back = ListenerLaser("/prius_autoparking/scan_back")
+    pose3d = ListenerPose3d("/prius_autoparking/odom")
 
     # Explicit initialization functions
     # Class method, so user can call it without instantiation
-    @classmethod
-    def initRobot(cls):
-        new_instance = cls()
-        return new_instance
-    
-    def getPose3d(self):
-        return self.pose3d.getPose3d()
+    @staticmethod
+    def getPose3d():
+        return HAL.pose3d.getPose3d()
 
-    def getFrontLaserData(self):
-        return self.laser_front.getLaserData()
+    @staticmethod
+    def getFrontLaserData():
+        return HAL.laser_front.getLaserData()
 
-    def getRightLaserData(self):
-        return self.laser_right.getLaserData()
+    @staticmethod
+    def getRightLaserData():
+        return HAL.laser_right.getLaserData()
 
-    def getBackLaserData(self):
-        return self.laser_back.getLaserData()
+    @staticmethod
+    def getBackLaserData():
+        return HAL.laser_back.getLaserData()
 
-    def setV(self, velocity):
-        self.motors.sendV(velocity)
+    @staticmethod
+    def setV(velocity):
+        HAL.motors.sendV(velocity)
 
-    def setW(self, velocity):
-        self.motors.sendW(velocity)
+    @staticmethod
+    def setW(velocity):
+        HAL.motors.sendW(velocity)
