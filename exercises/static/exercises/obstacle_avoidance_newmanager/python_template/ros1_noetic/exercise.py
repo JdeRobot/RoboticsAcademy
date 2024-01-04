f38d7910caba252090b2f28067ef0d98a2394498
ango1994 <ango19941994@gmail.com>
2024-01-04 12:25:07 +0100

    Add obstacle avoidance

--- exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros1_noetic/exercise.py
+++ exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros1_noetic/exercise.py
@@ -1,73 +1,179 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+
+
 import time
 import threading
 import sys
 from datetime import datetime
 import re
-import subprocess
 import importlib
-import os
-from exercises.static.exercises.obstacle_avoidance_newmanager.python_template.ros1_noetic.gui import GUI, ThreadGUI
-from exercises.static.exercises.obstacle_avoidance_newmanager.python_template.ros1_noetic.hal import HAL
+
+from gui import GUI, ThreadGUI
+from hal import HAL
 from console import start_console, close_console
 
-class Template:
-    """
-    Main class for executing and controlling user code.
-    """
 
+class Template:
+    # Initialize class variables
+    # self.ideal_cycle to run an execution for atleast 1 second
+    # self.process for the current running process
     def __init__(self):
-        """
-        Initializes the Template class, setting up the environment for user code execution.
-        """
         self.measure_thread = None
         self.thread = None
         self.reload = False
         self.stop_brain = False
         self.user_code = ""
         self.ideal_cycle = 20
+        self.iteration_counter = 0
 
+        # Initialize the GUI, HAL and Console behind the scenes
         self.hal = HAL()
         self.gui = GUI("0.0.0.0", self.hal)
         self.execute_user_code()
 
-    def add_frequency_control(self, code):
-        frequency_control_code_imports = """
-from datetime import datetime
-ideal_cycle = 20
-"""
-        code = frequency_control_code_imports + code
+    # Function to parse the code
+    # A few assumptions:
+    # 1. The user always passes sequential and iterative codes
+    # 2. Only a single infinite loop
+    def parse_code(self, source_code):
+        """
+        Parses the provided source code, separating the sequential and iterative parts.
+        """
+        sequential_code, iterative_code = self.seperate_seq_iter(source_code)
+        return iterative_code, sequential_code
+
+    # Function to parse code according to the debugging level
+    def debug_parse(self, source_code, debug_level):
+        if (debug_level == 1):
+            # If debug level is 0, then all the GUI operations should not be called
+            source_code = re.sub(r'GUI\..*', '', source_code)
+
+        return source_code
+
+    # Function to seperate the iterative and sequential code
+    def seperate_seq_iter(self, source_code):
+        if source_code == "":
+            return "", ""
+
+        # Search for an instance of while True
         infinite_loop = re.search(
-            r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', code)        
-        frequency_control_code_pre = """
-    start_time = datetime.now()
-            """
-        code = code[:infinite_loop.end()] + frequency_control_code_pre + code[infinite_loop.end():]
-        frequency_control_code_post = """
-    finish_time = datetime.now()
-    dt = finish_time - start_time
-    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', source_code)
 
-    if (ms < ideal_cycle):
-        time.sleep((ideal_cycle - ms) / 1000.0)
-"""        
-        code = code + frequency_control_code_post
-        return code
+        # Seperate the content inside while True and the other
+        # (Seperating the sequential and iterative part!)
+        try:
+            start_index = infinite_loop.start()
+            iterative_code = source_code[start_index:]
+            sequential_code = source_code[:start_index]
 
+            # Remove while True: syntax from the code
+            # And remove the the 4 spaces indentation before each command
+            iterative_code = re.sub(
+                r'[^ ]while\s*\(\s*True\s*\)\s*:|[^ ]while\s*True\s*:|[^ ]while\s*1\s*:|[^ ]while\s*\(\s*1\s*\)\s*:', '', iterative_code)
+            # Add newlines to match line on bug report
+            extra_lines = sequential_code.count('\n')
+            while (extra_lines >= 0):
+                iterative_code = '\n' + iterative_code
+                extra_lines -= 1
+            iterative_code = re.sub(r'^[ ]{4}', '', iterative_code, flags=re.M)
+
+        except:
+            sequential_code = source_code
+            iterative_code = ""
+
+        return sequential_code, iterative_code
+
+    # The process function
 
     def process_code(self, source_code):
-        """
-        Processes the provided source code, executing the sequential part first and then the iterative part.
-        """
+
+        # Redirect the information to console
         start_console()
 
+        # Reference Environment for the exec() function
+        iterative_code, sequential_code = self.parse_code(source_code)
+
+        # Whatever the code is, first step is to just stop!
         self.hal.motors.sendV(0)
         self.hal.motors.sendW(0)
 
-        processed_code = self.add_frequency_control(source_code)
-        exec(processed_code)
+        # print("The debug level is " + str(debug_level)
+        # print(sequential_code)
+        # print(iterative_code)
+
+        # The Python exec function
+        # Run the sequential part
+        gui_module, hal_module = self.generate_modules()
+        reference_environment = {"GUI": gui_module, "HAL": hal_module}
+        exec(sequential_code, reference_environment)
+
+        # Run the iterative part inside template
+        # and keep the check for flag
+        while self.reload == False:
+            while (self.stop_brain == True):
+                if (self.reload == True):
+                    break
+                time.sleep(0.1)
+
+            start_time = datetime.now()
+
+            # Execute the iterative portion
+            exec(iterative_code, reference_environment)
+
+            # Template specifics to run!
+            finish_time = datetime.now()
+            dt = finish_time - start_time
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * \
+                1000 + dt.microseconds / 1000.0
+
+            # Keep updating the iteration counter
+            if (iterative_code == ""):
+                self.iteration_counter = 0
+            else:
+                self.iteration_counter = self.iteration_counter + 1
+
+            # The code should be run for atleast the target time step
+            # If it's less put to sleep
+            if (ms < self.ideal_cycle):
+                time.sleep((self.ideal_cycle - ms) / 1000.0)
+
+        close_console()
+        print("Current Thread Joined!")
+
+    # Function to generate the modules for use in ACE Editor
+
+    def generate_modules(self):
+        # Define HAL module
+        hal_module = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("HAL", None))
+        hal_module.HAL = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("HAL", None))
+        hal_module.HAL.motors = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("motors", None))
+
+        # Add HAL functions
+        hal_module.HAL.getPose3d = self.hal.pose3d.getPose3d
+        hal_module.HAL.getLaserData = self.hal.laser.getLaserData
+        hal_module.HAL.getImage = self.hal.getImage
+        hal_module.HAL.setV = self.hal.motors.sendV
+        hal_module.HAL.setW = self.hal.motors.sendW
+
+        # Define GUI module
+        gui_module = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("GUI", None))
+        gui_module.GUI = importlib.util.module_from_spec(importlib.machinery.ModuleSpec("GUI", None))
+
+        # Add GUI functions
+        gui_module.GUI.showImage = self.gui.showImage
+        gui_module.GUI.map = self.gui.map
+        gui_module.GUI.showForces = self.gui.showForces
+        gui_module.GUI.showLocalTarget = self.gui.showLocalTarget
+
+        # Adding modules to system
+        # Protip: The names should be different from
+        # other modules, otherwise some errors
+        sys.modules["HAL"] = hal_module
+        sys.modules["GUI"] = gui_module
+
+        return gui_module, hal_module
 
     def execute_user_code(self):
         """
@@ -84,5 +190,6 @@ ideal_cycle = 20
         self.thread.start()
         print("Código del usuario en ejecución.")
 
+# Execute!
 if __name__ == "__main__":
     server = Template()
--- exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros1_noetic/gui.py
+++ exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros1_noetic/gui.py
@@ -5,41 +5,50 @@ import threading
 import time
 from datetime import datetime
 import subprocess
+
 import websocket
 import os
 
 from interfaces.pose3d import ListenerPose3d
 from interfaces.laser import ListenerLaser
 
-from exercises.static.exercises.obstacle_avoidance_newmanager.python_template.ros1_noetic.lap import Lap
-from exercises.static.exercises.obstacle_avoidance_newmanager.python_template.ros1_noetic.map import Map
+from lap import Lap
+from map import Map
 
 # Graphical User Interface Class
 
 
 class GUI:
-    laser_object = ListenerLaser("/F1ROS/laser/scan")
-    pose3d_object = ListenerPose3d("/F1ROS/odom")
-    map = Map(laser_object, pose3d_object)
-    image_to_be_shown = None
-    image_to_be_shown_updated = False
-    image_show_lock = threading.Lock()
+    map = None
 
     # Initialization function
     # The actual initialization
     def __init__(self, host, hal):
+   
+
         self.payload = {'image': '', 'lap': '', 'map': ''}
+    
         self.client = None
 
-        # Image variables
+        self.host = host
 
+        # Image variables
+        self.image_to_be_shown = None
+        self.image_to_be_shown_updated = False
+        self.image_show_lock = threading.Lock()
 
         self.acknowledge = False
         self.acknowledge_lock = threading.Lock()
 
         # Take the console object to set the same websocket and client
         self.hal = hal
-   
+
+
+        # Create the map object
+        laser_object = ListenerLaser("/F1ROS/laser/scan")
+        pose3d_object = ListenerPose3d("/F1ROS/odom")
+        self.map = Map(laser_object, pose3d_object)
+
         # Create the lap object
         self.lap = Lap(self.map)
 
@@ -53,6 +62,7 @@ class GUI:
             self.client.run_forever(ping_timeout=None, ping_interval=0)
 
 
+
     # Explicit initialization function
     # Class method, so user can call it without instantiation
     @classmethod
@@ -63,15 +73,15 @@ class GUI:
     # Function to prepare image payload
     # Encodes the image as a JSON string and sends through the WS
     def payloadImage(self):
-        GUI.image_show_lock.acquire()
-        image_to_be_shown_updated = GUI.image_to_be_shown_updated
-        image_to_be_shown = GUI.image_to_be_shown
-        GUI.image_show_lock.release()
+        self.image_show_lock.acquire()
+        image_to_be_shown_updated = self.image_to_be_shown_updated
+        image_to_be_shown = self.image_to_be_shown
+        self.image_show_lock.release()
 
         image = image_to_be_shown
         payload = {'image': '', 'shape': ''}
 
-        if image_to_be_shown_updated == False:
+        if(image_to_be_shown_updated == False):
             return payload
 
         shape = image.shape
@@ -81,31 +91,28 @@ class GUI:
         payload['image'] = encoded_image.decode('utf-8')
         payload['shape'] = shape
 
-        GUI.image_show_lock.acquire()
-        GUI.image_to_be_shown_updated = False
-        GUI.image_show_lock.release()
+        self.image_show_lock.acquire()
+        self.image_to_be_shown_updated = False
+        self.image_show_lock.release()
 
         return payload
 
     # Function for student to call
-    @staticmethod
-    def showForces(vec1, vec2, vec3):
-        GUI.map.setCar(vec1[0], vec1[1])
-        GUI.map.setObs(vec2[0], vec2[1])
-        GUI.map.setAvg(vec3[0], vec3[1])
+    def showForces(self, vec1, vec2, vec3):
+        self.map.setCar(vec1[0], vec1[1])
+        self.map.setObs(vec2[0], vec2[1])
+        self.map.setAvg(vec3[0], vec3[1])
     
     # Function for student to call
-    @staticmethod
-    def showLocalTarget(newVec):
-        GUI.map.setTargetPos(newVec[0], newVec[1])
+    def showLocalTarget(self, newVec):
+        self.map.setTargetPos(newVec[0], newVec[1])
 
     # Function for student to call
-    @staticmethod
-    def showImage(image):
-        GUI.image_show_lock.acquire()
-        GUI.image_to_be_shown = image
-        GUI.image_to_be_shown_updated = True
-        GUI.image_show_lock.release()
+    def showImage(self, image):
+        self.image_show_lock.acquire()
+        self.image_to_be_shown = image
+        self.image_to_be_shown_updated = True
+        self.image_show_lock.release()
 
     # Function to get value of Acknowledge
     def get_acknowledge(self):
@@ -137,12 +144,19 @@ class GUI:
         map_message = self.map.get_json_data()
         self.payload["map"] = map_message
 
-        message =json.dumps(self.payload)
+        message = json.dumps(self.payload)
         if self.client:
             try:
                 self.client.send(message)
             except Exception as e:
                 print(f"Error sending message: {e}")
+
+    # Function to read the message from websocket
+    # Gets called when there is an incoming message from the client
+    def get_message(self, client, server, message):
+        # Acknowledge Message for GUI Thread
+        if(message[:4] == "#ack"):
+            self.set_acknowledge(True)
 
     def on_message(self, ws, message):
         """Handles incoming messages from the websocket client."""
@@ -151,13 +165,16 @@ class GUI:
 
 
 
+    # Function to reset
+    def reset_gui(self):
+        self.lap.reset()
+        self.map.reset()
+
+
 # This class decouples the user thread
 # and the GUI update thread
 class ThreadGUI:
-    """Class to manage GUI updates and frequency measurements in separate threads."""
-
     def __init__(self, gui):
-        """Initializes the ThreadGUI with a reference to the GUI instance."""
         self.gui = gui
         self.ideal_cycle = 80
         self.real_time_factor = 0
@@ -165,6 +182,7 @@ class ThreadGUI:
         self.iteration_counter = 0
         self.running = True
 
+    # Function to start the execution of threads
     def start(self):
         """Starts the GUI, frequency measurement, and real-time factor threads."""
         self.frequency_thread = threading.Thread(target=self.measure_and_send_frequency)
@@ -201,7 +219,15 @@ class ThreadGUI:
             gui_frequency = round(1000 / self.ideal_cycle, 1)
             self.frequency_message = {'brain': brain_frequency, 'gui': gui_frequency, 'rtf': self.real_time_factor}
             message = json.dumps(self.frequency_message)
+            if self.gui.client:
+                try:
+                    self.gui.client.send(message)
+                except Exception as e:
+                    print(f"Error sending frequency message: {e}")
 
+
+
+    # The main thread of execution
     def run(self):
         """Main loop to update the GUI at regular intervals."""
         while self.running:
