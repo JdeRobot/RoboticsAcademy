b6e10b53a01906841b2a52e12a6cd7387998e1a9
Óscar Martínez <oscar.personal@tutanota.com>
2024-02-23 11:41:51 +0100

    Removing testing manager added by error

--- src/manager/libs/singleton.py
+++
@@ -1,8 +0,0 @@
-def singleton(cls):
-    instances = {}
-
-    def get_instance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
-    return get_instance()
