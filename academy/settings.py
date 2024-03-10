ff77d1cd06c67b1ecdab088673f49f9a7217f517
Óscar Martínez <oscar.personal@tutanota.com>
2024-03-10 20:15:02 +0100

    Cleaning unused files

--- academy/settings.py
+++ academy/settings.py
@@ -134,11 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, 'react_frontend/static'),    # React frontend statics
 )
 
