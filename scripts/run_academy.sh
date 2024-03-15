fd7081d6462eaa09398cb3c2e7663a1191501e02
Óscar Martínez Martínez <oscar.robotics@tutanota.com>
2024-03-15 13:28:04 +0100

    Update run_academy.sh

--- scripts/run_academy.sh
+++ scripts/run_academy.sh
@@ -1,3 +1,3 @@
-'#!/bin/sh'
+#!/bin/sh
 
 curl -sL https://raw.githubusercontent.com/JdeRobot/RoboticsAcademy/superthin-dev/docker-compose-user.yml -o docker-compose.yml && docker-compose up; docker-compose down
