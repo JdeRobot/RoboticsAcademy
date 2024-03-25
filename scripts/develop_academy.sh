fee61cdb4a154039642b024d07eefac0b3b93437
Óscar Martínez <oscar.personal@tutanota.com>
2024-03-25 21:12:09 +0100

    New automatic script

--- scripts/develop_academy.sh
+++ scripts/develop_academy.sh
@@ -1,9 +1,48 @@
 #!/bin/sh
 
-# Check if docker-compose is installed
+# Initialize variables with default values
+version="https://github.com/JdeRobot/RoboticsApplicationManager.git"
+branch=main
+
+# Loop through the arguments using a while loop
+while getopts "rb" opt; do
+  case $opt in
+    l) version="$OPTARG" ;;   # If -l option is provided, set list to true
+    b) branch="$OPTARG" ;;   # If -b option is provided, set boolean to true
+    \?) echo "Invalid option: -$OPTARG" >&2 ;;   # If an invalid option is provided, print an error message
+  esac
+done
+
+echo "RAM src: $version"
+echo "RAM branch: $branch"
+
+# Install docker-compose if not installed
 if ! command -v docker-compose &> /dev/null; then
-    sudo apt install docker-compose
+  sudo apt install docker-compose
 fi
+
+# Clone the desired RAM fork and branch
+if ! [ -d src ]; then
+  git clone $version -b $branch src;
+  chown -R $(id -u):$(id -g) src/
+fi
+
+# Prepare nvm
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+if ! command -v nvm &> /dev/null; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  export NVM_DIR=$HOME/.nvm;
+  source $NVM_DIR/nvm.sh;
+fi
+
+# Prepare the frontend
+nvm install 16
+nvm use 16
+cd react_frontend/
+yarn install
+yarn build
+cd ..
 
 # Proceed with docker-compose commands
 docker-compose up; 
