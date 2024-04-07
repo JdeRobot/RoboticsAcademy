44fabe686843aaf38922a0e32e04237262e05a5d
pawanw17 <pawanw17@gmail.com>
2024-04-07 17:16:15 +0530

    Amazon warehouse migration to superthin

--- exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/GUI.py
@@ -1,69 +1,66 @@
 import json
-import os
 import re
 import threading
 import time
 from datetime import datetime
 import websocket
-import subprocess
+import rclpy
+from console import start_console
 
 from map import Map
+from interfaces.pose3d import ListenerPose3d
 
 # Graphical User Interface Class
 class GUI:
     # Initialization function
     # The actual initialization
-    def __init__(self, host, hal):
+    def __init__(self, host):
+
+        # ROS2 init
+        rclpy.init(args=None)
+        node = rclpy.create_node('GUI')
+
         self.payload = {'map': '', 'array': ''}
 
         # GUI websocket
-
+        self.host = host
         self.client = None
 
         # For path
         self.array_lock = threading.Lock()
         self.array = None
 
-        self.acknowledge = False
-        self.acknowledge_lock = threading.Lock()
+        self.ack = False
+        self.ack_lock = threading.Lock()
 
-        self.hal = hal
-  
+        self.pose3d_object = ListenerPose3d("/amazon_robot/odom")
+
+        executor = rclpy.executors.MultiThreadedExecutor()
+        executor.add_node(self.pose3d_object)
+        executor_thread = threading.Thread(target=executor.spin, daemon=True)
+        executor_thread.start()
 
         # Create the map object    
-        self.map = Map(self.hal.pose3d)
+        self.map = Map(self.pose3d_object)
         self.client_thread = threading.Thread(target=self.run_websocket)
         self.client_thread.start()
 
     def run_websocket(self):
         while True:
-            self.client = websocket.WebSocketApp('ws://127.0.0.1:2303',
-                                                 on_message=self.on_message,)
+            self.client = websocket.WebSocketApp(self.host, on_message=self.on_message)
             self.client.run_forever(ping_timeout=None, ping_interval=0)
-
-
-    # Explicit initialization function
-    # Class method, so user can call it without instantiation
-    @classmethod
-    def initGUI(cls, host, console):
-        # self.payload = {'map': ''}
-        new_instance = cls(host, console)
-        return new_instance
 
     # Function to get value of Acknowledge
     def get_acknowledge(self):
-        self.acknowledge_lock.acquire()
-        acknowledge = self.acknowledge
-        self.acknowledge_lock.release()
+        with self.ack_lock:
+            ack = self.ack
 
-        return acknowledge
+        return ack
 
     # Function to get value of Acknowledge
     def set_acknowledge(self, value):
-        self.acknowledge_lock.acquire()
-        self.acknowledge = value
-        self.acknowledge_lock.release()
-
+        with self.ack_lock:
+            self.ack = value
 
     # Update the gui
     def update_gui(self):
@@ -88,12 +85,9 @@ class GUI:
         if message.startswith("#ack"):
             self.set_acknowledge(True)
 
-
-
     # Function to reset
     def reset_gui(self):
         self.map.reset()
-
 
 #------------------------------------------------------------#    
     # Process the array(ideal path) to be sent to websocket
@@ -129,53 +123,14 @@ class ThreadGUI:
     def __init__(self, gui):
         """Initializes the ThreadGUI with a reference to the GUI instance."""
         self.gui = gui
-        self.ideal_cycle = 80
-        self.real_time_factor = 0
-        self.frequency_message = {'brain': '', 'gui': '', 'rtf': ''}
         self.iteration_counter = 0
         self.running = True
 
     def start(self):
         """Starts the GUI, frequency measurement, and real-time factor threads."""
-        self.frequency_thread = threading.Thread(target=self.measure_and_send_frequency)
         self.gui_thread = threading.Thread(target=self.run)
-        self.rtf_thread = threading.Thread(target=self.get_real_time_factor)
-        self.frequency_thread.start()
         self.gui_thread.start()
-        self.rtf_thread.start()
         print("GUI Thread Started!")
-
-    def get_real_time_factor(self):
-        """Continuously calculates the real-time factor."""
-        while True:
-            time.sleep(2)
-            args = ["gz", "stats", "-p"]
-            stats_process = subprocess.Popen(args, stdout=subprocess.PIPE)
-            with stats_process.stdout:
-                for line in iter(stats_process.stdout.readline, b''):
-                    stats_list = [x.strip() for x in line.split(b',')]
-                    self.real_time_factor = stats_list[0].decode("utf-8")
-
-    def measure_and_send_frequency(self):
-        """Measures and sends the frequency of GUI updates and brain cycles."""
-        previous_time = datetime.now()
-        while self.running:
-            time.sleep(2)
-            current_time = datetime.now()
-            dt = current_time - previous_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-            previous_time = current_time
-            measured_cycle = ms / self.iteration_counter if self.iteration_counter > 0 else 0
-            self.iteration_counter = 0
-            brain_frequency = round(1000 / measured_cycle, 1) if measured_cycle != 0 else 0
-            gui_frequency = round(1000 / self.ideal_cycle, 1)
-            self.frequency_message = {'brain': brain_frequency, 'gui': gui_frequency, 'rtf': self.real_time_factor}
-            message = json.dumps(self.frequency_message)
-            if self.gui.client:
-                try:
-                    self.gui.client.send(message)
-                except Exception as e:
-                    print(f"Error sending frequency message: {e}")
 
     def run(self):
         """Main loop to update the GUI at regular intervals."""
@@ -190,3 +145,12 @@ class ThreadGUI:
             time.sleep(sleep_time)
 
 
+# Create a GUI interface
+host = "ws://127.0.0.1:2303"
+gui_interface = GUI(host)
+
+# Spin a thread to keep the interface updated
+thread_gui = ThreadGUI(gui_interface)
+thread_gui.start()
+
+start_console()
