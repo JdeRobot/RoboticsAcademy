1e2804d04a8e559b313a848ffd275784643dfb8a
pawanw17 <pawanw17@gmail.com>
2024-04-07 23:47:46 +0530

    montecarlo migration to superthin

---
+++ exercises/static/exercises/montecarlo_laser_loc/python_template/ros2_humble/HAL.py
@@ -0,0 +1,45 @@
+import rclpy
+import threading
+
+from interfaces.motors import PublisherMotors
+from interfaces.pose3d import ListenerPose3d
+from interfaces.laser import ListenerLaser
+from interfaces.bumper import ListenerBumper
+
+
+# Hardware Abstraction Layer
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+
+rclpy.create_node('HAL')
+
+motors = PublisherMotors("/cmd_vel", 4, 0.3)
+pose3d = ListenerPose3d("/odom")
+laser = ListenerLaser("/roombaROS/laser/scan")
+bumper = ListenerBumper("/roombaROS/events/bumper","roombaROS")
+
+# Spin nodes so that subscription callbacks load topic data
+# Bumper has to be spinned differently so that GetEntityState works
+executor = rclpy.executors.MultiThreadedExecutor()
+executor.add_node(pose3d)
+executor.add_node(laser)
+executor_thread = threading.Thread(target=executor.spin, daemon=True)
+executor_thread.start()
+
+print("HAL-Nodes Thread Started")
+
+def getBumperData():
+    bumper.spin_bumper_node()
+    return bumper.getBumperData()
+
+def getPose3d():
+    return pose3d.getPose3d()
+
+def getLaserData():
+    return laser.getLaserData()
+
+def setV(velocity):
+    motors.sendV(velocity)
+
+def setW(velocity):
+    motors.sendW(velocity)
