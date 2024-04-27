c24d2f71714f3fe8664bc2309af6ab7e46da922c
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-04-27 20:23:17 +0200

    Removing Ros1 and assets

--- exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros1_noetic/code/academy.py
+++
@@ -1,8 +0,0 @@
-# Enter sequential code!
-from exercises.static.exercises.obstacle_avoidance_newmanager.python_template.ros1_noetic.gui import GUI
-from exercises.static.exercises.obstacle_avoidance_newmanager.python_template.ros1_noetic.hal import HAL
-
-while True:
-    # Enter iterative code!
-    img = HAL.getImage()
-    GUI.showImage(img)
