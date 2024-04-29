50e3215416b649c58cf3fe52b05fc97fe4a268f2
Javier Izquierdo <124056014+javizqh@users.noreply.github.com>
2024-04-29 14:05:46 +0200

    Solving laser issues (#2507)
    Solving laser issues

--- exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros2_humble/HAL.py
+++ exercises/static/exercises/obstacle_avoidance_newmanager/python_template/ros2_humble/HAL.py
@@ -31,7 +31,10 @@ def getPose3d():
     return pose3d.getPose3d()
 
 def getLaserData():
-    return laser.getLaserData()
+    laser_data = laser.getLaserData()
+    while len(laser_data.values) == 0:
+        laser_data = laser.getLaserData()
+    return laser_data
 
 # Get Image from ROS Driver Camera
 def getImage():
