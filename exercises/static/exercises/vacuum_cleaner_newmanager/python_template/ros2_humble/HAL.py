2c67b36c018eff7bd23a502ad54fc98dbe578793
pawanw17 <pawanw17@gmail.com>
2024-06-08 18:37:35 +0530

    vacuum cleaner HAL updates

--- exercises/static/exercises/vacuum_cleaner_newmanager/python_template/ros2_humble/HAL.py
+++ exercises/static/exercises/vacuum_cleaner_newmanager/python_template/ros2_humble/HAL.py
@@ -1,56 +1,64 @@
 import rclpy
 import sys
-from console import start_console
+import threading
+import time
 
 from hal_interfaces.general.motors import MotorsNode
 from hal_interfaces.general.odometry import OdometryNode
 from hal_interfaces.general.laser import LaserNode
 from hal_interfaces.general.bumper import BumperNode
 
-### HAL INIT ###
+
+freq = 30.0
 
 print("HAL initializing", flush=True)
 if not rclpy.ok():
     rclpy.init(args=sys.argv)
 
-motor_node = MotorsNode("/cmd_vel", 4, 0.3)
-odometry_node = OdometryNode("/odom")
-laser_node = LaserNode("/roombaROS/laser/scan")
-bumper_node = BumperNode(
-    [
-        "/roombaROS/events/right_bumper",
-        "/roombaROS/events/center_bumper",
-        "/roombaROS/events/left_bumper",
-    ]
-)
+    ### HAL INIT ###
+    motor_node = MotorsNode("/cmd_vel", 4, 0.3)
+    odometry_node = OdometryNode("/odom")
+    laser_node = LaserNode("/roombaROS/laser/scan")
+    bumper_node = BumperNode(
+        [
+            "/roombaROS/events/right_bumper",
+            "/roombaROS/events/center_bumper",
+            "/roombaROS/events/left_bumper",
+        ]
+    )
+
+    # Spin nodes so that subscription callbacks load topic data
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(odometry_node)
+    executor.add_node(laser_node)
+    executor.add_node(bumper_node) 
+    def __auto_spin() -> None:
+        while rclpy.ok():
+            executor.spin_once(timeout_sec=0)
+            time.sleep(1/freq)
+    executor_thread = threading.Thread(target=__auto_spin, daemon=True)
+    executor_thread.start()
+
 
 ### GETTERS ###
-
 
 # Laser
 def getLaserData():
     try:
-        rclpy.spin_once(laser_node)
-        values = laser_node.getLaserData().values
-        return values
+        return laser_node.getLaserData()
     except Exception as e:
         print(f"Exception in hal getLaserData {repr(e)}")
-
 
 # Pose
 def getPose3d():
     try:
-        rclpy.spin_once(odometry_node)
-        pose = odometry_node.getPose3d()
-        return pose
+        return odometry_node.getPose3d()
     except Exception as e:
-        print(f"Exception in hal getPose3d {repr(e)}")
-
+        print(f"Exception in hal getPose3d {repr(e)}")        
 
 # Bumper
 def getBumperData():
     try:
-        rclpy.spin_once(bumper_node)
         return bumper_node.getBumperData()
     except Exception as e:
         print(f"Exception in hal getBumper {repr(e)}")
@@ -58,12 +66,10 @@ def getBumperData():
 
 ### SETTERS ###
 
-
 # Linear speed
 def setV(v):
-    motor_node.sendV(v)
-
+    motor_node.sendV(float(v))
 
 # Angular speed
 def setW(w):
-    motor_node.sendW(w)
+    motor_node.sendW(float(w))
