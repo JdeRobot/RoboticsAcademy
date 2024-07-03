f7fd1303493f1abe84ddd60e8b010e6fcc076ce8
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-03 15:59:08 +0200

    Adding SimTimeNode and getSimTime

--- exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/HAL.py
+++ exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/HAL.py
@@ -5,6 +5,7 @@ import time
 from hal_interfaces.general.motors import MotorsNode
 from hal_interfaces.general.odometry import OdometryNode
 from hal_interfaces.general.laser import LaserNode
+from hal_interfaces.general.sim_time import SimTimeNode
 from hal_interfaces.specific.amazon_warehouse.platform_controller import PlatformCommandNode, PublisherPlatformNode
 
 # Hardware Abstraction Layer
@@ -18,6 +19,7 @@ if not rclpy.ok():
     motor_node = MotorsNode("/amazon_robot/cmd_vel", 4, 0.3)
     odometry_node = OdometryNode("/amazon_robot/odom")
     laser_node = LaserNode("/amazon_robot/scan")
+    sim_time_node = SimTimeNode()
     platform_listener = PlatformCommandNode("/send_effort")
     platform_pub = PublisherPlatformNode("/send_effort")
 
@@ -25,6 +27,7 @@ if not rclpy.ok():
     executor = rclpy.executors.MultiThreadedExecutor()
     executor.add_node(odometry_node)
     executor.add_node(laser_node)
+    executor.add_node(sim_time_node)
     executor.add_node(platform_listener)
     def __auto_spin() -> None:
         while rclpy.ok():
@@ -35,6 +38,9 @@ if not rclpy.ok():
 
 def getPose3d():
     return odometry_node.getPose3d()
+
+def getSimTime():
+    return sim_time_node.getSimTime()
 
 def getLaserData():
     laser_data = laser_node.getLaserData()
