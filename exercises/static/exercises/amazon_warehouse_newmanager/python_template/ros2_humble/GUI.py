dc519d4a2efbf4502ae064ef4998afccb19a5e02
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-09 12:43:47 +0200

    One class amazon warehouse

--- exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/GUI.py
@@ -1,64 +1,84 @@
-import json
 import re
+import matplotlib.pyplot as plt
+import json
 import threading
 import time
-from datetime import datetime
 import websocket
+from src.manager.ram_logging.log_manager import LogManager
+from gazebo_msgs.srv import SetEntityState, GetEntityState
 import rclpy
 from console import start_console
-import matplotlib.pyplot as plt
-
-from HAL import getPose3d
 from map import Map
+from HAL import getPose3d
 
-# Graphical User Interface Class
-class GUI:
-    # Initialization function
-    # The actual initialization
-    def __init__(self, host):
 
-        # ROS2 init
+class ThreadingGUI:
+
+    def __init__(self, host="ws://127.0.0.1:2303", freq=30.0):
+
+        # ROS 2 init
         if not rclpy.ok():
-            rclpy.init(args=None)
+            rclpy.init()
 
-        self.payload = {'map': '', 'array': ''}
+        # Execution control vars
+        self.out_period = 1.0 / freq
 
-        # GUI websocket
-        self.host = host
-        self.client = None
-
-        # For path
-        self.array_lock = threading.Lock()
-        self.array = None
-
-        self.ack = False
+        self.ack = True
         self.ack_lock = threading.Lock()
 
-        # Create the map object    
+        self.array_lock = threading.Lock()
+        self.array = None
+        
+        self.running = True
+        
+        self.host = host
+        self.node = rclpy.create_node("node")
+
+        # Payload vars
+        self.payload = {'map': '', 'array': ''}
+        self.init_coords = (171, 63)
+        self.start_coords = (201, 85.5)
         self.map = Map(getPose3d)
-        self.client_thread = threading.Thread(target=self.run_websocket)
-        self.client_thread.start()
 
+        # Initialize and start the WebSocket client thread
+        threading.Thread(target=self.run_websocket, daemon=True).start()
+
+        # Initialize and start the image sending thread (GUI out thread)
+        threading.Thread(
+            target=self.gui_out_thread, name="gui_out_thread", daemon=True
+        ).start()
+
+    # Init websocket client
     def run_websocket(self):
-        while True:
-            self.client = websocket.WebSocketApp(self.host, on_message=self.on_message)
-            self.client.run_forever(ping_timeout=None, ping_interval=0)
+        self.client = websocket.WebSocketApp(self.host, on_message=self.gui_in_thread)
+        self.client.run_forever(ping_timeout=None, ping_interval=0)
 
-    # Function to get value of Acknowledge
-    def get_acknowledge(self):
-        with self.ack_lock:
-            ack = self.ack
+    # Process incoming messages to the GUI
+    def gui_in_thread(self, ws, message):
 
-        return ack
+        # In this case, incoming msgs can only be acks
+        if "ack" in message:
+            with self.ack_lock:
+                self.ack = True
 
-    # Function to get value of Acknowledge
-    def set_acknowledge(self, value):
-        with self.ack_lock:
-            self.ack = value
+    # Process outcoming messages from the GUI
+    def gui_out_thread(self):
+        while self.running:
+            start_time = time.time()
 
-    # Update the gui
-    def update_gui(self):
-        # Payload path array
+            # Check if a new map should be sent
+            self.send_map()
+            # with self.ack_lock:
+            #     if self.ack and self.map is not None:
+            #         self.ack = False
+
+            # Maintain desired frequency
+            elapsed = time.time() - start_time
+            sleep_time = max(0, self.out_period - elapsed)
+            time.sleep(sleep_time)
+
+    # Prepares and sends a map to the websocket server
+    def send_map(self):
         self.payload["array"] = self.array
 
         # Payload Map Message
@@ -74,17 +94,7 @@ class GUI:
             except Exception as e:
                 print(f"Error sending message: {e}")
 
-    def on_message(self, ws, message):
-        """Handles incoming messages from the websocket client."""
-        if message.startswith("#ack"):
-            self.set_acknowledge(True)
-
-    # Function to reset
-    def reset_gui(self):
-        self.map.reset()
-
-#------------------------------------------------------------#    
-    # Process the array(ideal path) to be sent to websocket
+        # Process the array(ideal path) to be sent to websocket
     def showPath(self, array):
         array_scaled = []
         for wp in array:
@@ -110,49 +120,16 @@ class GUI:
     
     def getMap(self, url):
         return plt.imread(url)
-#------------------------------------------------------------#
 
 
-class ThreadGUI:
-    """Class to manage GUI updates and frequency measurements in separate threads."""
-
-    def __init__(self, gui):
-        """Initializes the ThreadGUI with a reference to the GUI instance."""
-        self.gui = gui
-        self.iteration_counter = 0
-        self.running = True
-
-    def start(self):
-        """Starts the GUI, frequency measurement, and real-time factor threads."""
-        self.gui_thread = threading.Thread(target=self.run)
-        self.gui_thread.start()
-        print("GUI Thread Started!")
-
-    def run(self):
-        """Main loop to update the GUI at regular intervals."""
-        while self.running:
-            start_time = datetime.now()
-            self.gui.update_gui()
-            self.iteration_counter += 1
-            finish_time = datetime.now()
-            dt = finish_time - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-            sleep_time = max(0, (50 - ms) / 1000.0)
-            time.sleep(sleep_time)
-
-
-# Create a GUI interface
 host = "ws://127.0.0.1:2303"
-gui_interface = GUI(host)
+gui = ThreadingGUI(host)
 
-# Spin a thread to keep the interface updated
-thread_gui = ThreadGUI(gui_interface)
-thread_gui.start()
-
+# Redirect the console
 start_console()
 
 def showPath(array):
-    return gui_interface.showPath(array)
+    return gui.showPath(array)
 
 def getMap(url):
-    return gui_interface.getMap(url)
+    return gui.getMap(url)
