c019470ad3677ff49fccfd36762ebf3b692702de
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-09 18:33:57 +0200

    Autoparking with ack

--- exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/GUI.py
@@ -1,51 +1,81 @@
 import json
 import threading
 import time
-from datetime import datetime
 import websocket
-import logging
 import rclpy
-
-from HAL import getFrontLaserData, getRightLaserData, getBackLaserData
-from map import Map
+from src.manager.ram_logging.log_manager import LogManager
 from console import start_console
+from map import Map
+from HAL import getFrontLaserData, getRightLaserData, getBackLaserData
 
 # Graphical User Interface Class
-class GUI:
-    """Graphical User Interface class"""
 
-    def __init__(self, host):
-        print("GUI IS BEING CALLED\n\n\n\n")
-        """Initializes the GUI"""
+class ThreadingGUI:
 
-        # ROS2 init
+    def __init__(self, host="ws://127.0.0.1:2303", freq=30.0):
+
+        # ROS 2 init
         if not rclpy.ok():
-            rclpy.init(args=None)
+            rclpy.init()
 
-        self.payload = {'map': ''}
-        self.client = None
-        self.host = host
-        self.ack = False
+        # Execution control vars
+        self.out_period = 1.0 / freq
+
+        self.ack = True
+        self.ack_frontend = False
         self.ack_lock = threading.Lock()
-        self.running = True
-        self.iteration_counter = 0
 
-        # Create Map object
+        self.running = True
+
+        self.host = host
+        self.node = rclpy.create_node("node")
+
+        # Payload vars
+        self.payload = {'map': ''}
         self.map = Map(getFrontLaserData, getRightLaserData, getBackLaserData)
 
-        # Start the websocket and GUI update threads
-        self.websocket_thread = threading.Thread(target=self.run_websocket)
-        self.update_thread = threading.Thread(target=self.run)
-        self.websocket_thread.start()
-        self.update_thread.start()
+        # Initialize and start the WebSocket client thread
+        threading.Thread(target=self.run_websocket, daemon=True).start()
 
+        # Initialize and start the image sending thread (GUI out thread)
+        threading.Thread(
+            target=self.gui_out_thread, name="gui_out_thread", daemon=True
+        ).start()
+
+    # Init websocket client
     def run_websocket(self):
-        while self.running:
-            self.client = websocket.WebSocketApp(self.host, on_message=self.on_message)
-            self.client.run_forever(ping_timeout=None, ping_interval=0)
+        self.client = websocket.WebSocketApp(self.host, on_message=self.gui_in_thread)
+        self.client.run_forever(ping_timeout=None, ping_interval=0)
 
+    # Process incoming messages to the GUI
+    def gui_in_thread(self, ws, message):
+
+        # In this case, incoming msgs can only be acks
+        if "ack" in message:
+            with self.ack_lock:
+                self.ack = True
+                self.ack_frontend = True
+
+    # Process outcoming messages from the GUI
+    def gui_out_thread(self):
+        while self.running:
+            start_time = time.time()
+
+            # Check if a new map should be sent
+            with self.ack_lock:
+                if self.ack:
+                    self.update_gui()
+                    if self.ack_frontend: 
+                        self.ack = False
+
+            # Maintain desired frequency
+            elapsed = time.time() - start_time
+            sleep_time = max(0, self.out_period - elapsed)
+            time.sleep(sleep_time)
+
+    # Prepares and sends a map to the websocket server
     def update_gui(self):
-        """Updates the GUI with the latest map information."""
+
         map_message = self.map.get_json_data()
         self.payload["map"] = map_message
         message = json.dumps(self.payload)
@@ -53,33 +83,15 @@ class GUI:
             try:
                 self.client.send(message)
             except Exception as e:
-                print(f"Error sending message: {e}")
+                LogManager.logger.info(f"Error sending message: {e}")
 
-    def on_message(self, ws, message):
-        """Handles incoming messages from the websocket client."""
-        if message.startswith("#ack"):
-            with self.ack_lock:
-                self.ack = True
 
     def reset_gui(self):
         """Resets the GUI to its initial state."""
         self.map.reset()
 
-    def run(self):
-        """Main loop to update the GUI at regular intervals."""
-        while self.running:
-            start_time = datetime.now()
-            self.update_gui()
-            self.iteration_counter += 1
-            finish_time = datetime.now()
-            dt = finish_time - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-            sleep_time = max(0, (50 - ms) / 1000.0)
-            time.sleep(sleep_time)
-
-# Create a GUI interface
 host = "ws://127.0.0.1:2303"
-gui_interface = GUI(host)
+gui = ThreadingGUI(host)
 
-# Start the console
+# Redirect the console
 start_console()
