365cff061c66ab09082b1c50eae1724feda8378c
duro <davidduro2002@gmail.com>
2024-07-09 11:44:24 +0200

    First exercise modification

--- exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/autoparking_newmanager/python_template/ros2_humble/GUI.py
@@ -27,15 +27,22 @@ class GUI:
         self.host = host
         self.ack = False
         self.ack_lock = threading.Lock()
+        self.running = True
+        self.iteration_counter = 0
 
-        # create Map object
+        # Create Map object
         self.map = Map(getFrontLaserData, getRightLaserData, getBackLaserData)
 
-        threading.Thread(target=self.run_websocket).start()
+        # Start the websocket and GUI update threads
+        self.websocket_thread = threading.Thread(target=self.run_websocket)
+        self.update_thread = threading.Thread(target=self.run)
+        self.websocket_thread.start()
+        self.update_thread.start()
 
     def run_websocket(self):
-        self.client = websocket.WebSocketApp(self.host, on_message=self.on_message)
-        self.client.run_forever(ping_timeout=None, ping_interval=0)
+        while self.running:
+            self.client = websocket.WebSocketApp(self.host, on_message=self.on_message)
+            self.client.run_forever(ping_timeout=None, ping_interval=0)
 
     def update_gui(self):
         """Updates the GUI with the latest map information."""
@@ -52,33 +59,17 @@ class GUI:
         """Handles incoming messages from the websocket client."""
         if message.startswith("#ack"):
             with self.ack_lock:
-                self.ack=True
+                self.ack = True
 
     def reset_gui(self):
         """Resets the GUI to its initial state."""
         self.map.reset()
 
-
-class ThreadGUI:
-    """Class to manage GUI updates and frequency measurements in separate threads."""
-
-    def __init__(self, gui):
-        """Initializes the ThreadGUI with a reference to the GUI instance."""
-        self.gui = gui
-        self.iteration_counter = 0
-        self.running = True
-
-    def start(self):
-        """Starts the GUI, frequency measurement, and real-time factor threads."""
-        self.gui_thread = threading.Thread(target=self.run)
-        self.gui_thread.start()
-        print("GUI Thread Started!")
-
     def run(self):
         """Main loop to update the GUI at regular intervals."""
         while self.running:
             start_time = datetime.now()
-            self.gui.update_gui()
+            self.update_gui()
             self.iteration_counter += 1
             finish_time = datetime.now()
             dt = finish_time - start_time
@@ -86,13 +77,9 @@ class ThreadGUI:
             sleep_time = max(0, (50 - ms) / 1000.0)
             time.sleep(sleep_time)
 
-
 # Create a GUI interface
 host = "ws://127.0.0.1:2303"
 gui_interface = GUI(host)
 
-# Spin a thread to keep the interface updated
-thread_gui = ThreadGUI(gui_interface)
-thread_gui.start()
-
+# Start the console
 start_console()
