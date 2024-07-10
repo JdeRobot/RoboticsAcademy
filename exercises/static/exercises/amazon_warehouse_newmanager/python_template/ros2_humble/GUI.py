e7c10724a82664f321f68100f7fb2e35457eaea6
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-10 18:47:46 +0200

    Amazon warehouse with gui_interfaces

--- exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/amazon_warehouse_newmanager/python_template/ros2_humble/GUI.py
@@ -1,40 +1,21 @@
 import re
-import matplotlib.pyplot as plt
 import json
 import threading
-import time
-import websocket
-from src.manager.ram_logging.log_manager import LogManager
-from gazebo_msgs.srv import SetEntityState, GetEntityState
-import rclpy
-from console import start_console
+import matplotlib.pyplot as plt
+
+from gui_interfaces.general.threading_gui import ThreadingGUI
 from map import Map
 from HAL import getPose3d
+from console import start_console
 
 
-class ThreadingGUI:
+class GUI(ThreadingGUI):
 
-    def __init__(self, host="ws://127.0.0.1:2303", freq=30.0):
-
-        # ROS 2 init
-        if not rclpy.ok():
-            rclpy.init()
-
-        # Execution control vars
-        self.out_period = 1.0 / freq
-
-        self.ack = True
-        self.ack_frontend = False
-        self.ack_lock = threading.Lock()
+    def __init__(self, host="ws://127.0.0.1:2303"):
+        super().__init__(host)
 
         self.array_lock = threading.Lock()
         self.array = None
-        
-        self.running = True
-        
-        self.host = host
-        self.client = None
-        self.node = rclpy.create_node("node")
 
         # Payload vars
         self.payload = {'map': '', 'array': ''}
@@ -42,46 +23,7 @@ class ThreadingGUI:
         self.start_coords = (201, 85.5)
         self.map = Map(getPose3d)
 
-        # Initialize and start the WebSocket client thread
-        threading.Thread(target=self.run_websocket, daemon=True).start()
-
-        # Initialize and start the image sending thread (GUI out thread)
-        threading.Thread(
-            target=self.gui_out_thread, name="gui_out_thread", daemon=True
-        ).start()
-
-    # Init websocket client
-    def run_websocket(self):
-        self.client = websocket.WebSocketApp(self.host, on_message=self.gui_in_thread)
-        self.client.run_forever(ping_timeout=None, ping_interval=0)
-
-    # Process incoming messages to the GUI
-    def gui_in_thread(self, ws, message):
-
-        # In this case, incoming msgs can only be acks
-        if "ack" in message:
-            with self.ack_lock:
-                self.ack = True
-                self.ack_frontend = True
-        else:
-            LogManager.logger.error("Unsupported msg")
-
-    # Process outcoming messages from the GUI
-    def gui_out_thread(self):
-        while self.running:
-            start_time = time.time()
-
-            # Check if a new map should be sent
-            with self.ack_lock:
-                if self.ack and self.client:
-                    self.update_gui()
-                    if self.ack_frontend: 
-                        self.ack = False
-
-            # Maintain desired frequency
-            elapsed = time.time() - start_time
-            sleep_time = max(0, self.out_period - elapsed)
-            time.sleep(sleep_time)
+        self.start()
 
     # Prepares and sends a map to the websocket server
     def update_gui(self):
@@ -94,11 +36,7 @@ class ThreadingGUI:
         self.payload["map"] = pos_message
 
         message = json.dumps(self.payload)
-        if self.client:
-            try:
-                self.client.send(message)
-            except Exception as e:
-                LogManager.logger.info(f"Error sending message: {e}")
+        self.send_to_client(message)
 
         # Process the array(ideal path) to be sent to websocket
     def showPath(self, array):
@@ -129,7 +67,7 @@ class ThreadingGUI:
 
 
 host = "ws://127.0.0.1:2303"
-gui = ThreadingGUI(host)
+gui = GUI(host)
 
 # Redirect the console
 start_console()
