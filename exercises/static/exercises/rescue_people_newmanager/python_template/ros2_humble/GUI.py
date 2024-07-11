4663f202d1408931979adddbbaf2bfb1763ae2ea
duro <davidduro2002@gmail.com>
2024-07-11 15:08:38 +0200

    Rescue people modified for easier gui_interfaces implementation

--- exercises/static/exercises/rescue_people_newmanager/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/rescue_people_newmanager/python_template/ros2_humble/GUI.py
@@ -19,6 +19,7 @@ class ThreadingGUI:
         self.left_image = None
         self.image_lock = threading.Lock()
         self.ack = True
+        self.ack_frontend = False
         self.ack_lock = threading.Lock()
         self.running = True
 
@@ -45,6 +46,7 @@ class ThreadingGUI:
         if "ack" in message:
             with self.ack_lock:
                 self.ack = True
+                self.ack_frontend = True
         else:
             LogManager.logger.error("Unsupported msg")
 
@@ -55,11 +57,10 @@ class ThreadingGUI:
 
             # Check if a new image should be sent
             with self.ack_lock:
-                with self.image_lock:
-                    if self.ack:
-                        if np.any(self.left_image) or np.any(self.right_image):
-                            self.send_images()
-                            self.ack = False
+                if self.ack:
+                    self.update_gui()
+                    if self.ack_frontend: 
+                        self.ack = False
 
             # Maintain desired frequency
             elapsed = time.time() - start_time
@@ -67,40 +68,41 @@ class ThreadingGUI:
             time.sleep(sleep_time)
 
     # Prepares and send image to the websocket server
-    def send_images(self):
+    def update_gui(self):
+        with self.image_lock:
+            if np.any(self.left_image) or np.any(self.right_image):
+                if np.any(self.left_image):
+                    _, encoded_left_image = cv2.imencode(".JPEG", self.left_image)
+                    b64_left = base64.b64encode(encoded_left_image).decode("utf-8")
+                    shape_left = self.left_image.shape
+                else:
+                    b64_left = None
+                    shape_left = 0
 
-        if np.any(self.left_image):
-            _, encoded_left_image = cv2.imencode(".JPEG", self.left_image)
-            b64_left = base64.b64encode(encoded_left_image).decode("utf-8")
-            shape_left = self.left_image.shape
-        else:
-            b64_left = None
-            shape_left = 0
+                if np.any(self.right_image):
+                    _, encoded_right_image = cv2.imencode(".JPEG", self.right_image)
+                    b64_right = base64.b64encode(encoded_right_image).decode("utf-8")
+                    shape_right = self.right_image.shape
+                else:
+                    b64_right = None
+                    shape_right = 0
 
-        if np.any(self.right_image):
-            _, encoded_right_image = cv2.imencode(".JPEG", self.right_image)
-            b64_right = base64.b64encode(encoded_right_image).decode("utf-8")
-            shape_right = self.right_image.shape
-        else:
-            b64_right = None
-            shape_right = 0
-
-        payload_left = {
-            "image_left": b64_left,
-            "shape_left": shape_left,
-        }
-        payload_right = {
-            "image_right": b64_right,
-            "shape_right": shape_right,
-        }
-        self.msg["image_left"] = json.dumps(payload_left)
-        self.msg["image_right"] = json.dumps(payload_right)
-        message = json.dumps(self.msg)
-        try:
-            if self.client:
-                self.client.send(message)
-        except Exception as e:
-            LogManager.logger.info(f"Error sending message: {e}")
+                payload_left = {
+                    "image_left": b64_left,
+                    "shape_left": shape_left,
+                }
+                payload_right = {
+                    "image_right": b64_right,
+                    "shape_right": shape_right,
+                }
+                self.msg["image_left"] = json.dumps(payload_left)
+                self.msg["image_right"] = json.dumps(payload_right)
+                message = json.dumps(self.msg)
+                try:
+                    if self.client:
+                        self.client.send(message)
+                except Exception as e:
+                    LogManager.logger.info(f"Error sending message: {e}")
 
     # Functions to set the next image to be sent
     def setLeftImage(self, image):
