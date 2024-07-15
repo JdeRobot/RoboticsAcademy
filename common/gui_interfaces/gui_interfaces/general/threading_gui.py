9b12def3212db47351712ac855088e642d0cbd24
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-15 22:27:01 +0200

    Trying passing self as arg

--- common/gui_interfaces/gui_interfaces/general/threading_gui.py
+++ common/gui_interfaces/gui_interfaces/general/threading_gui.py
@@ -30,14 +30,14 @@ class ThreadingGUI:
     def start(self):
         # Initialize and start the WebSocket client thread
         # threading.Thread(target=self.run_websocket, daemon=True).start()
-        client_process = Process(target=self.run_websocket)
+        client_process = Process(target=self.run_websocket, args=(self))
         client_process.start()
 
         # Initialize and start the image sending thread (GUI out thread)
         # threading.Thread(
         #     target=self.gui_out_thread, name="gui_out_thread", daemon=True
         # ).start()
-        gui_process = Process(target=self.gui_out_thread)
+        gui_process = Process(target=self.gui_out_thread, args=(self))
         gui_process.start()
 
         # client_process.join()
