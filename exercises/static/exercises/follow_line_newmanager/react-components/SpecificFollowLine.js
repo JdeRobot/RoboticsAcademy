ebbfc5c4d67f7b222d9d8c079b8324260da489ff
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-09-06 19:20:22 +0200

    Migrating changes to Unibotics

--- exercises/static/exercises/follow_line_newmanager/react-components/SpecificFollowLine.js
+++ exercises/static/exercises/follow_line_newmanager/react-components/SpecificFollowLine.js
@@ -2,9 +2,8 @@ import * as React from "react";
 import { Box } from "@mui/material";
 import "./css/GUICanvas.css";
 import { drawImage } from "./helpers/showImagesFollowLine";
-import { getProgress, resetProgress } from "./helpers/showProgressFollowLine";
 import { getCarPose } from "./helpers/showCarPositionFollowLine";
-import { displayLapTime, isLapFinished, isInTimeoutLap, addLap } from "./helpers/showLapTimeFollowLine";
+import { displayLapTime} from "./helpers/showLapTimeFollowLine";
 
 import defaultCircuit from "../resources/images/default_circuit.png";
 import montmeloCircuit from "../resources/images/montmelo_circuit.png";
@@ -15,39 +14,23 @@ const width = 1280;
 const height = 720;
 
 const SpecificFollowLine = (props) => {
-  const [progress, setProgress] = React.useState(0)
   const [lapTime, setLapTime] = React.useState(null)
   const [carPose, setCarPose] = React.useState(null)
   const [circuitImg, setCircuitImg] = React.useState(defaultCircuit);
   const canvasRef = React.useRef(null)
   var circuitName = "simple";
-  var lapFinishedTime = null;
-  var laps = [];
 
   React.useEffect(() => {
     console.log("TestShowScreen subscribing to ['update'] events");
     const callback = (message) => {
       if(message.data.update.image){
-        // console.log(message.data.update)
         const image = JSON.parse(message.data.update.image)
         if(image.image){
           drawImage(message.data.update)
         }
         try {
           const pose = getCarPose(circuitName, message.data.update.map)
-
-          if (isLapFinished(circuitName, pose) && !isInTimeoutLap(lapFinishedTime, 6)) {
-            lapFinishedTime = Date.now()
-            laps.push(addLap(laps, message.data.update.lap))
-            resetProgress()
-            console.log(laps)
-          }
-
-          if (!isInTimeoutLap(lapFinishedTime, 6)) {
-            setLapTime(displayLapTime(message.data.update.lap))
-          }
-
-          setProgress(getProgress(circuitName, pose))
+          setLapTime(displayLapTime(message.data.update.lap))
           setCarPose(pose)
         } catch (error) {
           
@@ -80,30 +63,22 @@ const SpecificFollowLine = (props) => {
       } else if (message.data.state === "paused") {
         window.RoboticsExerciseComponents.commsManager.send("gui", "pause");
       } else if (message.data.state === "visualization_ready") {
-        lapFinishedTime = null;
-        laps = [];
-        resetProgress()
-        setProgress(0)
         setCarPose(null)
         setLapTime(null)
         switch (context.mapSelected) {
-          case "Simple":
-          case "Simple Ackermann":
+          case "follow_line_default_ros2":
             circuitName = "simple";
             setCircuitImg(defaultCircuit);
             break;
-          case "Montmelo":
-          case "Montmelo Ackermann":
+          case "follow_line_montmelo_ros2":
             circuitName = "montmelo";
             setCircuitImg(montmeloCircuit);
             break;
-          case "Montreal":
-          case "Montreal Ackermann":
+          case "follow_line_montreal_ros2":
             circuitName = "montreal";
             setCircuitImg(montrealCircuit);
             break;
-          case "Nürburgring":
-          case "Nürburgring Ackermann":
+          case "follow_line_nurburgring_ros2":
             circuitName = "nürburgring";
             setCircuitImg(ngbCircuit);
             break;
@@ -135,10 +110,6 @@ const SpecificFollowLine = (props) => {
       { lapTime &&
         <label className="overlay" id="lap-time">{lapTime} s</label>
       }
-      <div className="overlay" id="progress-bar">
-        <div id="progress-content" style={{width: progress+"%"}}/>
-        <label id="progress-label">{progress}%</label>
-      </div>
       <div className="overlay" id="circuit-aerial">
         <img src={circuitImg} alt="" id="circuit-img" />
         {carPose &&
