62d39b971a73b275f8925eda22afc96ae73fd02d
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-09-10 19:38:12 +0200

    Adding scalings

--- exercises/static/exercises/follow_line_newmanager/react-components/helpers/showCarPositionFollowLine.js
+++ exercises/static/exercises/follow_line_newmanager/react-components/helpers/showCarPositionFollowLine.js
@@ -1,12 +1,19 @@
 export const getCarPose = (circuit, position) => {
   var scaleX, scaleY, offsetX, offsetY;
+  let ackUniverse =  circuit.includes("ack")
+  let ackMultiplier = 1;
   let pose = position.substring( 1, position.length - 1);
   let pos = pose.split(",").map(function (item) {
     return parseFloat(item);
   });
   
+  if (ackUniverse) {
+    circuit = circuit.replace(" ack","")
+    ackMultiplier = 3
+  }
+
   switch (circuit) {
-    case "simple":
+    case "default":
       scaleY = 1.25; offsetY = 77
 			scaleX = -2.6; offsetX = 151
       break;
@@ -19,7 +26,7 @@ export const getCarPose = (circuit, position) => {
       scaleY = 0.685; offsetY = 77
 			scaleX = -0.48; offsetX = 151
       break;
-    case "nürburgring":
+    case "ngb":
       scaleY = 1.5; offsetY = 77
 			scaleX = -1.495; offsetX = 151
       break;
@@ -29,8 +36,8 @@ export const getCarPose = (circuit, position) => {
       break;
   }
 
-  var x = Math.round(pos[0]) * scaleX + offsetX;
-  var y = Math.round(pos[1]) * scaleY + offsetY;
+  var x = Math.round(pos[0]) * scaleX / ackMultiplier + offsetX;
+  var y = Math.round(pos[1]) * scaleY / ackMultiplier + offsetY;
 
   return [x,y];
 };
