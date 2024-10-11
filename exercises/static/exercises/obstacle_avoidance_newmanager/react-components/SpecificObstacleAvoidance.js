d85785f281953a1a2c3c27ece6a46d646ed427de
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-10-11 22:36:10 +0200

    Adding arrows

--- exercises/static/exercises/obstacle_avoidance_newmanager/react-components/SpecificObstacleAvoidance.js
+++ exercises/static/exercises/obstacle_avoidance_newmanager/react-components/SpecificObstacleAvoidance.js
@@ -1,14 +1,18 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { paintEvent } from "./helpers/map";
 
 import F1Car from "../resources/images/f1-car.svg";
+import Arrow from "../resources/images/arrow.svg";
 import "./css/GUICanvas.css";
 
 function SpecificObstacleAvoidance(props) {
-  const guiCanvasRef = React.useRef();
+  const meter = 73; // 1m = 73px
+
   const [laser, setLaser] = React.useState([])
   const [maxRange, setMaxRange] = React.useState([])
+  const [carForce, setCarForce] = React.useState([2 * meter, 0])
+  const [avgForce, setAvgForce] = React.useState([2 * meter, 0])
+  const [obsForce, setObsForce] = React.useState([2 * meter, -Math.PI / 2])
 
   React.useEffect(() => {
     console.log("TestShowScreen subscribing to ['update'] events");
@@ -17,16 +21,32 @@ function SpecificObstacleAvoidance(props) {
       const data = message.data.update;
       if(data.map){
         const dataToDraw = JSON.parse(data.map)
-        // TODO: For the arrows use the same as the lasers
 
-        paintEvent(dataToDraw.target, dataToDraw.car, dataToDraw.obstacle, dataToDraw.average, dataToDraw.laser, dataToDraw.max_range)
         setLaser(dataToDraw.laser)
         setMaxRange(dataToDraw.max_range)
+        var carForceDist = getDist(dataToDraw.car[0], dataToDraw.car[1])
+        setCarForce([carForceDist * meter, getAng(dataToDraw.car[0], dataToDraw.car[1])])
+        var avgForceDist = getDist(dataToDraw.average[0], dataToDraw.average[1])
+        setAvgForce([avgForceDist * meter, getAng(dataToDraw.average[0], dataToDraw.average[1])])
+        var obsForceDist = getDist(dataToDraw.obstacle[0], dataToDraw.obstacle[1])
+        setObsForce([obsForceDist * meter, getAng(dataToDraw.obstacle[0], dataToDraw.obstacle[1])])
       }
 
       // Send the ACK of the msg
       window.RoboticsExerciseComponents.commsManager.send("gui", "ack");
     };
+
+    const getDist = (x,y) => {
+      return Math.sqrt(Math.pow(x,2) + Math.pow(y,2))
+    }
+
+    const getAng = (x,y) => {
+      var ang = Math.PI / 2;
+    	if(x != 0){
+        ang = Math.atan2(y, x);
+      }
+      return ang;
+    }
 
     window.RoboticsExerciseComponents.commsManager.subscribe(
       [window.RoboticsExerciseComponents.commsManager.events.UPDATE],
@@ -44,19 +64,7 @@ function SpecificObstacleAvoidance(props) {
 
   return (
     <div style={{display: "flex",   width: "100%",
-    height: "100%", backgroundColor: "#363233", position:"relative", overflowY:"auto"}}>
-      <canvas
-        ref={guiCanvasRef}
-        id="local-map"
-
-        style={{
-          backgroundColor: "#363233",
-          marginTop: "5px",
-          width: "50%",
-          height: "100%",
-          margin: "auto",
-      }}
-      />
+    height: "100%", backgroundColor: "#363233", position:"relative", overflow:"hidden"}}>
       <img src={F1Car} id="f1-car"/>
       {laser.map(element => {
         var ang = -element[1]
@@ -65,6 +73,9 @@ function SpecificObstacleAvoidance(props) {
           <hr className="laser-beam" style={{rotate: "z "+ ang +"rad", width: "calc("+length + "%)"}}/>
         )})
       }
+      <img className="arrow green" src={Arrow} style={{height: carForce[0], rotate: "z "+ -carForce[1] +"rad"}}/>
+      <img className="arrow red" src={Arrow} style={{height: obsForce[0], rotate: "z "+ -obsForce[1] +"rad"}}/>
+      <img className="arrow" src={Arrow} style={{height: avgForce[0], rotate: "z "+ -avgForce[1] +"rad", zIndex: "6"}}/>
     </div>
   );
 }
