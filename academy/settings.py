1faea77fac6cd4b1f7c411f384774d29d466869b
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-10-17 22:52:10 +0200

    More changes

--- academy/settings.py
+++ academy/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(BASE_DIR / 'db.sqlite3'),
     },
-    'universe': {
+    'universes': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': str('/universes.sql'),
     }
