9d1b434d4e42ba006ffc9c653edc73d1b8da51ef
Rishabh Dang <142956540+Rish-1903@users.noreply.github.com>
2025-04-01 18:55:38 +0530

    Update utils.py

--- exercises/static/exercises/human_detection/benchmarking/utils.py
+++ exercises/static/exercises/human_detection/benchmarking/utils.py
@@ -1,47 +1,48 @@
 from enum import Enum
-
+import os
+import sys
+from typing import List, Dict, Tuple, Optional, Union
+import glob
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 
 class MethodAveragePrecision(Enum):
-    """
-    Class representing if the coordinates are relative to the
-    image size or are absolute values.
-    """
+    """Enum for different methods of calculating average precision."""
     EveryPointInterpolation = 1
     ElevenPointInterpolation = 2
 
 
 class CoordinatesType(Enum):
-    """
-    Class representing if the coordinates are relative to the
-    image size or are absolute values.
-    """
+    """Enum representing if coordinates are relative or absolute."""
     Relative = 1
     Absolute = 2
 
 
 class BBType(Enum):
-    """
-    Class representing if the bounding box is groundtruth or not.
-    """
+    """Enum representing if bounding box is ground truth or detection."""
     GroundTruth = 1
     Detected = 2
 
 
 class BBFormat(Enum):
-    """
-    Class representing the format of a bounding box.
-    It can be (X,Y,width,height) => XYWH
-    or (X1,Y1,X2,Y2) => XYX2Y2
-    """
+    """Enum representing bounding box format (XYWH or XYX2Y2)."""
     XYWH = 1
     XYX2Y2 = 2
 
 
-def convertToRelativeValues(size, box):
-    dw = 1. / (size[0])
-    dh = 1. / (size[1])
+def convert_to_relative_values(size: Tuple[int, int], box: Tuple[float, float, float, float]) -> Tuple[float, float, float, float]:
+    """Convert absolute coordinates to relative values (0-1 range)."""
+    if not all(isinstance(x, (int, float)) for x in box):
+        raise ValueError("Box coordinates must be numeric")
+    if size[0] <= 0 or size[1] <= 0:
+        raise ValueError("Image size must be positive")
+
+    dw = 1.0 / size[0]
+    dh = 1.0 / size[1]
     cx = (box[1] + box[0]) / 2.0
     cy = (box[3] + box[2]) / 2.0
     w = box[1] - box[0]
@@ -53,52 +54,68 @@ def convertToRelativeValues(size, box):
     return (x, y, w, h)
 
 
-def convertToAbsoluteValues(size, box):
-    xIn = round(((2 * float(box[0]) - float(box[2])) * size[0] / 2))
-    yIn = round(((2 * float(box[1]) - float(box[3])) * size[1] / 2))
-    xEnd = xIn + round(float(box[2]) * size[0])
-    yEnd = yIn + round(float(box[3]) * size[1])
-    if xIn < 0:
-        xIn = 0
-    if yIn < 0:
-        yIn = 0
-    if xEnd >= size[0]:
-        xEnd = size[0] - 1
-    if yEnd >= size[1]:
-        yEnd = size[1] - 1
-    return (xIn, yIn, xEnd, yEnd)
+def convert_to_absolute_values(size: Tuple[int, int], box: Tuple[float, float, float, float]) -> Tuple[int, int, int, int]:
+    """Convert relative coordinates back to absolute pixel values."""
+    if not all(0 <= x <= 1 for x in box[2:4]):
+        raise ValueError("Relative width and height must be between 0 and 1")
+    
+    x_in = round(((2 * float(box[0]) - float(box[2])) * size[0] / 2))
+    y_in = round(((2 * float(box[1]) - float(box[3])) * size[1] / 2)
+    x_end = x_in + round(float(box[2]) * size[0])
+    y_end = y_in + round(float(box[3]) * size[1])
+    
+    # Clip values to image boundaries
+    x_in = max(0, x_in)
+    y_in = max(0, y_in)
+    x_end = min(size[0] - 1, x_end)
+    y_end = min(size[1] - 1, y_end)
+    
+    return (x_in, y_in, x_end, y_end)
 
 
-def add_bb_into_image(image, bb, color=(255, 0, 0), thickness=2, label=None):
-    r = int(color[0])
-    g = int(color[1])
-    b = int(color[2])
-
+def add_bb_into_image(
+    image: np.ndarray,
+    bb,
+    color: Tuple[int, int, int] = (255, 0, 0),
+    thickness: int = 2,
+    label: Optional[str] = None,
+    font_scale: float = 0.5,
+    font_thickness: int = 1
+) -> np.ndarray:
+    """Add bounding box to image with optional label."""
+    if not isinstance(image, np.ndarray):
+        raise TypeError("Image must be a numpy array")
+    if image.ndim != 3 or image.shape[2] != 3:
+        raise ValueError("Image must be 3-channel BGR")
+    
+    r, g, b = int(color[0]), int(color[1]), int(color[2])
     font = cv2.FONT_HERSHEY_SIMPLEX
-    fontScale = 0.5
-    fontThickness = 1
 
     x1, y1, x2, y2 = bb.getAbsoluteBoundingBox(BBFormat.XYX2Y2)
-    x1 = int(x1)
-    y1 = int(y1)
-    x2 = int(x2)
-    y2 = int(y2)
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
     cv2.rectangle(image, (x1, y1), (x2, y2), (b, g, r), thickness)
-    # Add label
+    
     if label is not None:
-        # Get size of the text box
-        (tw, th) = cv2.getTextSize(label, font, fontScale, fontThickness)[0]
-        # Top-left coord of the textbox
-        (xin_bb, yin_bb) = (x1 + thickness, y1 - th + int(12.5 * fontScale))
-        # Checking position of the text top-left (outside or inside the bb)
-        if yin_bb - th <= 0:  # if outside the image
-            yin_bb = y1 + th  # put it inside the bb
-        r_Xin = x1 - int(thickness / 2)
-        r_Yin = y1 - th - int(thickness / 2)
-        # Draw filled rectangle to put the text in it
-        cv2.rectangle(image, (r_Xin, r_Yin - thickness),
-                      (r_Xin + tw + thickness * 3, r_Yin + th + int(12.5 * fontScale)), (b, g, r),
-                      -1)
-        cv2.putText(image, label, (xin_bb, yin_bb), font, fontScale, (0, 0, 0), fontThickness,
-                    cv2.LINE_AA)
+        # Get text size
+        (tw, th), _ = cv2.getTextSize(label, font, font_scale, font_thickness)
+        
+        # Text position
+        text_x = x1 + thickness
+        text_y = y1 - th + int(12.5 * font_scale)
+        
+        # Adjust if text would go above image
+        if text_y - th <= 0:
+            text_y = y1 + th
+            
+        # Background rectangle coordinates
+        bg_x1 = x1 - int(thickness / 2)
+        bg_y1 = y1 - th - int(thickness / 2)
+        bg_x2 = bg_x1 + tw + thickness * 3
+        bg_y2 = bg_y1 + th + int(12.5 * font_scale)
+        
+        # Draw background and text
+        cv2.rectangle(image, (bg_x1, bg_y1), (bg_x2, bg_y2), (b, g, r), -1)
+        cv2.putText(image, label, (text_x, text_y), font, font_scale, 
+                   (0, 0, 0), font_thickness, cv2.LINE_AA)
+    
     return image
