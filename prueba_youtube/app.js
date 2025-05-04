fe1f3e1c9b244b5432a3978c867339ffe4623383
vanejessi <jessi_3118@hotmail.com>
2025-05-04 10:08:45 +0200

    VideoYoutube test

---
+++ prueba_youtube/app.js
@@ -0,0 +1,23 @@
+function App() {
+  const videoId = '3n4Fdrz4LxQ'; // Puedes cambiarlo por otro
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Reproductor de YouTube</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
