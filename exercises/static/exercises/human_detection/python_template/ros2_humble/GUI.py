d26e96fc9a5e8768340b8a7578218a073cbdd14f
codezerro <kabircp08@gmail.com>
2025-06-11 00:44:53 +0600

    update

--- exercises/static/exercises/human_detection/python_template/ros2_humble/GUI.py
+++ exercises/static/exercises/human_detection/python_template/ros2_humble/GUI.py
@@ -9,12 +9,6 @@ import numpy as np
 
 # Graphical User Interface Class
 
-import os
-
-current_path = os.path.abspath(__file__)
-print("############################### Full path: ######################", current_path)
-
-
 class GUI(MeasuringThreadingGUI):
     def __init__(self, host="ws://127.0.0.1:2303"):
         super().__init__(host)
