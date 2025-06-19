96875be5145d9b6d8197d6e251399d6143e2de94
codezerro <kabircp08@gmail.com>
2025-06-19 14:45:14 +0600

    update regarding the review

--- database/exercises/db.sql
+++ database/exercises/db.sql
@@ -101,7 +101,7 @@ COPY public.exercises (id, exercise_id, name, description, tags, status, templat
 15	basic_computer_vision	Basic Computer Vision	Basic Computer Vision exercise using React and RAM	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/basic_computer_vision/python_template/
 16	follow_road	Follow Road	Follow Road exercise	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/follow_road/python_template/
 17	pick_place	Pick and Place	Pick and Place exercise	{"tags": "ROS2"}	PROTOTYPE	RoboticsAcademy/exercises/static/exercises/pick_place/python_template/
-18	human_detection	Human Detection	Human Detection with Computer Vision and Deep learning model.	{"tags": ["Computer Vision","Deep Learning"]}	PROTOTYPE	RoboticsAcademy/exercises/static/exercises/human_detection/python_template/
+18	human_detection	Human Detection	Deep learning-based human detection.	{"tags": ["Computer Vision","Deep Learning"]}	PROTOTYPE	RoboticsAcademy/exercises/static/exercises/human_detection/python_template/
 \.
 -- 16	rescue_people_classic	Rescue People Old	Rescue People exercise With Gazebo Classic	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/rescue_people_classic/python_template/
 
--- exercises/views.py
+++ exercises/views.py
@@ -1,11 +1,4 @@
-import json
-import mimetypes
 import os
-import shutil
-import tempfile
-import subprocess
-import zipfile
-import pylint as lint
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
@@ -15,7 +8,6 @@ from rest_framework.decorators import api_view
 from .models import Exercise
 from rest_framework.response import Response
 from rest_framework import status
-import base64
 
 
 # TODO: Too many hardcoded strings, review
@@ -45,19 +37,3 @@ def user_code_zip(request, exercise_id):
 
     except Exception as e:
         return Response({"success": False, "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    
-    """
-    try:
-        for x in os.listdir(exercise_path):
-            file_path = os.path.join(exercise_path, x)
-            with open(file_path, "rb") as f:  # Open in binary mode
-                content = f.read()
-                # If binary (e.g., .onnx), encode to base64 to make it JSON-safe
-                encoded_content = base64.b64encode(content).decode("utf-8")
-                files.append({"name": x, "content": encoded_content, "binary": True})
-
-        return JsonResponse({"success": True, "files": files})
-    except Exception as e:
-        return Response({"success": False, "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    """
-
