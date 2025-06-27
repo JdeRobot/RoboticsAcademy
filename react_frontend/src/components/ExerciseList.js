adcfd3e7900fe5ae61b3f69e5ee516df4be364a6
Apolo151 <abdallah.ibrahim5115@gmail.com>
2025-06-27 12:50:31 +0300

    update js linting workflow

--- react_frontend/src/components/ExerciseList.js
+++ react_frontend/src/components/ExerciseList.js
@@ -6,7 +6,7 @@ import HomepageContext from "../contexts/HomepageContext";
 
 const serverBase = `${document.location.protocol}//${document.location.hostname}:7164`;
 
-// let ros_version;
+// let ros_version
 
 const ExerciseList = () => {
   const { getSearchBarText, getFilterItemsList } = useContext(HomepageContext);
