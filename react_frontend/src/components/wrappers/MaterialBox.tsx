ecc50760978a1fa8e3fa99522fc7b7f1a1976a50
David Rodriguez Rives   -   The Pirate Cat <63803821+miniesda@users.noreply.github.com>
2025-07-23 19:59:40 +0200

    MaterialBox.js to MaterialBox.tsx

---
+++ react_frontend/src/components/wrappers/MaterialBox.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/system";
+import RoboticsTheme from "Components/RoboticsTheme";
+import React, { ReactNode } from "react";
+
+interface MaterialBoxProps {
+  id?: string;
+  children: ReactNode;
+}
+
+const MaterialBox: React.FC<MaterialBoxProps> = ({ id, children }) => {
+  return (
+    <RoboticsTheme>
+      <Box id={id}>{children}</Box>
+    </RoboticsTheme>
+  );
+};
+
+export default MaterialBox;
