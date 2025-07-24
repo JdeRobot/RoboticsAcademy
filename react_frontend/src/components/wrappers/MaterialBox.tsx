b044ba105ca7e0ddfbd6382a0eeb5d4929d5e86c
David Rodriguez Rives   -   The Pirate Cat <63803821+miniesda@users.noreply.github.com>
2025-07-24 16:24:32 +0200

    Update MaterialBox.tsx

--- react_frontend/src/components/wrappers/MaterialBox.tsx
+++ react_frontend/src/components/wrappers/MaterialBox.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/system";
-import RoboticsTheme from "Components/RoboticsTheme";
+import RoboticsTheme from "Components/RoboticsTheme.tsx";
 import React, { ReactNode } from "react";
 
 interface MaterialBoxProps {
