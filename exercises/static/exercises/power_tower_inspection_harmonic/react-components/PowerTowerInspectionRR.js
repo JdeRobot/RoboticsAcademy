148240cb083e228a79d98125f0c5707776e0836a
Arcane-01 <bt20ece046@students.vnit.ac.in>
2025-08-30 17:53:51 +0300

    added files for power tower inspection gz harmonic

---
+++ exercises/static/exercises/power_tower_inspection_harmonic/react-components/PowerTowerInspectionRR.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Fragment} from "react";
+
+import "./css/PowertowerInspectionRR.css";
+
+const PowerTowerInspectionRR = (props) => {
+  return (
+    <Fragment>
+    {props.children}
+    </Fragment>
+  );
+};
+
+export default PowerTowerInspectionRR;
