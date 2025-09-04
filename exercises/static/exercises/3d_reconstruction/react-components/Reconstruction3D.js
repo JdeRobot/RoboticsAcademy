276ef572e8c2cdff4a3bc4c7364d6a2e49cd55e9
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-09-04 19:12:53 +0200

    3d exercise

--- exercises/static/exercises/3d_reconstruction/react-components/Reconstruction3D.js
+++ exercises/static/exercises/3d_reconstruction/react-components/Reconstruction3D.js
@@ -1,61 +1,60 @@
-import * as React from "react";
-
+import { useState, useEffect } from "react";
+import { events } from "jderobot-commsmanager";
+import { useExercise } from "Contexts/ExerciseContext";
 import "./css/Reconstruction3DRR.css";
 import { draw, reset_all } from "./helpers/helperRecontruction";
 
-const Reconstruction3D = (props) => {
-    React.useEffect(() => {
-        console.log("TestShowScreen subscribing to ['update'] events");
-        const callback = (message) => {
-          if(message.data.update.img1){
-            const data = message.data.update;
-            draw(data)
-          }
-          // Send the ACK of the msg
-          window.RoboticsExerciseComponents.commsManager.send("gui", "ack");
-        };
-    
-        window.RoboticsExerciseComponents.commsManager.subscribe(
-          [window.RoboticsExerciseComponents.commsManager.events.UPDATE],
-          callback
-        );
-    
-        return () => {
-          console.log("TestShowScreen unsubscribing from ['state-changed'] events");
-          window.RoboticsExerciseComponents.commsManager.unsubscribe(
-            [window.RoboticsExerciseComponents.commsManager.events.UPDATE],
-            callback
-          );
-        };
-      }, []);
+const Reconstruction3D = () => {
+  const exerciseContext = useExercise();
+  const [manager, setManager] = useState(exerciseContext.manager);
 
-      React.useEffect(() => {
-        const callback = (message) => {
-          if (message.data.state === "ready") {
-            try {
-              reset_scene3d();
-              reset_all()
-            } catch (error) {
-            }
-          }
-        }
-        window.RoboticsExerciseComponents.commsManager.subscribe(
-          [window.RoboticsExerciseComponents.commsManager.events.STATE_CHANGED],
-          callback
-        );
+  useEffect(() => {
+    setManager(exerciseContext.manager);
+  }, [exerciseContext]);
 
-        return () => {
-          console.log("TestShowScreen unsubscribing from ['state-changed'] events");
-          window.RoboticsExerciseComponents.commsManager.unsubscribe(
-            [window.RoboticsExerciseComponents.commsManager.events.STATE_CHANGED],
-            callback
-          );
-        };
-      }, [])
+  useEffect(() => {
+    if (manager === null) {
+      return;
+    }
+
+    const updateCallback = (message) => {
+      if (message.data.update.img1) {
+        const data = message.data.update;
+        draw(data);
+      }
+      // Send the ACK of the msg
+      manager.send("gui", "ack");
+    };
+
+    const stateCallback = (message) => {
+      if (message.data.state === "ready") {
+        try {
+          reset_scene3d();
+          reset_all();
+        } catch (error) {}
+      }
+    };
+
+    manager.subscribe(events.UPDATE, updateCallback);
+    manager.subscribe(events.STATE_CHANGED, stateCallback);
+
+    return () => {
+      manager.unsubscribe(events.UPDATE, updateCallback);
+      manager.unsubscribe(events.STATE_CHANGED, stateCallback);
+    };
+  }, [manager]);
+
   return (
-    <div style={{display: "flex", height: "100%", width: "100%", position:"relative"}}>
-      <div id="canvas" style={{height: "100%", width: "33%"}}/>
-      <canvas id="gui_canvas" style={{width: "67%"}}></canvas>
+    <div
+      style={{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        position: "relative",
+      }}
+    >
+      <div id="canvas" style={{ height: "100%", width: "33%" }} />
+      <canvas id="gui_canvas" style={{ width: "67%" }}></canvas>
     </div>
   );
 };
