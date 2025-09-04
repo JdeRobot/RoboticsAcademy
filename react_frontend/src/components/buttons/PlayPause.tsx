fea4c1a6cc952e28e342dee86cd028aec3d2819a
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-09-04 19:32:49 +0200

    Improve play button

--- react_frontend/src/components/buttons/PlayPause.tsx
+++ react_frontend/src/components/buttons/PlayPause.tsx
@@ -30,6 +30,7 @@ const PlayPauseButton = ({
   const exerciseContext = useExercise();
   const { warning, error } = useError();
   const codeRef = useRef("");
+  const runningCodeRef = useRef("");
   const isCodeUpdatedRef = useRef<boolean | undefined>(undefined);
   const [isCodeUpdated, _updateCode] = useState<boolean | undefined>(false);
 
@@ -75,6 +76,20 @@ const PlayPauseButton = ({
       return;
     }
 
+    if (appRunning) {
+      try {
+        await manager.pause();
+        setAppRunning(false);
+        console.log("App paused correctly!");
+        return;
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          console.error("Error pausing app: " + e.message);
+          error("Error pausing app: " + e.message);
+        }
+      }
+    }
+
     if (save === undefined) {
       publish("autoSave");
       updateCode(false);
@@ -84,68 +99,62 @@ const PlayPauseButton = ({
       return setTimeout(onAppStateChange, 100, true);
     }
 
-    console.log(appRunning)
+    if (runningCodeRef.current === codeRef.current) {
+      await manager.resume();
+      setAppRunning(true);
+      console.log("App resumed correctly!");
+      return;
+    }
 
-    if (!appRunning) {
-      try {
-        const zip = new JSZip();
-        const commonsZip = await zip.loadAsync(commons);
+    try {
+      const zip = new JSZip();
+      const commonsZip = await zip.loadAsync(commons);
 
-        const extraFiles: { name: string; content: string }[] =
-          await getProjectExtraFiles(project);
+      const extraFiles: { name: string; content: string }[] =
+        await getProjectExtraFiles(project);
 
-        extraFiles.forEach((file) => {
-          commonsZip.file(file.name, file.content);
-        });
+      extraFiles.forEach((file) => {
+        commonsZip.file(file.name, file.content);
+      });
 
-        commonsZip.file("academy.py", codeRef.current);
+      commonsZip.file("academy.py", codeRef.current);
 
-        // add onnx file to the zip if it exists
-        if (hasDLModel) {
-          if (dlModel) {
-            commonsZip.file("model.onnx", dlModel);
-          } else {
-            throw new Error("No ONNX model found.");
-          }
-        }
-
-        // Convert the blob to base64 using FileReader
-        const reader = new FileReader();
-        reader.onloadend = async () => {
-          const base64data = reader.result; // Get the zip in base64
-          // Send the base64 encoded blob
-          if (base64data) {
-            await manager.run(
-              "/workspace/code/academy.py",
-              ["academy.py"],
-              base64data as string
-            );
-            console.log("Dockerized app started successfully");
-          }
-        };
-
-        zip.generateAsync({ type: "blob" }).then(function (content: Blob) {
-          reader.readAsDataURL(content);
-        });
-
-        setAppRunning(true);
-        console.log("App started successfully");
-      } catch (e: unknown) {
-        if (e instanceof Error) {
-          console.error("Error running app: " + e.message);
-          error("Error running app: " + e.message);
+      // add onnx file to the zip if it exists
+      if (hasDLModel) {
+        if (dlModel) {
+          commonsZip.file("model.onnx", dlModel);
+        } else {
+          throw new Error("No ONNX model found.");
         }
       }
-    } else {
-      try {
-        await manager.pause();
-        setAppRunning(false);
-        console.log("App paused correctly!");
-      } catch (e: unknown) {
-        if (e instanceof Error) {
-          console.error("Error pausing app: " + e.message);
-          error("Error pausing app: " + e.message);
+
+      runningCodeRef.current = codeRef.current;
+
+      // Convert the blob to base64 using FileReader
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        const base64data = reader.result; // Get the zip in base64
+        // Send the base64 encoded blob
+        if (base64data) {
+          await manager.run(
+            "/workspace/code/academy.py",
+            ["academy.py"],
+            base64data as string
+          );
+          console.log("Dockerized app started successfully");
         }
+      };
+
+      zip.generateAsync({ type: "blob" }).then(function (content: Blob) {
+        reader.readAsDataURL(content);
+      });
+
+      setAppRunning(true);
+      console.log("App started successfully");
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.error("Error running app: " + e.message);
+        error("Error running app: " + e.message);
       }
     }
   };
