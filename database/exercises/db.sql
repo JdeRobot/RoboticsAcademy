be4d2fd3af6414c802a5419582ca67840290ba6b
miniesda <miniesda@gmail.com>
2025-10-01 19:21:50 +0200

    Update db.sql

--- database/exercises/db.sql
+++ database/exercises/db.sql
@@ -136,7 +136,7 @@ COPY public.exercises (id, exercise_id, name, description, tags, status, url) FR
 22	end_to_end_visual_control	End to End Visual Control	End to end visual control using deep learning.	["Computer Vision","Deep Learning","AUTONOMOUS DRIVING"]	PROTOTYPE	https://jderobot.github.io/RoboticsAcademy/exercises/AutonomousCars/end_to_end_visual_control
 23	digital_image_processing	Digital Image Processing Exercises	Digital Image Processing Exercises	["ROS2"]	PROTOTYPE	https://jderobot.github.io/RoboticsAcademy/exercises/ComputerVision/digital_image_processing
 24	car_junction	Car Junction	Autonomous Navigation through traffic at road junction.	["AUTONOMOUS DRIVING","ROS2"]	PROTOTYPE	https://jderobot.github.io/RoboticsAcademy/exercises/AutonomousCars/car_junction
-25	o3de_warehouse	O3DE Warehouse	Control an O3DE robot to organize a warehouse	{"tags": "ROS2"}	PROTOTYPE	RoboticsAcademy/exercises/static/exercises/o3de_warehouse/python_template/
+25	o3de_warehouse	O3DE Warehouse	Control an O3DE robot to organize a warehouse	{"tags": "ROS2"}	PROTOTYPE	https://jderobot.github.io/RoboticsAcademy/exercises
 \.
 
 --
