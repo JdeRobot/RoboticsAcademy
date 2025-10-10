6a5ed91e9ea0280cd91f2d2e6909144dbaa4fff2
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-10-10 11:11:01 +0200

    Fully working exercise

--- exercises/static/exercises/follow_line/cpp_template/ros2_humble/HAL.hpp
+++ exercises/static/exercises/follow_line/cpp_template/ros2_humble/HAL.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_HAL_HPP_
 
 #include "geometry_msgs/msg/twist.hpp"
+#include "gazebo_msgs/msg/performance_metrics.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -24,6 +25,9 @@ public:
     odom_sub_ = create_subscription<nav_msgs::msg::Odometry>(
         "/odom", 10,
         std::bind(&HAL::pose_callback, this, _1));
+    perf_sub_ = create_subscription<gazebo_msgs::msg::PerformanceMetrics>(
+        "/performance_metrics", 10,
+        std::bind(&HAL::performance_callback, this, _1));
   };
 
   static void set_v(const float speed)
@@ -49,6 +53,11 @@ public:
     return v;
   };
 
+  static double get_performance()
+  {
+    return last_perf.real_time_factor;
+  };
+
 private:
   void topic_callback_info(sensor_msgs::msg::Image::UniquePtr msg)
   {
@@ -72,20 +81,28 @@ private:
     last_odom = *msg;
   };
 
+  void performance_callback(gazebo_msgs::msg::PerformanceMetrics::UniquePtr msg)
+  {
+    last_perf = *msg;
+  };
+
   // Publisher
   static rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr cam_sub_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Subscription<gazebo_msgs::msg::PerformanceMetrics>::SharedPtr perf_sub_;
 
   // Message
   static geometry_msgs::msg::Twist last_twist;
   static cv::Mat image_rgb;
   static nav_msgs::msg::Odometry last_odom;
+  static gazebo_msgs::msg::PerformanceMetrics last_perf;
 };
 
 rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr HAL::vel_pub_ = nullptr;
 geometry_msgs::msg::Twist HAL::last_twist = geometry_msgs::msg::Twist();
 nav_msgs::msg::Odometry HAL::last_odom = nav_msgs::msg::Odometry();
+gazebo_msgs::msg::PerformanceMetrics HAL::last_perf = gazebo_msgs::msg::PerformanceMetrics();
 cv::Mat HAL::image_rgb = cv::Mat();
 
 #endif
