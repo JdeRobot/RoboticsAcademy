72340e262f711fe051e91eb90029435e029486b7
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-10-23 19:02:38 +0200

    Save db in admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from .models import Exercise, Universe, World, Tool, Robot
 
+from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(Exercise)
-admin.site.register(Tool)
-admin.site.register(Universe)
-admin.site.register(World)
-admin.site.register(Robot)
+class SaveExAdmin(admin.ModelAdmin):
+    change_list_template = './admin/change_list_ex.html'
+
+class SaveUnivAdmin(admin.ModelAdmin):
+    change_list_template = './admin/change_list_univ.html'
+
+admin.site.register(Exercise, SaveExAdmin)
+admin.site.register(Tool, SaveUnivAdmin)
+admin.site.register(Universe, SaveUnivAdmin)
+admin.site.register(World, SaveUnivAdmin)
+admin.site.register(Robot, SaveUnivAdmin)
--- exercises/urls.py
+++ exercises/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("exercises/<slug:exercise_id>/", views.load_exercise, name="load_exercise"),
+    path("save_exercise_db/", views.save_exercise_db, name="save_exercise_db"),
+    path("save_universe_db/", views.save_universe_db, name="save_universe_db"),
     path(
         "user_code_zip/",
         views.user_code_zip,
