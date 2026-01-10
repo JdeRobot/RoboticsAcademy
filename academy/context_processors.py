27d7564bdefae770d214ff4bd6832a008b6b700f
monish0705 <monishmr2006@gmail.com>
2026-01-10 12:16:14 +0530

    Add inline documentation to Django settings and context processor

--- academy/context_processors.py
+++ academy/context_processors.py
@@ -2,4 +2,21 @@ from django.conf import settings
 
 
 def version(request):
+    """
+    Context processor to expose the system version to templates.
+
+    This function makes the application version available globally
+    in Django templates via the context.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The incoming HTTP request object.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the system version under the
+        key 'SYS_VERSION'.
+    """
     return {"SYS_VERSION": settings.VERSION}
