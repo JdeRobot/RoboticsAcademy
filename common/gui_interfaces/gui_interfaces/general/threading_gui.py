00eba9bf80255030fb763e6b5875d3b6fd01bdc0
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-02-20 13:14:35 +0100

    Change import of logger

--- common/gui_interfaces/gui_interfaces/general/threading_gui.py
+++ common/gui_interfaces/gui_interfaces/general/threading_gui.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.insert(0, "/RoboticsApplicationManager")
 
-from manager.ram_logging.log_manager import LogManager
+from robotics_application_manager import LogManager
 
 
 class ThreadingGUI:
