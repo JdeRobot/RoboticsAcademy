5d65921fbbc47e8da6ad0833c739b1000e0fc071
Madhukar Vaibhav <libero09jnv@gmail.com>
2026-04-05 19:31:58 +0530

    Update
    Removed the lines commented.

--- exercises/autoparking/python_template/HAL.py
+++ exercises/autoparking/python_template/HAL.py
@@ -68,62 +68,78 @@ def getPose3d():
 
 
 def getFrontLaserData():
+    """
+    If you are using the new 3D LiDAR universe, use getLidarData() instead.
+
+    Returns:
+        LaserData: Object with attributes:
+            - values (list[float]): Distance measurements in meters
+            - minAngle (float): Start angle of scan in radians
+            - maxAngle (float): End angle of scan in radians
+            - timeStamp (float): Timestamp of the scan
+    """
     
     laser = laser_front_node.getLaserData()
     timestamp = laser.timeStamp
     while timestamp == 0.0:
-        laser = laser_front_node.getLaserData()
-        timestamp = laser.timeStamp
-    return laser
 
 
 def getRightLaserData():
+    """
+    If you are using the new 3D LiDAR universe, use getLidarData() instead.
+
+    Returns:
+        LaserData: Object with attributes:
+            - values (list[float]): Distance measurements in meters
+            - minAngle (float): Start angle of scan in radians
+            - maxAngle (float): End angle of scan in radians
+            - timeStamp (float): Timestamp of the scan
+    """
     
     laser = laser_right_node.getLaserData()
     timestamp = laser.timeStamp
     while timestamp == 0.0:
-        laser = laser_right_node.getLaserData()
-        timestamp = laser.timeStamp
-    return laser
 
 
 def getBackLaserData():
+    """
+    If you are using the new 3D LiDAR universe, use getLidarData() instead.
+
+    Returns:
+        LaserData: Object with attributes:
+            - values (list[float]): Distance measurements in meters
+            - minAngle (float): Start angle of scan in radians
+            - maxAngle (float): End angle of scan in radians
+            - timeStamp (float): Timestamp of the scan
+    """
     
     laser = laser_back_node.getLaserData()
     timestamp = laser.timeStamp
     while timestamp == 0.0:
-        laser = laser_back_node.getLaserData()
-        timestamp = laser.timeStamp
-    return laser
 
 
 def getLidarData():
+    """
+    Use this function instead of the laser functions (getFrontLaserData,
+    getRightLaserData, getBackLaserData) when working in the 3D LiDAR universe.
+
+    Returns:
+        LidarData: Object with the following attributes:
+            - points (list[tuple]): List of (x, y, z) tuples in meters
+            - intensities (list[float]): Intensity values per point
+                                        (empty if not available)
+            - timeStamp (float): Timestamp in seconds
+            - min_range (float): Minimum sensor range in meters (default: 0.1)
+            - max_range (float): Maximum sensor range in meters (default: 15.0)
+            - field_of_view (tuple): (horizontal, vertical) FOV in radians
+            - is_dense (bool): True if all points are finite (no NaN/Inf)
+
+    Example:
+        lidar = HAL.getLidarData()
+        for point in lidar.points:
+            x, y, z = point
+    """
    
     lidar = lidar_node.getLidarData()
     timestamp = lidar.timeStamp
     while timestamp == 0.0:
-        lidar = lidar_node.getLidarData()
-        timestamp = lidar.timeStamp
-    return lidar
-
-
-def setV(velocity):
-    """
-    Sets the linear velocity of the car.
-
-    Args:
-        velocity (float): Linear velocity in m/s.
-                         Positive = forward, Negative = backward
-    """
-    motor_node.sendV(float(velocity))
-
-
-def setW(velocity):
-    """
-    Sets the angular velocity of the car.
-
-    Args:
-        velocity (float): Angular velocity in rad/s.
-                         Positive = left turn, Negative = right turn
-    """
-    motor_node.sendW(float(velocity))
