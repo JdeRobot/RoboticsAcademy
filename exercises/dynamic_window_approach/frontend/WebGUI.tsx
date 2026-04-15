c169cac0a622b910618a86cd1efcf7986bfd1c89
Jose Miguel Jimenez Garcia <jm.jimenez.2020@alumnos.urjc.es>
2026-04-15 10:43:29 +0200

    changed files for DWA exercise

---
+++ exercises/dynamic_window_approach/frontend/WebGUI.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import { states } from "jderobot-commsmanager";
+import { useExercise } from "Contexts/ExerciseContext";
+import WebGUIContainer, {
+  connectApplication,
+} from "Components/exercise/WebGUIContainer";
+
+import F1Car from "./resources/f1-car.svg";
+import Arrow from "./resources/arrow.svg";
+import "./css/GUICanvas.css";
+
+function WebGUI() {
+  const meter = 73; // 1m = 73px
+
+  const [laser, setLaser] = useState<number[][]>([]);
+  const [maxRange, setMaxRange] = useState<number>(1000);
+  const [carForce, setCarForce] = useState<number[]>([2 * meter, 0]);
+  const [avgForce, setAvgForce] = useState<number[]>([2 * meter, 0]);
+  const [obsForce, setObsForce] = useState<number[]>([2 * meter, -Math.PI / 2]);
+  const [targetPose, setTargetPose] = useState<number[] | undefined>(undefined);
+  const exerciseContext = useExercise();
+  const [manager, setManager] = useState(exerciseContext.manager);
+
+  useEffect(() => {
+    setManager(exerciseContext.manager);
+  }, [exerciseContext]);
+
+  const updateCallback = (updateData: unknown) => {
+    let data = updateData as any;
+    data = data.update;
+
+    if (data.map) {
+      const dataToDraw = JSON.parse(data.map);
+
+      setLaser(dataToDraw.laser);
+      setMaxRange(dataToDraw.max_range);
+      const carForceDist = getDist(dataToDraw.car[0], dataToDraw.car[1]);
+      setCarForce([
+        carForceDist * meter,
+        getAng(dataToDraw.car[0], dataToDraw.car[1]),
+      ]);
+      const avgForceDist = getDist(
+        dataToDraw.average[0],
+        dataToDraw.average[1]
+      );
+      setAvgForce([
+        avgForceDist * meter,
+        getAng(dataToDraw.average[0], dataToDraw.average[1]),
+      ]);
+      const obsForceDist = getDist(
+        dataToDraw.obstacle[0],
+        dataToDraw.obstacle[1]
+      );
+      setObsForce([
+        obsForceDist * meter,
+        getAng(dataToDraw.obstacle[0], dataToDraw.obstacle[1]),
+      ]);
+      const targetDist = getDist(
+        dataToDraw.pose[0] - dataToDraw.target[0],
+        dataToDraw.pose[1] - dataToDraw.target[1]
+      );
+      const targetAng = getAng(
+        dataToDraw.pose[0] - dataToDraw.target[0],
+        dataToDraw.pose[1] - dataToDraw.target[1]
+      );
+      setTargetPose([
+        targetDist * meter,
+        targetAng - dataToDraw.pose[2] - Math.PI,
+      ]);
+    }
+  };
+
+  const getDist = (x: number, y: number) => {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  };
+
+  const getAng = (x: number, y: number) => {
+    const ang = Math.atan2(y, x);
+    return ang;
+  };
+
+  const stateCallback = (state: string) => {
+    if (state === states.TOOLS_READY) {
+      setTargetPose(undefined);
+    }
+  };
+
+  connectApplication(manager, updateCallback);
+
+  return (
+    <WebGUIContainer id="f1-road">
+      <img src={F1Car} id="f1-car" />
+      {laser.map((element) => {
+        const ang = -element[1];
+        const length = (element[0] / maxRange) * 100;
+        return (
+          <hr
+            className="laser-beam"
+            style={{
+              rotate: "z " + ang + "rad",
+              width: "calc(" + length + "%)",
+            }}
+          />
+        );
+      })}
+      <img
+        className="arrow green"
+        src={Arrow}
+        style={{ height: carForce[0], rotate: "z " + -carForce[1] + "rad" }}
+      />
+      <img
+        className="arrow red"
+        src={Arrow}
+        style={{ height: obsForce[0], rotate: "z " + -obsForce[1] + "rad" }}
+      />
+      <img
+        className="arrow"
+        src={Arrow}
+        style={{
+          height: avgForce[0],
+          rotate: "z " + -avgForce[1] + "rad",
+          zIndex: "6",
+        }}
+      />
+      {targetPose && (
+        <div
+          className="target-container"
+          style={{
+            height: targetPose[0],
+            rotate: "z " + -targetPose[1] + "rad",
+          }}
+        >
+          <div id="target" />
+        </div>
+      )}
+    </WebGUIContainer>
+  );
+}
+
+export default WebGUI;
---
+++ exercises/dynamic_window_approach/python_template/HAL.py
@@ -0,0 +1,59 @@
+import rclpy
+import threading
+import sys
+
+from hal_interfaces.general.motors import MotorsNode
+from hal_interfaces.general.odometry import OdometryNode
+from hal_interfaces.general.laser import LaserNode
+
+# Hardware Abstraction Layer
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+# {"name": "target10", "x": -47, "y": 45},
+
+
+# Mutes exceptions
+def custom_thread_excepthook(args):
+    if "spin" in args.thread.name:
+        return
+    sys.__excepthook__(args.exc_type, args.exc_value, args.exc_traceback)
+
+
+threading.excepthook = custom_thread_excepthook
+
+# ROS2 init
+if not rclpy.ok():
+    rclpy.init(args=None)
+    rclpy.create_node("HAL")
+
+pose3d = OdometryNode("/odom")
+motors = MotorsNode("/cmd_vel", 4, 0.3)
+laser = LaserNode("/f1/laser/scan")
+
+# Spin nodes so that subscription callbacks load topic data
+executor = rclpy.executors.MultiThreadedExecutor()
+executor.add_node(pose3d)
+executor.add_node(laser)
+executor_thread = threading.Thread(target=executor.spin, daemon=True)
+executor_thread.start()
+
+print("HAL-Nodes Thread Started")
+
+
+def getPose3d():
+    return pose3d.getPose3d()
+
+
+def getLaserData():
+    laser_data = laser.getLaserData()
+    while len(laser_data.values) == 0:
+        laser_data = laser.getLaserData()
+    return laser_data
+
+
+def setV(velocity):
+    motors.sendV(float(velocity))
+
+
+def setW(velocity):
+    motors.sendW(float(velocity))
