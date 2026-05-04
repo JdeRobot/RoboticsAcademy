a53800ac384c24850b2d4f71ed04bd44c0241057
Javier Izquierdo Hernandez <javizqh@pm.me>
2026-05-04 12:21:59 +0200

    Remove dwa

--- database/exercises/db.sql
+++ database/exercises/db.sql
@@ -137,7 +137,6 @@ COPY public.exercises (id, exercise_id, name, description, tags, status, url) FR
 24	car_junction	Car Junction	Autonomous Navigation through traffic at road junction.	["AUTONOMOUS DRIVING","ROS2"]	ACTIVE	https://jderobot.github.io/RoboticsAcademy/exercises/AutonomousCars/car_junction
 25	machine_vision	Machine Vision	Machine Vision exercise	["ROS2"]	PROTOTYPE	https://jderobot.github.io/RoboticsAcademy/exercises/IndustrialRobots/machine_vision
 26	labyrinth_escape	Labyrinth Escape	Labyrinth Escape exercise	["ROS2","Drones"]	ACTIVE	https://jderobot.github.io/RoboticsAcademy/exercises/Drones/labyrinth_escape
-27	dynamic_window_approach	Local Navigation	Dynamic Window Approach exercise	["ROS2","AUTONOMOUS DRIVING"]	PROTOTYPE	https://jderobot.github.io/RoboticsAcademy/exercises/AutonomousCars/obstacle_avoidance
 \.
 
 --
